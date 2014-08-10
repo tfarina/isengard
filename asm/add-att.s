9a646a53a63e0cce52ba280c299f55989d568141
Thiago Farina <tfarina@chromium.org>
2014-08-10 12:39:22 -0300

    Initial commit.
    Next will add a command to assembly add.c.
    
    It needs manual edition after clang outputs the assembly code
    for it though before you can call build-*.sh
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ asm/add-att.s
@@ -0,0 +1,11 @@
+	.globl	add
+add:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %esi
+	addl	-8(%rbp), %esi
+	movl	%esi, %eax
+	popq	%rbp
+	retq
---
+++ asm/add-intel.s
@@ -0,0 +1,11 @@
+	.globl	add
+add:
+	push	rbp
+	mov	rbp, rsp
+	mov	dword ptr [rbp - 4], edi
+	mov	dword ptr [rbp - 8], esi
+	mov	esi, dword ptr [rbp - 4]
+	add	esi, dword ptr [rbp - 8]
+	mov	eax, esi
+	pop	rbp
+	ret
