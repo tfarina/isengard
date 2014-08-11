01feba926c4c7dba3c480ab63f1fec797803e1e5
Thiago Farina <tfarina@chromium.org>
2014-08-10 22:21:05 -0300

    Add the yasm command.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- asm/build-att.sh
+++ asm/build-att.sh
@@ -3,6 +3,8 @@
 # Die if any command dies.
 set -e
 
+# yasm -f elf64 -p gas add-att.s -o add_def.o
+
 as add-att.s -o add_def.o
 clang++ -c add_main.cc -o add_main.o
 clang++ add_main.o add_def.o -o add_main
