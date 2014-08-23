92a75c8e6a9c5b130f56105895cf67da19cb8e99
Thiago Farina <tfarina@chromium.org>
2014-08-22 22:25:59 -0300

    Add sqlite/ directory.
    For playing with sqlite3 library.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ sqlite/create_users_db.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <sqlite3.h>
+
+// http://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
+
+static void html_content() {
+  printf("Content-type: text/html\r\n\r\n");
+}
+
+static void html_start() {
+  printf("<!DOCTYPE html>");
+  printf("<html>\n");
+}
+
+static void html_end() {
+  printf("</body>\r\n");
+  printf("</html>\r\n");
+}
+
+int main(int argc, char* argv[]) {
+  html_content();
+  html_start();
+  printf("<head>\n");
+  printf("</head>\n");
+  printf("<body>\n");
+
+  sqlite3* db;
+  int rc = sqlite3_open("users.db", &db);
+  if (rc) {
+    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return rc;
+  } else {
+    fprintf(stdout, "Opened database successfully\n");
+  }
+  sqlite3_close(db);
+
+  html_end();
+  return 0;
+}
