60d670265f96b5c802248c7dc0d1b5dbad245cc8
Thiago Farina <tfarina@chromium.org>
2014-08-28 09:17:16 -0300

    Cleanup select output.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/select.c
+++ sqlite/cli/select.c
@@ -6,35 +6,35 @@
 
 static int callback(void* data, int argc, char** argv, char** column_name) {
   int i;
-  fprintf(stdout, "<br>%s: <br><br>\n", (const char*)data);
+  fprintf(stdout, "%s: \n", (const char*)data);
   for (i = 0; i < argc; ++i) {
-    printf("%s = %s<br>\n", column_name[i], argv[i] ? argv[i] : "NULL");
+    printf("%-12s %s\n", column_name[i], argv[i] ? argv[i] : "NULL");
   }
-  printf("<br>\n");
+  printf("\n");
   return 0;
 }
 
 int main(int argc, char* argv[]) {
   sqlite3* db;
-  int rc = sqlite3_open("users.db", &db);
+  int rc;
+
+  rc = sqlite3_open("users.db", &db);
   if (rc) {
-    fprintf(stdout, "Can't open database: %s<br>\n", sqlite3_errmsg(db));
+    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
     sqlite3_close(db);
     return rc;
   }
 
-  fprintf(stdout, "Opened 'users.db' database successfully!<br>\n");
-
   const char* sql = "SELECT * FROM user";
   const char* data = "Callback function called";
+
   rc = sqlite3_exec(db, sql, callback, (void*)data, NULL);
   if (rc != SQLITE_OK) {
-    fprintf(stdout, "SQL error: %s\n<br>", sqlite3_errmsg(db));
+    fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
     sqlite3_close(db);
     return 1;
   }
 
-  fprintf(stdout, "Selected everything from 'users' table successfully!<br>\n");
   sqlite3_close(db);
 
   return 0;
