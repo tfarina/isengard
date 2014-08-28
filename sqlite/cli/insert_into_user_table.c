2de0f59ed8b799482c690171297be5c94d902b08
Thiago Farina <tfarina@chromium.org>
2014-08-28 00:13:49 -0300

    Silent insertion.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/insert_into_user_table.c
+++ sqlite/cli/insert_into_user_table.c
@@ -11,19 +11,16 @@ int main(int argc, char* argv[]) {
     return rc;
   }
 
-  fprintf(stdout, "Opened 'users.db' database successfully!<br>\n");
-
   const char *sql = "INSERT INTO user (login, pw, email) "
                     "VALUES ('tff', 'abcdef12345678', 'tff@gmail.com');";
 
   rc = sqlite3_exec(db, sql, NULL, NULL, NULL);
   if (rc != SQLITE_OK) {
-    fprintf(stdout, "SQL error: %s\n<br>", sqlite3_errmsg(db));
+    fprintf(stderr, "SQL error: %s\n<br>", sqlite3_errmsg(db));
     sqlite3_close(db);
     return 1;
   }
 
-  fprintf(stdout, "User 'tff' inserted into 'user' table successfully!<br>\n");
   sqlite3_close(db);
 
   return 0;
