3294641d3a4e95523fe1f273ba01ac433a7a515f
Thiago Farina <tfarina@chromium.org>
2014-08-27 23:07:19 -0300

    Add cli version.
    Pure, no cgi.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ sqlite/cli/create_users_db.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <sqlite3.h>
+
+// http://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
+
+static const char kDbName[] = "users.db";
+
+int main(int argc, char* argv[]) {
+  sqlite3* db;
+  int rc = sqlite3_open(kDbName, &db);
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return rc;
+  } else {
+    fprintf(stdout, "Opened database successfully\n");
+  }
+  sqlite3_close(db);
+
+  return 0;
+}
