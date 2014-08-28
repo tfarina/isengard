c6cd7de232430857d6684445f15ba6116793f98f
Thiago Farina <tfarina@chromium.org>
2014-08-28 08:46:02 -0300

    Modernize insert.c
    Make it less hardcoded and more similar to delete.c.
    
    That way we can pass the values from command line in a follow up change.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/insert.c
+++ sqlite/cli/insert.c
@@ -4,6 +4,8 @@
 
 int main(int argc, char* argv[]) {
   sqlite3* db;
+  sqlite3_stmt* stmt;
+
   int rc = sqlite3_open("users.db", &db);
   if (rc) {
     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
@@ -11,15 +13,20 @@ int main(int argc, char* argv[]) {
     return rc;
   }
 
-  const char *sql = "INSERT INTO user (login, pw, email) "
-                    "VALUES ('tff', 'abcdef12345678', 'tff@gmail.com');";
+  const char *sql = "INSERT INTO user (login, pw, email) VALUES (?1, ?2, ?3);";
 
-  rc = sqlite3_exec(db, sql, NULL, NULL, NULL);
+  rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
   if (rc != SQLITE_OK) {
-    fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+    fprintf(stderr, "%s\n", sqlite3_errmsg(db));
     sqlite3_close(db);
     return 1;
   }
+
+  sqlite3_bind_text(stmt, 1, "jrp", -1, SQLITE_STATIC);
+  sqlite3_bind_text(stmt, 2, "abcdef123", -1, SQLITE_STATIC);
+  sqlite3_bind_text(stmt, 3, "jrp@hotmail.com", -1, SQLITE_STATIC);
+  sqlite3_step(stmt);
+  sqlite3_finalize(stmt);
 
   sqlite3_close(db);
 
