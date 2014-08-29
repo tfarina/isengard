b354e5cd08bd119fe12a7d331428665e13157d60
Thiago Farina <tfarina@chromium.org>
2014-08-29 00:47:13 -0300

    Refactor the code to open sqlite database into a function.
    delete.c version.
    
    Heavily inspired by:
    http://w1.fi/cgit/hostap/tree/src/eap_server/eap_sim_db.c:db_open
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/delete.c
+++ sqlite/cli/delete.c
@@ -4,22 +4,31 @@
 
 // http://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
 
+static sqlite3* db_open(const char* db_file) {
+  sqlite3* db;
+
+  if (sqlite3_open(db_file, &db)) {
+    fprintf(stderr, "Failed to open database %s: %s\n", "users.db",
+            sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return NULL;
+  }
+
+  return db;
+}
+
 int main(int argc, char* argv[]) {
   sqlite3* db;
   sqlite3_stmt *stmt;
+  int rc;
 
-  int rc = sqlite3_open("users.db", &db);
-  if (rc) {
-    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-    sqlite3_close(db);
-    return rc;
-  }
+  db = db_open("users.db");
 
   const char* sql = "DELETE FROM user WHERE uid=?1;";
 
   rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
   if (rc != SQLITE_OK) {
-    fprintf(stderr, "%s\n", sqlite3_errmsg(db));
+    fprintf(stderr, "SQLite error: %s\n", sqlite3_errmsg(db));
     sqlite3_close(db);
     return 1;
   }
