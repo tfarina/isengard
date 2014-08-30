6ebd22f2ddecd52f4d584e2d60e202fb3dbcdf46
Thiago Farina <tfarina@chromium.org>
2014-08-30 00:00:32 -0300

    Refactor update.c as well.
    Mordernizing it and extract the functions to open the db and to update
    user email, like the other tools.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/update.c
+++ sqlite/cli/update.c
@@ -14,23 +14,39 @@ static int callback(void* data, int argc, char** argv, char** column_name) {
   return 0;
 }
 
-int main(int argc, char* argv[]) {
+static sqlite3* db_open(const char* db_file) {
   sqlite3* db;
-  int rc = sqlite3_open("users.db", &db);
-  if (rc) {
-    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+
+  if (sqlite3_open(db_file, &db)) {
+    fprintf(stderr, "Failed to open database %s: %s\n", "users.db",
+            sqlite3_errmsg(db));
     sqlite3_close(db);
-    return rc;
+    return NULL;
   }
 
+  return db;
+}
+
+static int db_update(sqlite3* db) {
   const char *sql = "UPDATE user SET email='john@yahoo.com' WHERE uid=1; "
                     "SELECT * FROM user";
   const char* data = "Callback function called";
-  rc = sqlite3_exec(db, sql, callback, (void*)data, NULL);
-  if (rc != SQLITE_OK) {
+  if (sqlite3_exec(db, sql, callback, (void*)data, NULL) != SQLITE_OK) {
     fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+    return -1;
+  }
+
+  return 0;
+}
+
+int main(int argc, char* argv[]) {
+  sqlite3* db;
+
+  db = db_open("users.db");
+
+  if (db_update(db)) {
     sqlite3_close(db);
-    return 1;
+    return -1;
   }
 
   sqlite3_close(db);
