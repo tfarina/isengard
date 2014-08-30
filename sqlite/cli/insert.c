af513c06c2cd588cdb998b55020da1b8b252a7d3
Thiago Farina <tfarina@chromium.org>
2014-08-30 13:31:43 -0300

    Do not hardcode the database name to users.db in db_open.
    After this, db_open is generic enough.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/insert.c
+++ sqlite/cli/insert.c
@@ -6,7 +6,7 @@ static sqlite3* db_open(const char* db_file) {
   sqlite3* db;
 
   if (sqlite3_open(db_file, &db)) {
-    fprintf(stderr, "Failed to open database %s: %s\n", "users.db",
+    fprintf(stderr, "Failed to open database %s: %s\n", db_file,
             sqlite3_errmsg(db));
     sqlite3_close(db);
     return NULL;
