d9592c72cd485d5c77fdaf949e941d591fd1f32c
Thiago Farina <tfarina@chromium.org>
2014-09-21 11:56:19 -0300

    Add some documentation to sqlite-schema.sql
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/sqlite-schema.sql
+++ sqlite/sqlite-schema.sql
@@ -1,5 +1,16 @@
+/*
+ * SQLITE DATABASE SCHEMA FOR THE cli DATABASE.
+ *
+ * To recreate the database: sqlite3 users.db < sqlite-schema.sql
+ *
+ */
+
+
 PRAGMA foreign_keys=OFF;
+
 BEGIN TRANSACTION;
+
+/* User table --------------------------------------------------------------- */
 
 CREATE TABLE user (
   uid INTEGER PRIMARY KEY,
@@ -8,5 +19,12 @@ CREATE TABLE user (
   email TEXT
 );
 
+/* Initial Data ------------------------------------------------------------- */
+
+/*
+ * Populating with standard values.
+ */
+
 INSERT INTO "user" VALUES (1, 'tff', 'abcdef12345', 'tff@gmail.com');
+
 COMMIT;
