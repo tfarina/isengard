6c4d991474d3e0be69f5470da8c8b62bda82dd6d
Thiago Farina <tfarina@chromium.org>
2014-09-30 23:35:27 -0300

    Remove SQLite code.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- mysql/php/register.php
+++ mysql/php/register.php
@@ -4,18 +4,6 @@ include("distwi.php");
 $username = get_post_param_trim("username");
 $password = get_post_param_trim("password");
 
-$query = <<<EOD
-  INSERT INTO user (login, pw) VALUES ( '$username', '$password' )
-EOD;
-
-//$db->exec($query) or die("Unable to add user $username");
-
-// $result = $db->query('SELECT * FROM user') or die('Query failed');
-// while ($row = $result->fetchArray())
-//         {
-//   echo "Username: {$row['login']}\nPassword: {$row['pw']}\n";
-// }
-
 $query = sprintf("INSERT INTO `user` (login, pw) VALUES ('%s', '%s')",
     mysql_real_escape_string($username),
     mysql_real_escape_string($password));
