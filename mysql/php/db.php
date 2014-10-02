6254ba8e61bc6c2e5cc44077481576a20a9c924b
Thiago Farina <tfarina@chromium.org>
2014-10-02 10:45:11 -0300

    Do not echo if the connection to the database was successful.
    It just polutes the user interface.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- mysql/php/db.php
+++ mysql/php/db.php
@@ -9,11 +9,9 @@ if (!$connection) {
   die('Failed to connect to the database: ' . mysql_error());
 }
 
-echo "Connected successfully\n";
-
 $db_selected = mysql_select_db('valentino');
 if (!$db_selected) {
-  die('Database selection failed ' . mysql_error());
+  die('Failed to select the database: ' . mysql_error());
 }
 
 //mysql_close($connection);
