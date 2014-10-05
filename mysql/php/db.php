74f823d69a70a26f4eebf5e4c69d5cfdff396708
Thiago Farina <tfarina@chromium.org>
2014-10-05 16:24:00 -0300

    Extract database name into a variable.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- mysql/php/db.php
+++ mysql/php/db.php
@@ -3,13 +3,14 @@
 $hostname = 'localhost';
 $username = 'vrossi';
 $password = 'vr46';
+$database = 'valentino';
 
 $connection = mysql_connect($hostname, $username, $password);
 if (!$connection) {
   die('Failed to connect to the database: ' . mysql_error());
 }
 
-$db_selected = mysql_select_db('valentino');
+$db_selected = mysql_select_db($database);
 if (!$db_selected) {
   die('Failed to select the database: ' . mysql_error());
 }
