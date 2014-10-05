4ba98a1f2f9dc10e00654061fb0721a33421b8fd
Thiago Farina <tfarina@chromium.org>
2014-10-04 22:56:20 -0300

    Add 'login' form and a minimal login.php script.
    The script does trivial checks but it seems to work.
    
    It shows a blank page if we the user entered the registered
    username/password correctly.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ mysql/php/login.php
@@ -0,0 +1,19 @@
+<?
+include("distwi.php");
+
+$username = get_post_param_trim("username");
+$password = get_post_param_trim("password");
+
+if (!$username || !$password)
+    go_back("You need to enter both username and password to login.");
+
+$query = sprintf("SELECT uid FROM user WHERE login='%s' AND length(pw) > 0 AND pw='%s'",
+    mysql_real_escape_string($username),
+    mysql_real_escape_string($password));
+
+$result = mysql_query($query);
+if (mysql_num_rows($result) == 0) {
+    go_back("Wrong username or password.");
+}
+
+?>
--- mysql/php/welcome.php
+++ mysql/php/welcome.php
@@ -18,6 +18,20 @@
         </tr>
       </table>
     </form>
+    <h2>Already registered? Login here</h2>
+    <form method="POST" action="login.php">
+      <table>
+        <tr>
+          <td>Username</td><td><input type="text" name="username"></td>
+        </tr>
+        <tr>
+          <td>Password</td><td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+          <td colspan="2" align="right"><input type="submit" name="doit" value="Login"></td>
+        </tr>
+      </table>
+    </form>
   </div>
   Distwi is a very simple PHP application.
 </div>
