5b585bac981ffa025837e4b7d18e84345587fedc
Thiago Farina <tfarina@chromium.org>
2014-10-05 22:26:23 -0300

    Add slide 20 of go tour.
    tour.golang.org/#20
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ go/tour/slide20.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//
+// For is Go's "while":
+//
+// At that point you can drop the semicolons: C's while is spelled for in Go.
+//
+func main() {
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+}
