aba8d4ccaf0bf629d238f163741860a49ef237e4
Thiago Farina <tfarina@chromium.org>
2014-10-06 18:06:47 -0300

    Define database constants.
    Instead of using variables.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- mysql/php/twitwi/db.php
+++ mysql/php/twitwi/db.php
@@ -1,16 +1,16 @@
 <?
 
-$hostname = 'localhost';
-$username = 'twin';
-$password = 'catheham';
-$database = 'twitwi';
+define("DB_SERVER", "localhost");
+define("DB_USERNAME", "twin");
+define("DB_PASSWORD", "catheham");
+define("DB_NAME", "twitwi");
 
-$connection = mysql_connect($hostname, $username, $password);
+$connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 if (!$connection) {
   die('Failed to connect to the database: ' . mysql_error());
 }
 
-$db_selected = mysql_select_db($database);
+$db_selected = mysql_select_db(DB_NAME);
 if (!$db_selected) {
   die('Failed to select the database: ' . mysql_error());
 }
