98589cee689f103f6d2a2f8c8ff64a617e3a8ce2
Thiago Farina <tfarina@chromium.org>
2014-10-07 17:37:59 -0300

    Add simple python script that connects to MySQL database.
    It just selects all the entries from the 'user' table and displays it.
    
    Based on code from: http://zetcode.com/db/mysqlpython/
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ mysql/python/show_users.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import MySQLdb
+
+connection = MySQLdb.connect('localhost', 'vrossi', 'vr46', 'valentino')
+
+with connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM user")
+
+        rows = cursor.fetchall()
+
+        for row in rows:
+                print row
