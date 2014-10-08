ee391b0073a633b03884d2f50d740a02aec58052
Thiago Farina <tfarina@chromium.org>
2014-10-07 22:53:47 -0300

    Add query script.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ mysql/php/tutorial/query.php
@@ -0,0 +1,43 @@
+<?
+
+$host = "localhost";
+$user = "vrossi";
+$pass = "vr46";
+$db = "valentino";
+
+$r = mysql_connect($host, $user, $pass);
+
+if (!$r) {
+    echo "Could not connect to server\n";
+    trigger_error(mysql_error(), E_USER_ERROR);
+} else {
+    echo "Connection established\n";
+}
+
+$r2 = mysql_select_db($db);
+
+if (!$r2) {
+    echo "Cannot select database\n";
+    trigger_error(mysql_error(), E_USER_ERROR);
+} else {
+    echo "Database selected\n";
+}
+
+$query = "SELECT * FROM cars LIMIT 5";
+
+$rs = mysql_query($query);
+
+if (!$rs) {
+    echo "Could not execute query: $query";
+    trigger_error(mysql_error(), E_USER_ERROR);
+} else {
+    echo "Query: $query executed\n";
+}
+
+while ($row = mysql_fetch_assoc($rs)) {
+    echo $row['id'] . " " . $row['name'] . " " . $row['price'] . "\n";
+}
+
+mysql_close();
+
+?>
