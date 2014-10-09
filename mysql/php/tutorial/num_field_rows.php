d498bbe6d8715e802602e75838dc0f971eee1550
Thiago Farina <tfarina@chromium.org>
2014-10-09 00:29:34 -0300

    Add an example of using mysql_num_fields/mysql_num_rows.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ mysql/php/tutorial/num_field_rows.php
@@ -0,0 +1,44 @@
+<?
+
+$host = "localhost";
+$user = "vrossi";
+$pass = "vr46";
+$db = "valentino";
+
+$r = mysql_connect($host, $user, $pass);
+
+if (!$r) {
+    echo "Could not connect to server\n";
+    trigger_error(mysql_error(), E_USER_ERROR);
+} else {
+    echo "Connection established\n";
+}
+
+$r2 = mysql_select_db($db);
+
+if (!$r2) {
+    echo "Cannot select database\n";
+    trigger_error(mysql_error(), E_USER_ERROR);
+} else {
+    echo "Database selected\n";
+}
+
+$query = "SELECT * FROM cars WHERE id IN (1, 2, 3)";
+
+$rs = mysql_query($query);
+
+if (!$rs) {
+    echo "Could not execute query: $query";
+    trigger_error(mysql_error(), E_USER_ERROR);
+} else {
+    echo "Query: $query executed\n";
+}
+
+echo "We have ". mysql_num_fields($rs) ." fields\n";
+echo "We have ". mysql_num_rows($rs) ." rows\n";
+
+print_r(mysql_fetch_row($rs));
+
+mysql_close();
+
+?>
