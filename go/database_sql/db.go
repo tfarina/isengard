e1f0818f392b4880088ece3599f5b2b6247df23d
Thiago Farina <tfarina@chromium.org>
2014-10-15 20:52:35 -0300

    Remove unnecessary spaces in db.go
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- go/database_sql/db.go
+++ go/database_sql/db.go
@@ -13,14 +13,12 @@ import (
 func main() {
 	// TODO(tfarina): Create go_tutorial database and a user/pass for it.
 	db, err := sql.Open("mysql", "pyuser:yellow46@tcp(127.0.0.1:3306)/python_tutorial")
-
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
 	err = db.Ping()
-
 	if err != nil {
 		log.Fatal(err)
 	}
