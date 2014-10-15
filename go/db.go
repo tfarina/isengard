6397c78b7ab7ee378869b367ceeecc096ca3e08d
Thiago Farina <tfarina@chromium.org>
2014-10-15 20:28:21 -0300

    Add a go database/sql example that connects to a mysql database.
    Following the getting-started from http://go-database-sql.org/.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ go/db.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+//
+// http://go-database-sql.org/accessing.html
+//
+func main() {
+	// TODO(tfarina): Create go_tutorial database and a user/pass for it.
+	db, err := sql.Open("mysql", "pyuser:yellow46@tcp(127.0.0.1:3306)/python_tutorial")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
