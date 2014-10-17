fda280223b7e4cea47f3d6c6d54e9cfea1b21243
Thiago Farina <tfarina@chromium.org>
2014-10-17 18:47:39 -0300

    Print as string.
    Better error handling.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- go/fetch.go
+++ go/fetch.go
@@ -10,9 +10,12 @@ func main() {
 	log.Println("http://google.com")
 	resp, err := http.Get("https://www.google.com/")
 	if err != nil {
-		// handle error
+		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	log.Println(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(body))
 }
