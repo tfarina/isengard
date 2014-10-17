29e72b00ae4ce1e701d6b761902809bafd14041c
Thiago Farina <tfarina@chromium.org>
2014-10-17 18:44:55 -0300

    Add a webapps directory.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- go/webapps/hello2.go
+++ go/webapps/hello2.go
@@ -6,8 +6,10 @@
 
 package main
 
-import "net/http"
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<html><body><h2>Hello, www!</h2><a href=\"http://google.com\">Google</a></body></html>")
