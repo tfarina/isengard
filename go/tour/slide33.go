6e090681d567ca30d537ecf9d99d86c31e8d68aa
Thiago Farina <tfarina@chromium.org>
2014-10-16 22:51:23 -0300

    Add an example of empty slice.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- go/tour/slide33.go
+++ go/tour/slide33.go
@@ -14,4 +14,7 @@ func main() {
 	// place. Since it does the hi-1 for the second part, then [:3] will go
 	// from 0 to 2.
 	fmt.Println(primes[:3])
+
+	// [low:low] evaluates to an empty slide. No matter index you use. :(
+	fmt.Println(primes[2:2])
 }
