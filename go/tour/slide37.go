56b48ee01d79e87b3732b806d2818ddc8138298b
Thiago Farina <tfarina@chromium.org>
2014-11-12 00:14:09 -0200

    Add slide 37 of go tour.
    tour.golang.org/#37
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ go/tour/slide37.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+//
+// Range continued
+//
+// You can skip the index or value by assigning to _.
+//
+// If you only want the index, drop the ", value" entirely.
+//
+func main() {
+	pow := make([]int, 10)
+
+	for i := range pow {
+		pow[i] = 1 << uint(i)
+	}
+
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
+}
