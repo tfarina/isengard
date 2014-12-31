772b9dbac68d1a7233670b95cd7eef986a78724a
Thiago Farina <tfarina@chromium.org>
2014-12-30 22:15:20 -0200

    Add a debug build script for the intel syntax.
    It has -g flag so we can launch it under gdb and debug it.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- asm/build-intel-debug.sh
+++ asm/build-intel-debug.sh
@@ -4,5 +4,5 @@
 set -e
 
 as -mnaked-reg -msyntax=intel add-intel.s -o add_def.o
-clang++ -c add_main.cc -o add_main.o
-clang++ add_main.o add_def.o -o add_main
+clang++ -g -c add_main.cc -o add_main.o
+clang++ add_main.o add_def.o -o add_intel_debug
