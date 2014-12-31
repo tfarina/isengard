3feb95513a465edf2ee614b1ff6a2bedcd15fff0
Thiago Farina <tfarina@chromium.org>
2014-12-31 11:43:40 -0200

    Add ch01 example from RaySeyfarth Assembly book.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ asm/rayseyfarth/ch01/exit.asm
@@ -0,0 +1,18 @@
+;   Program: exit
+;
+;   Executes the exit system call
+;
+;   No input
+;
+;   Output: only the exit status ($? in the shell)
+;   echo $?
+;
+
+segment .text
+
+global  _start
+
+_start:
+    mov  eax, 1       ; 1 is the exit syscall number
+    mov  ebx, 5       ; the status value to return
+    int  0x80         ; execute a system call
