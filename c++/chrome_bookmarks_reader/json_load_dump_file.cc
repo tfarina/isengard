670e1cd9d9943572c644f83c6673bfdd3f2b7c16
Thiago Farina <tfarina@chromium.org>
2015-01-18 16:17:23 -0200

    Add an example showing how to use libjansson.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ c++/chrome_bookmarks_reader/json_load_dump_file.cc
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2009 Petri Lehtinen <petri@digip.org>
+ *
+ * Jansson is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ */
+
+#include <stdio.h>
+
+#include <jansson.h>
+
+int main(int argc, char** argv) {
+  if (argc != 3) {
+    fprintf(stderr, "usage: %s in-file out-file\n", argv[0]);
+    return 2;
+  }
+
+  json_error_t error;
+  json_t* json = json_load_file(argv[1], 0, &error);
+  if (!json) {
+    fprintf(stderr, "%d\n%s\n", error.line, error.text);
+    return 1;
+  }
+
+  json_t* bookmarks_object = json_object_get(json, "roots");
+  if (!bookmarks_object) {
+    fprintf(stderr, "%s\n", "no bookmarks object");
+    return 1;
+  }
+
+  printf("%s\n", json_dumps(bookmarks_object, 0));
+
+  void* iter = json_object_iter(bookmarks_object);
+  const char* iter_key = json_object_iter_key(iter);
+  printf("iter key: %s\n", iter_key);
+
+  json_t* iter_value = json_object_iter_value(iter);
+  // now here I can get the type!
+  printf("iter value: %s\n", json_dumps(iter_value, 0));
+
+  json_t* type = json_object_get(iter_value, "type");
+  const char* type_value = json_string_value(type);
+  printf("type value: %s\n", type_value);
+
+  //json_t* children = json_object_get(iter_value, "children");
+
+  // now to get any element in this array:
+  //json_t* element = json_array_get(children, 0);
+
+  printf("advance\n");
+  iter = json_object_iter_next(bookmarks_object, iter);
+  iter_key = json_object_iter_key(iter);
+  printf("iter key: %s\n", iter_key);
+
+  //json_dump_file(json, argv[2], 0);
+  json_decref(json);
+
+  return 0;
+}
