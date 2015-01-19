fdb3231ef4a1d529f3a13558c78098ba293ab9bd
Thiago Farina <tfarina@chromium.org>
2015-01-19 19:33:50 -0200

    Change 'update' tool to use db.h
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/update.c
+++ sqlite/cli/update.c
@@ -2,6 +2,8 @@
 
 #include <sqlite3.h>
 
+#include "db.h"
+
 // http://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
 
 // static int callback(void* data, int argc, char** argv, char** column_name) {
@@ -13,19 +15,6 @@
 //   printf("\n");
 //   return 0;
 // }
-
-static sqlite3* db_open(const char* db_file) {
-  sqlite3* db;
-
-  if (sqlite3_open(db_file, &db)) {
-    fprintf(stderr, "Failed to open database %s: %s\n", db_file,
-            sqlite3_errmsg(db));
-    sqlite3_close(db);
-    return NULL;
-  }
-
-  return db;
-}
 
 static int db_init_user_table(sqlite3* db) {
   const char* sql =
