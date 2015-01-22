ee6b41bc127f04efeda8a22b0418a246c7503ecf
Thiago Farina <tfarina@chromium.org>
2015-01-22 21:24:23 -0200

    Separate the action of running from the action of building.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- java/zetcodetutorial/javaswingtutorial/firstprograms/build.sh
+++ java/zetcodetutorial/javaswingtutorial/firstprograms/build.sh
@@ -3,5 +3,4 @@
 # https://ant.apache.org/manual/tutorial-HelloWorldWithAnt.html
 
 mkdir -p build/classes
-javac -sourcepath com/zetcode -d build/classes com/zetcode/EmptyWindow.java
-java -cp build/classes com.zetcode.EmptyWindow
+javac -sourcepath com/zetcode -d build/classes com/zetcode/*.java
