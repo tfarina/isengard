37a4c44882520de1ab5b473da9a8546293f3370e
Thiago Farina <tfarina@chromium.org>
2015-01-22 22:10:17 -0200

    Add batch script that allow us to build this on Windows as well.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ java/zetcodetutorial/build.bat
@@ -0,0 +1,8 @@
+@echo off
+
+if not exist ".\build" (
+  md build\classes
+)
+
+javac -sourcepath com\zetcode -d build\classes com\zetcode\HelloWorld.java
+java -cp build\classes com.zetcode.HelloWorld
