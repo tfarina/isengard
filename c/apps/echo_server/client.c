3cc0ea42a9a52527e620d2e01175b6b853303a49
Thiago Farina <tfarina@chromium.org>
2015-03-11 22:36:48 -0300

    Unbreak the client.
    Pass the correct port, now that the server listen on port 8088.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/echo_server/client.c
+++ c/apps/echo_server/client.c
@@ -9,19 +9,21 @@
 #include <strings.h>
 #include <arpa/inet.h>
 
+#define SERVER_PORT 8088
+
 int main(int argc,char **argv) {
   int sockfd;
   char sendline[100];
   char recvline[100];
   struct sockaddr_in servaddr;
 
-  sockfd = socket(AF_INET,SOCK_STREAM,0);
-  bzero(&servaddr,sizeof servaddr);
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  memset(&servaddr, 0, sizeof(servaddr));
 
   servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(22000);
+  servaddr.sin_port = htons(SERVER_PORT);
 
-  inet_pton(AF_INET, "127.0.0.1", &(servaddr.sin_addr));
+  inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
 
   connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
