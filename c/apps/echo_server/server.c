21c1766a96ad5f9f2870f4be0cc0b03d692bf653
Thiago Farina <tfarina@chromium.org>
2015-03-11 21:56:40 -0300

    s_addr should use htonl.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/echo_server/server.c
+++ c/apps/echo_server/server.c
@@ -17,7 +17,7 @@ int main() {
   memset(&servaddr, 0, sizeof(servaddr));
 
   servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
   servaddr.sin_port = htons(22000);
 
   listen_fd = socket(AF_INET, SOCK_STREAM, 0);
