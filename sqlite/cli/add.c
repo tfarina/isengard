bcef14d2fa9b7bbae6010bf357903bef6e20d648
Thiago Farina <tfarina@chromium.org>
2015-04-12 22:39:59 -0300

    Use two spaces indent in add.c
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/add.c
+++ sqlite/cli/add.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 int cmd_add(int argc, char **argv) {
-        if (argc != 4) {
-                printf("usage: add USERNAME PASSWORD EMAIL\n");
-                return 1;
-        }
+  if (argc != 4) {
+    printf("usage: add USERNAME PASSWORD EMAIL\n");
+    return -1;
+  }
 
-        return 0;
+  return 0;
 }
