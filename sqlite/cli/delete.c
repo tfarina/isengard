d2491fdd12d201c039548407dfee3f8b3ba71460
Thiago Farina <tfarina@chromium.org>
2015-04-25 15:40:21 -0300

    Change delete.c to reuse db_open() from db.h
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/delete.c
+++ sqlite/cli/delete.c
@@ -3,20 +3,9 @@
 
 #include <sqlite3.h>
 
+#include "db.h"
+
 // http://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
-
-static sqlite3* db_open(const char* db_file) {
-  sqlite3* db;
-
-  if (sqlite3_open(db_file, &db)) {
-    fprintf(stderr, "Failed to open database %s: %s\n", db_file,
-            sqlite3_errmsg(db));
-    sqlite3_close(db);
-    return NULL;
-  }
-
-  return db;
-}
 
 static int db_init_user_table(sqlite3* db) {
   const char* sql =
