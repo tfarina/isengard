250f52d6d6aaa7076eff41ae309e14f062e8d43c
Thiago Farina <tfarina@chromium.org>
2015-04-25 15:48:42 -0300

    Fix indentation in cmd_del.c
    Print something when calling 'list' command.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/cmd_del.c
+++ sqlite/cli/cmd_del.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 int cmd_del(int argc, char **argv) {
-        printf("New argc count: %d\n", argc);
-        printf("New argv[0]: %s\n", argv[0]);
-        printf("function: cmd_del() called!\n");
+  printf("New argc count: %d\n", argc);
+  printf("New argv[0]: %s\n", argv[0]);
+  printf("function: cmd_del() called!\n");
 
-        return 0;
+  return 0;
 }
--- sqlite/cli/cmd_list.c
+++ sqlite/cli/cmd_list.c
@@ -1,5 +1,11 @@
 #include "umc.h"
 
+#include <stdio.h>
+
 int cmd_list(int argc, char **argv) {
+  printf("New argc count: %d\n", argc);
+  printf("New argv[0]: %s\n", argv[0]);
+  printf("function: cmd_list() called!\n");
+
   return 0;
 }
