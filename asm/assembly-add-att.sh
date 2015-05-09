b2ae324d4580fc8304b00378e1c8ae360f1ce707
Thiago Farina <tfarina@chromium.org>
2015-05-09 14:51:26 -0300

    cfi directives
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- asm/assembly-add-att.sh
+++ asm/assembly-add-att.sh
@@ -5,4 +5,4 @@ set -e
 
 clang -S -O0 -fno-unwind-tables -fno-asynchronous-unwind-tables add.c -o add_att_x64.s
 
-echo 'now edit add_att_x64.s to remove the clutter'
+echo 'now edit add_att_x64.s to remove the cfi directives'
