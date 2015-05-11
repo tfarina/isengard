da49ab6ff10d757913f159dbf23d4bf95c960fad
Thiago Farina <tfarina@chromium.org>
2015-05-11 15:48:40 -0300

    Add usage output to umc.
    The output was inspired by tick's
    https://github.com/jvirtanen/tick/blob/master/tick.c#L40
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/umc.c
+++ sqlite/cli/umc.c
@@ -1,6 +1,7 @@
 #include "umc.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
@@ -14,9 +15,23 @@ static struct {
   { "list", cmd_list },
 };
 
+static void usage(void) {
+#define FMT                              \
+"\n usage: umc COMMAND [ARGS]\n"         \
+"\n The commands are:\n"                 \
+"   add       Add a new user\n"          \
+"   del       Delete an existing user\n" \
+"   list      List all users\n"          \
+"\n"
+
+        fprintf(stderr, FMT);
+#undef FMT
+        exit(EXIT_FAILURE);
+}
+
 int main(int argc, char **argv) {
         if (argc < 2)
-                return 1;
+                usage();
 
         int i;
         for (i = 0; i < ARRAY_SIZE(cmds); ++i) {
