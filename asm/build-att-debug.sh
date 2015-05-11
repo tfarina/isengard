e8638983b42f664482b53afc1b6558e74cac2a5e
Thiago Farina <tfarina@chromium.org>
2015-05-10 23:42:18 -0300

    Pass the .s file to as as the last parameter in the build scripts.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- asm/build-att-debug.sh
+++ asm/build-att-debug.sh
@@ -5,6 +5,6 @@ set -e
 
 # yasm -f elf64 -p gas -o add_def.o add_att_x64.s
 
-as add_att_x64.s -o add_def.o
+as -o add_def.o add_att_x64.s
 clang -g -c add_main.c -o add_main.o
 clang -o add_att_debug add_def.o add_main.o
