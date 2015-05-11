4746d17b6b5cb33671b604609c17227853a37845
Thiago Farina <tfarina@chromium.org>
2015-05-11 00:21:49 -0300

    Add some comment explaining the purpose of add_main.c
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- asm/add_main.c
+++ asm/add_main.c
@@ -1,4 +1,5 @@
-/* yasm -f elf64 -p gas -o add_def.o add_att_x64.s
+/*
+ * yasm -f elf64 -p gas -o add_def.o add_att_x64.s
  * clang -c add_main.c -o add_main.o
  * clang -o add_main add_def.o add_main.o
  */
@@ -7,6 +8,9 @@
 
 int add(int a, int b);
 
+/*
+ * This shows how to call a function implemented in Assembly from a C program.
+ */
 int main(void) {
     printf("%d\n", add(3, 5));
 
