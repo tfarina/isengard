14adce6c044c9d3b8785f78c2aff9616b6f37512
Thiago Farina <tfarina@chromium.org>
2015-05-10 23:33:27 -0300

    Fix the command lines used in add_main.c
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- asm/add_main.c
+++ asm/add_main.c
@@ -1,6 +1,7 @@
-// yasm -f elf64 -p gas add2.asm -o add_def.o
-// clang -c add_main.c -o add_main.o
-// clang add_main.o add_def.o -o add_main
+/* yasm -f elf64 -p gas add_intel_x64.s -o add_def.o
+ * clang -c add_main.c -o add_main.o
+ * clang -o add_main add_def.o add_main.o
+ */
 
 #include <stdio.h>
 
