05b9bcf9b119b1fd4cdf1457aac9e4c93a255c77
Thiago Farina <tfarina@chromium.org>
2015-05-11 17:54:42 -0300

    Remove xstrstr and xmemcmp implementation.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- buf/unix.c
+++ buf/unix.c
@@ -28,17 +28,7 @@ size_t xstrlen(const char *s)
 	return strlen(s);
 }
 
-char* xstrstr(char *a, char *b)
-{
-	return strstr(a, b);
-}
-
 void xmemmove(void *dst, const void *src, size_t n)
 {
 	memmove(dst, src, n);
-}
-
-int xmemcmp(void *a, void *b, int n)
-{
-	return memcmp(a, b, n);
 }
