e60a2de0ef991fbe5cb817920768af37b842491c
Thiago Farina <tfarina@chromium.org>
2015-05-10 23:39:42 -0300

    Pass the source file as the last parameter to yasm in the build scripts.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- asm/build-att-release.sh
+++ asm/build-att-release.sh
@@ -3,7 +3,7 @@
 # Die if any command dies.
 set -e
 
-# yasm -f elf64 -p gas add_att_x64.s -o add_def.o
+# yasm -f elf64 -p gas -o add_def.o add_att_x64.s
 
 as add_att_x64.s -o add_def.o
 clang -c add_main.c -o add_main.o
