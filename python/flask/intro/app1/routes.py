53269361b42b3f6479a873ecf6999dbf060ad7a8
Thiago Farina <tfarina@chromium.org>
2015-06-23 14:07:37 -0300

    Add 'about' page.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- python/flask/intro/app1/routes.py
+++ python/flask/intro/app1/routes.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def home():
   return render_template("home.html")
 
+@app.route("/about")
+def about():
+  return render_template("about.html")
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0")
