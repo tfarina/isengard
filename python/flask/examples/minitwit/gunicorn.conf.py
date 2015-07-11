9e0fd0d37ca44bad5a61a4c64cd4e94fdc0a3374
Thiago Farina <tfarina@chromium.org>
2015-07-11 00:32:16 -0300

    Move the configuration of gunicorn to a config file.
    That way we have more flexibility to configure it and add more stuff
    later.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ python/flask/examples/minitwit/gunicorn.conf.py
@@ -0,0 +1,4 @@
+# http://agiliq.com/blog/2014/06/minimal-gunicorn-configuration/
+bind='127.0.0.1:8000'
+pidfile='minitwit.pid'
+daemon=True
--- python/flask/examples/minitwit/run_server.sh
+++ python/flask/examples/minitwit/run_server.sh
@@ -2,4 +2,4 @@
 
 # https://exploreflask.com/deployment.html
 
-sudo gunicorn -b 127.0.0.1:8000 -p minitwit.pid -D minitwit:app
+sudo gunicorn -c gunicorn.conf.py minitwit:app
