105a95e030e5f822487c605064c3b53650f4f468
Thiago Farina <tfarina@chromium.org>
2015-07-18 15:02:12 -0300

    Improve the script to create database and user on MySQL.
    When creating a user, using 'create user ...', it will fail
    if the user already exists:
    
    Error Code: 1396. Operation CREATE USER failed for 'blaine'@'localhost'
    
    The alternative it to use grant - because grant will create the user if the
    account does not already exist.
    
    grant seems to be the most elegant way to create a use on MySQL.
    
    http://www.cryer.co.uk/brian/mysql/how_to_create_user_if_not_exist.htm
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- python/flask/examples/minitwit/database/setup_db.sql
+++ python/flask/examples/minitwit/database/setup_db.sql
@@ -1,6 +1,5 @@
 # SQL for setting up the database and user.
 
 CREATE DATABASE IF NOT EXISTS minitwit;
-CREATE USER 'blaine'@'localhost' IDENTIFIED BY '19801219';
-GRANT ALL ON minitwit.* TO 'blaine'@'localhost';
+GRANT ALL ON minitwit.* TO 'blaine'@'localhost' IDENTIFIED BY '19801219';
 FLUSH PRIVILEGES;
