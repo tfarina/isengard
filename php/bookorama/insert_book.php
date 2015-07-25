36bebbf16c4cb614489019ab9940e2548f42b229
Thiago Farina <tfarina@chromium.org>
2015-07-24 21:52:08 -0300

    php/bookorama: Add the insert_book.php file.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ php/bookorama/insert_book.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once("book_sc_fns.php");
+session_start();
+
+do_html_header("Adding a book");
+if (check_admin_user()) {
+  if (filled_out($_POST)) {
+    $isbn = $_POST['isbn'];
+    $title = $_POST['title'];
+    $author = $_POST['author'];
+    $catid = $_POST['catid'];
+    $price = $_POST['price'];
+    $description = $_POST['description'];
+
+    if (insert_book($isbn, $title, $author, $catid, $price, $description)) {
+      echo "<p>Book <em>".strislashes($title)."</em> was added to the database.
+           </p>";
+    } else {
+      echo "<p>Book <em>".strislashes($title)."</em> could not be added to
+            the database.</p>";
+    }
+  } else {
+    echo "<p>You have not filled out the form. Please, try again.</p>";
+  }
+
+  do_html_url("admin.php", "Back to administration menu");
+} else {
+  echo "<p>You are not authorized to view this page.</p>";
+}
+
+do_html_footer();
+
+?>
