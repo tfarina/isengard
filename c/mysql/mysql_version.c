4f6cb74fbc235111d0383d9e66f28816eba42bda
Thiago Farina <tfarina@chromium.org>
2015-07-29 19:06:57 -0300

    c: Add the initial connection with MySQL from C.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ c/mysql/mysql_version.c
@@ -0,0 +1,15 @@
+/*
+ * Modified from http://zetcode.com/db/mysqlc/
+ * sudo apt-get install libmysqlclient-dev
+ * gcc mysql_version.c -o mysql_version `mysql_config --cflags --libs`
+ * gcc -o mysql_version `mysql_config --cflags` mysql_version.c `mysql_config --libs`
+ */
+
+#include <stdio.h>
+#include <mysql.h>
+
+int main(int argc, char **argv) {
+  printf("MySQL client version: %s\n", mysql_get_client_info());
+
+  return 0;
+}
