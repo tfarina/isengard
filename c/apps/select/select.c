9370c6bb25b0b65d55346961d61e067a080bd1b4
Thiago Farina <tfarina@chromium.org>
2015-08-06 14:02:31 -0300

    c: Add select() demo.
    http://beej.us/guide/bgnet/output/html/multipage/advanced.html#select
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ c/apps/select/select.c
@@ -0,0 +1,32 @@
+/*
+ * Author: beej.us
+ * URL: http://beej.us/guide/bgnet/output/html/multipage/advanced.html#select
+ * Slightly modified from http://beej.us/guide/bgnet/examples/select.c
+ */
+
+#include <stdio.h>
+#include <sys/select.h>
+#include <sys/time.h>
+
+#define STDIN 0 /* file descriptor for standard input. */
+
+int main(void) {
+  struct timeval tv;
+  fd_set readfds;
+
+  tv.tv_sec = 2;
+  tv.tv_usec = 500000;
+
+  FD_ZERO(&readfds);
+  FD_SET(STDIN, &readfds);
+
+  /* Don't care about writefds and exceptfds. */
+  select(STDIN + 1, &readfds, NULL, NULL, &tv);
+
+  if (FD_ISSET(STDIN, &readfds))
+    printf("A key was pressed.\n");
+  else
+    printf("Timed out.\n");
+
+  return 0;
+}
