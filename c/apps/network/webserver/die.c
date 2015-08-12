0978895db2110dedc8e4e0b9497d90906e80a99e
Thiago Farina <tfarina@chromium.org>
2015-08-12 18:00:47 -0300

    Put my die() functions all in sync.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/network/webserver/die.c
+++ c/apps/network/webserver/die.c
@@ -4,11 +4,15 @@
 #include <stdlib.h>
 
 void die(const char *msg, ...) {
-    fprintf(stderr, "fatal: ");
     va_list args;
+
+    fprintf(stderr, "fatal: ");
+
     va_start(args, msg);
     vfprintf(stderr, msg, args);
     va_end(args);
+
     fprintf(stderr, "\n");
+
     exit(EXIT_FAILURE);
 }
