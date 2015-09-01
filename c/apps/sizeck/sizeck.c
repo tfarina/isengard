f4282b875c8d001e23a4afd32109d680438a9290
Thiago Farina <tfarina@chromium.org>
2015-08-31 23:19:19 -0300

    add more documentation about the 8 bits.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/sizeck/sizeck.c
+++ c/apps/sizeck/sizeck.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Remember: 1 byte = 8 bits\n\n");
+    printf("Remember: 1 byte = 8 bits\n");
+    printf("and with 8 bits it is possible to represent 256 (2^8) values\n");
+    printf("this is also possible because a bit can only be either 0 or 1\n\n");
 
     printf("char: %lu byte\n", sizeof(char));
     printf("short: %lu bytes\n", sizeof(short));
