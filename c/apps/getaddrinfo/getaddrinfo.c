14264584586deee6bf725ce2416a9eed06b89fb1
Thiago Farina <tfarina@chromium.org>
2015-09-17 14:26:09 -0300

    getaddrinfo: Some changes to cleanup it.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/getaddrinfo/getaddrinfo.c
+++ c/apps/getaddrinfo/getaddrinfo.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <errno.h>
 #include <netdb.h>
-#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 
 /* Get ip from domain name. */
-static int hostname_to_ip(char *hostname, char *ip)
-{
+static int hostname_to_ip(char *hostname, char *ip) {
   int sockfd;
   struct addrinfo hints;
   struct addrinfo *servinfo;
@@ -25,7 +24,6 @@ static int hostname_to_ip(char *hostname, char *ip)
     return 1;
   }
 
-
   for (p = servinfo; p != NULL; p = p->ai_next) {
     h = (struct sockaddr_in *)p->ai_addr;
     strcpy(ip, inet_ntoa(h->sin_addr));
@@ -33,18 +31,18 @@ static int hostname_to_ip(char *hostname, char *ip)
   }
 
   freeaddrinfo(servinfo);
+
   return 0;
 }
 
-int main(int argc, char **argv)
-{
-  if (argc < 2) {
-    printf("Please, provide a hostname to resolve\n");
-    exit(1);
-  }
-
+int main(int argc, char **argv) {
   char *hostname = argv[1];
   char ip[100];
+
+  if (argc < 2) {
+    fprintf(stderr, "Please, provide a hostname to resolve\n");
+    exit(EXIT_FAILURE);
+  }
 
   hostname_to_ip(hostname, ip);
   printf("%s resolved to %s\n", hostname, ip);
