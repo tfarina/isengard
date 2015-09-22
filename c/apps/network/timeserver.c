65644616d717305bffb7181f64aa04981791f0e6
Thiago Farina <tfarina@chromium.org>
2015-09-22 20:16:19 -0300

    network: Use a shorter buffer size.
    256 is enough to handle the size of the time string.
    
    Value taken from FreeBSD's inetd:
    https://github.com/freebsd/freebsd/blob/master/usr.sbin/inetd/builtins.c#L227
    
    This code is not stable yet though, seeing "fatal: accept failed".
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/network/timeserver.c
+++ c/apps/network/timeserver.c
@@ -26,7 +26,7 @@
 
 #define SERVER_PORT 8088
 #define BACKLOG 1024
-#define MAXLINE 4096
+#define BUFSIZE 256
 
 typedef void (*sig_handler)(int);
 
@@ -49,11 +49,11 @@ static void handle_chld(int sig) {
 /* Echo the current day time to the connected client. */
 static void process_request(int sockfd) {
   time_t t;
-  char str[MAXLINE];
+  char buf[BUFSIZE];
 
   time(&t);
-  sprintf(str, "%.24s\r\n", ctime(&t));
-  Writen(sockfd, str, strlen(str));
+  sprintf(buf, "%.24s\r\n", ctime(&t));
+  Writen(sockfd, buf, strlen(buf));
 }
 
 int main(int argc, char **argv) {
