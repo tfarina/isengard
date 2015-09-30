57ce438760400523caa82f773a5bd380098dbdcc
Thiago Farina <tfarina@chromium.org>
2015-09-30 13:00:12 -0300

    echo: Mark die() function as noreturn.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/network/echo/die.h
+++ c/apps/network/echo/die.h
@@ -1,6 +1,7 @@
 #ifndef DIE_H_
 #define DIE_H_
 
-void die(const char *msg, ...);
+void die(const char *msg, ...) __attribute__((noreturn))
+    __attribute__((format(printf, 1, 2)));
 
 #endif  // DIE_H_
