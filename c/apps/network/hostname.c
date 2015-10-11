47c0849c6b9a48a2601a0a0058f9d218468ee3dd
Thiago Farina <tfarina@chromium.org>
2015-10-11 14:13:04 -0300

    hostname: Output the string error message.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/network/hostname.c
+++ c/apps/network/hostname.c
@@ -1,13 +1,15 @@
+#include <errno.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 int main(void) {
   char buf[HOST_NAME_MAX];
 
   if (gethostname(buf, sizeof(buf)) == -1) {
-    fprintf(stderr, "gethostname() failed.\n");
+    fprintf(stderr, "gethostname() failed: %s.\n", strerror(errno));
     exit(EXIT_FAILURE);
   }
 
