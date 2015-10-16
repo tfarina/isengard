5751c95da9a022292fbc3c3810426345a9f3d32e
Thiago Farina <tfarina@chromium.org>
2015-10-16 18:28:51 -0300

    c/mysql/show.c: Initialize MYSQL pointer after declaring it.
    And check the return value of mysql_init() using the C-idiom.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/mysql/show.c
+++ c/mysql/show.c
@@ -11,19 +11,19 @@ static const char kDBUser[] = "ken";
 static const char kDBPassword[] = "194304";
 static const char kDBName[] = "ctestdb";
 
-static void finish_with_error(MYSQL *con) {
-  fprintf(stderr, "%s\n", mysql_error(con));
-  mysql_close(con);
-  exit(1); 
+static void finish_with_error(MYSQL *sql) {
+  fprintf(stderr, "%s\n", mysql_error(sql));
+  mysql_close(sql);
+  exit(EXIT_FAILURE);
 }
 
 int main(int argc, char **argv) {
-  MYSQL *sql = mysql_init(NULL);
+  MYSQL *sql = NULL;
   unsigned int port = 0;
 
-  if (sql == NULL) {
+  if ((sql = mysql_init(NULL)) == NULL) {
     fprintf(stderr, "mysql_init() failed\n");
-    exit(1);
+    exit(EXIT_FAILURE);
   }
 
   if (mysql_real_connect(sql, kDBHost, kDBUser, kDBPassword, kDBName, port, NULL, 0) == NULL) {
