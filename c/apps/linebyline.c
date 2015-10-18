77f73270cc50a8d53b2804f6a91cd016bae2420e
Thiago Farina <tfarina@chromium.org>
2015-10-17 22:52:52 -0300

    c/apps: Add a program that reads a file and print it line by line.
    This shows how to parse a file and is forms the base for implementing a
    configuration system based on config files.
    
    Source from MyDNS.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ c/apps/linebyline.c
@@ -0,0 +1,43 @@
+#include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv) {
+  FILE *fp;
+  char linebuf[BUFSIZ];
+  int lineno = 0;
+  struct stat st;
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: linebyline <filename>\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (stat(argv[1], &st)) {
+    fprintf(stderr, "%s: %s\n", argv[1], strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  if ((fp = fopen(argv[1], "r"))) {
+    /* This loops through the lines of the file, printing it. */
+    while (fgets(linebuf, sizeof(linebuf), fp)) {
+      lineno++;
+      printf("%d:%s", lineno, linebuf);
+
+      /* This loops through *every* character present in this line. */
+      char *c;
+      for (c = linebuf; *c; c++) {
+        printf("%c", *c);
+      }
+    }
+    fclose(fp);
+  } else {
+    fprintf(stderr, "%s: %s\n", argv[1], strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  return 0;
+}
