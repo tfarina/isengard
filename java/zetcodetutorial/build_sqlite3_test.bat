17530c5e0860232c88290524f874aa33fde0b369
Thiago Farina <tfarina@chromium.org>
2015-10-28 14:36:57 -0200

    java: Move sqlite3 jar file into third_party/sqlite3.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- java/zetcodetutorial/build_sqlite3_test.bat
+++ java/zetcodetutorial/build_sqlite3_test.bat
@@ -4,5 +4,5 @@ if not exist ".\build" (
   md build\classes
 )
 
-javac -cp lib\* -sourcepath com\zetcode -d build\classes com\zetcode\Sqlite3Test.java
-java -cp lib\*;build\classes com.zetcode.Sqlite3Test
+javac -cp third_party\sqlite3\* -sourcepath com\zetcode -d build\classes com\zetcode\Sqlite3Test.java
+java -cp third_party\sqlite3\*;build\classes com.zetcode.Sqlite3Test
--- java/zetcodetutorial/build_sqlite3_test.sh
+++ java/zetcodetutorial/build_sqlite3_test.sh
@@ -4,8 +4,8 @@
 
 mkdir -p output/classes
 
-javac -classpath lib/sqlite-jdbc-3.8.7.jar \
+javac -classpath third_party/sqlite3/sqlite-jdbc-3.8.7.jar \
       -sourcepath com/zetcode \
       -d output/classes com/zetcode/Sqlite3Test.java
 
-java -cp lib/*:output/classes com.zetcode.Sqlite3Test
+java -cp third_party/sqlite3/*:output/classes com.zetcode.Sqlite3Test
