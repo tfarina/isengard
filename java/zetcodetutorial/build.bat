c56fa6acc3df7fc25053b00cb55f38e6694dab05
Thiago Farina <tfarina@chromium.org>
2015-10-28 14:38:13 -0200

    java: Fix the build file for Windows.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- java/zetcodetutorial/build.bat
+++ java/zetcodetutorial/build.bat
@@ -1,8 +1,8 @@
 @echo off
 
-if not exist ".\build" (
-  md build\classes
+if not exist ".\output" (
+  md output\classes
 )
 
-javac -sourcepath com\zetcode -d build\classes com\zetcode\HelloWorld.java
-java -cp build\classes com.zetcode.HelloWorld
+javac -sourcepath com\zetcode -d output\classes com\zetcode\HelloWorld.java
+java -cp output\classes com.zetcode.HelloWorld
