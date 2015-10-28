e908211335876ec65606fc1e41be189d1be3d769
Thiago Farina <tfarina@chromium.org>
2015-10-28 13:44:41 -0200

    java: Specify the sqlite jar file explicitly.
    And spell out 'classpath' in the command line.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- java/zetcodetutorial/build_sqlite3_test.sh
+++ java/zetcodetutorial/build_sqlite3_test.sh
@@ -3,5 +3,9 @@
 # https://bitbucket.org/xerial/sqlite-jdbc#markdown-header-usage
 
 mkdir -p output/classes
-javac -cp lib/* -sourcepath com/zetcode -d output/classes com/zetcode/Sqlite3Test.java
+
+javac -classpath lib/sqlite-jdbc-3.8.7.jar \
+      -sourcepath com/zetcode \
+      -d output/classes com/zetcode/Sqlite3Test.java
+
 java -cp lib/*:output/classes com.zetcode.Sqlite3Test
