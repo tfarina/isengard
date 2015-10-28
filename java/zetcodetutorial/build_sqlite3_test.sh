d71f5b829ac4eee641336813d18853e0a9175b1c
Thiago Farina <tfarina@chromium.org>
2015-10-28 13:40:00 -0200

    java: put sqlite3 test into output directory too.
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- java/zetcodetutorial/build_sqlite3_test.sh
+++ java/zetcodetutorial/build_sqlite3_test.sh
@@ -2,6 +2,6 @@
 
 # https://bitbucket.org/xerial/sqlite-jdbc#markdown-header-usage
 
-mkdir -p build/classes
-javac -cp lib/* -sourcepath com/zetcode -d build/classes com/zetcode/Sqlite3Test.java
-java -cp lib/*:build/classes com.zetcode.Sqlite3Test
+mkdir -p output/classes
+javac -cp lib/* -sourcepath com/zetcode -d output/classes com/zetcode/Sqlite3Test.java
+java -cp lib/*:output/classes com.zetcode.Sqlite3Test
