28b168a1c8272768af2f96cd7029dd2c9d5e44fd
Thiago Farina <tfarina@chromium.org>
2015-10-28 13:30:56 -0200

    java: Extract the java file into a variable.
    To make it easier to add more source files in future.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- java/zetcodetutorial/build.sh
+++ java/zetcodetutorial/build.sh
@@ -2,5 +2,7 @@
 
 # https://ant.apache.org/manual/tutorial-HelloWorldWithAnt.html
 
+javafiles="com/zetcode/HelloWorld.java"
+
 mkdir -p output/classes
-javac -sourcepath com/zetcode -d output/classes -source 1.8 -target 1.8 com/zetcode/HelloWorld.java
+javac -sourcepath com/zetcode -d output/classes -source 1.8 -target 1.8 $javafiles
