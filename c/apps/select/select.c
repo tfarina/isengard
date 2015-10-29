359c9a3a961d17f5bc4de095173f61caae41ea9c
Thiago Farina <tfarina@chromium.org>
2015-10-29 16:45:43 -0200

    select: Change the fd from being a macro to a variable.
    That way it will be easier to move this code into a function.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/select/select.c
+++ c/apps/select/select.c
@@ -8,9 +8,8 @@
 #include <sys/select.h>
 #include <sys/time.h>
 
-#define STDIN 0 /* file descriptor for standard input. */
-
 int main(void) {
+  int fd = 0; /* File descriptor for standard input (STDIN). */
   double timeout = 2500.0; /* This is in milliseconds. */
   struct timeval tv;
   fd_set readfds;
@@ -20,10 +19,10 @@ int main(void) {
   tv.tv_usec = (long)(timeout * 1000.0) % 1000000;
 
   FD_ZERO(&readfds);
-  FD_SET(STDIN, &readfds);
+  FD_SET(fd, &readfds);
 
   /* Don't care about writefds and exceptfds. */
-  rv = select(STDIN + 1, &readfds, NULL, NULL, &tv);
+  rv = select(fd + 1, &readfds, NULL, NULL, &tv);
   if (rv == 0) {
     printf("Timed out.\n");
     return -1;
@@ -31,7 +30,7 @@ int main(void) {
     /* TODO: Check errno for EAGAIN and EINTR. */
   }
 
-  if (FD_ISSET(STDIN, &readfds))
+  if (FD_ISSET(fd, &readfds))
     printf("A key was pressed.\n");
   else
     printf("Timed out.\n");
