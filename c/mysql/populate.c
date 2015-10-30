36d360e232bff54e7d65ed5529fe1c19318ebd8f
Thiago Farina <tfarina@chromium.org>
2015-10-30 17:46:32 -0200

    c/mysql/populate.c: Pass constants to mysql_real_connect().
    This makes it easier to read in future.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/mysql/populate.c
+++ c/mysql/populate.c
@@ -6,6 +6,11 @@
 #include <stdlib.h>
 #include <mysql/mysql.h>
 
+static const char kDBHost[] = "localhost";
+static const char kDBUser[] = "ken";
+static const char kDBPassword[] = "194304";
+static const char kDBName[] = "ctestdb";
+
 static void finish_with_error(MYSQL *sql) {
   fprintf(stderr, "%s\n", mysql_error(sql));
   mysql_close(sql);
@@ -14,17 +19,20 @@ static void finish_with_error(MYSQL *sql) {
 
 int main(int argc, char **argv) {
   MYSQL *sql = NULL;
+  unsigned int port = 0;
 
   if ((sql = mysql_init(NULL)) == NULL) {
     fprintf(stderr, "mysql_init() failed\n");
     exit(EXIT_FAILURE);
   }
 
-  if (mysql_real_connect(sql, "localhost", "ken", "194304", "ctestdb", 0, NULL, 0) == NULL) {
+  if (mysql_real_connect(sql, kDBHost, kDBUser, kDBPassword, kDBName, port,
+                         NULL, 0) == NULL) {
     finish_with_error(sql);
   }
 
-  if (mysql_query(sql, "INSERT INTO bookmarks VALUES(1, 'https://google.com', 'Google')")) {
+  if (mysql_query(sql,
+          "INSERT INTO bookmarks VALUES (1, 'https://google.com', 'Google')")) {
     finish_with_error(sql);
   }
 
