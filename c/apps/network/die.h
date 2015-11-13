820516b650529ca261dc2854591fff491fa6ee06
Thiago Farina <tfarina@chromium.org>
2015-11-13 20:52:28 -0200

    c/apps/network: update another copy of die() function.
    Sync it with commit 34dfc48ab443a2dc670da6d05c5c2f4a576c3cee.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/network/die.h
+++ c/apps/network/die.h
@@ -1,6 +1,14 @@
 #ifndef DIE_H_
 #define DIE_H_
 
-void die(const char *msg, ...);
+#if defined(__GNUC__)
+#define NORETURN __attribute__((__noreturn__))
+#elif defined(_MSC_VER)
+#define NORETURN __declspec(noreturn)
+#else
+#define NORETURN
+#endif
 
-#endif  // DIE_H_
+NORETURN void die(const char *msg, ...) __attribute__((format(printf, 1, 2)));
+
+#endif  /* DIE_H_ */
