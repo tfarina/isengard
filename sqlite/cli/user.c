6dd81e7ce8a312aaf5910e7da94cafdad9148f70
Thiago Farina <tfarina@chromium.org>
2015-11-17 11:32:25 -0200

    sqlite: create user table using sqlite3_stmt
    Inspired on
    http://git.gnupg.org/cgi-bin/gitweb.cgi?p=payproc.git;a=blob;f=src/preorder.c;hb=6b0b5cc632776c0a7672eb0165ba8f11bf6869b0#l249
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/user.c
+++ sqlite/cli/user.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 
 int db_user_create_table(sqlite3* db) {
+  int rv;
+  sqlite3_stmt *stmt;
   const char* sql =
     "CREATE TABLE IF NOT EXISTS 'user' ("
     "  uid INTEGER PRIMARY KEY," /* User ID */
@@ -11,8 +13,17 @@ int db_user_create_table(sqlite3* db) {
     "  email TEXT"               /* e-mail */
     ");";
 
-  if (sqlite3_exec(db, sql, NULL, NULL, NULL) != SQLITE_OK) {
+  if (sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) != SQLITE_OK) {
     fprintf(stderr, "error creating user table: %s\n", sqlite3_errmsg(db));
+    /* TODO: close db */
+    return -1;
+  }
+
+  rv = sqlite3_step(stmt);
+  sqlite3_finalize(stmt);
+  if (rv != SQLITE_DONE) {
+    fprintf(stderr, "error creating user table: %s\n", sqlite3_errstr(rv));
+    /* TODO: close db */
     return -1;
   }
 
