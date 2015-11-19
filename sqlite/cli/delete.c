5037336d1aa612a10416c3aaec8ba170c4d2a5c6
Thiago Farina <tfarina@chromium.org>
2015-11-19 17:00:09 -0200

    sqlite: sync up delete.c with the rest of the cli tools
    - better error message
    - check the return value of sqlite3_step
    - * with the variable names
    - rename db_user_remove to delete_user_record
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- sqlite/cli/delete.c
+++ sqlite/cli/delete.c
@@ -9,25 +9,33 @@
 /* The name of the user database file.  */
 static const char user_db_fname[] = "users.db";
 
-static int db_user_remove(sqlite3* db, const char* username) {
+static int delete_user_record(sqlite3 *db, const char *username) {
   sqlite3_stmt *stmt;
 
   const char *sql = "DELETE FROM user WHERE login=?1;";
 
   if (sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) != SQLITE_OK) {
-    fprintf(stderr, "SQLite error: %s\n", sqlite3_errmsg(db));
+    fprintf(stderr, "error preparing delete statement: %s\n",
+            sqlite3_errmsg(db));
     return -1;
   }
 
   sqlite3_bind_text(stmt, 1, username, -1, SQLITE_STATIC);
-  sqlite3_step(stmt);
+
+  if (sqlite3_step(stmt) != SQLITE_DONE) {
+    fprintf(stderr, "error deleting user from table: %s\n",
+            sqlite3_errmsg(db));
+    return -1;
+  }
+
   sqlite3_finalize(stmt);
+  stmt = NULL;
 
   return 0;
 }
 
 int main(int argc, char **argv) {
-  sqlite3* db;
+  sqlite3 *db;
 
   if (argc != 2) {
     fprintf(stderr, "usage: %s USERNAME\n", argv[0]);
@@ -51,7 +59,7 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  if (db_user_remove(db, argv[1])) {
+  if (delete_user_record(db, argv[1])) {
     sqlite3_close(db);
     return -1;
   }
