7b679900bc63d631941eddcb051eae744ab972a3
Thiago Farina <tfarina@chromium.org>
2015-11-21 10:24:04 -0200

    lecker: use only GRANT to create a user on MySQL
    When creating a user, using 'create user ...', it will fail if the user already
    exists:
    
    Error Code: 1396. Operation CREATE USER failed for 'hannibal'@'localhost'
    
    GRANT will create the user if the account does not already exist.
    
    GRANT seems to be the most elegant way to create a user on MySQL.
    
    http://www.cryer.co.uk/brian/mysql/how_to_create_user_if_not_exist.htm
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- python/webpy/lecker/setup_db.sql
+++ python/webpy/lecker/setup_db.sql
@@ -1,6 +1,5 @@
 # SQL for setting up the database and user.
 
 CREATE DATABASE IF NOT EXISTS lecker;
-CREATE USER 'hannibal'@'localhost' IDENTIFIED BY '19440807';
-GRANT ALL ON lecker.* TO 'hannibal'@'localhost';
+GRANT ALL ON lecker.* TO 'hannibal'@'localhost' IDENTIFIED BY '19440807';
 FLUSH PRIVILEGES;
