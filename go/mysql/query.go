426ef5282c92bc78e8728b69dc1661379cfb3b2e
Thiago Farina <tfarina@chromium.org>
2015-11-28 16:54:43 -0200

    go/mysql: move the first parameter of sql.Open into a constant
    This way the code becomes more generic.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- go/mysql/query.go
+++ go/mysql/query.go
@@ -8,11 +8,16 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+const (
+	// Default database driver.
+	DEFAULT_DRIVER = "mysql"
+)
+
 //
 // http://go-database-sql.org/retrieving.html
 //
 func main() {
-	db, err := sql.Open("mysql", "nash:virginia@tcp(127.0.0.1:3306)/go_tutorial")
+	db, err := sql.Open(DEFAULT_DRIVER, "nash:virginia@tcp(127.0.0.1:3306)/go_tutorial")
 	if err != nil {
 		log.Fatal(err)
 	}
