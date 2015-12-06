1cc2bf204591e69eadb2d25768501d63c8673ece
Thiago Farina <tfarina@chromium.org>
2015-12-06 12:29:15 -0200

    goapp: do not hard-code the port the server will be listening on
    Logic taken from
    https://github.com/metromoxie/personal-website/blob/db201d106cd81693742050462ee9d81e564a73ac/server.go
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/goapp/server.go
+++ website/goapp/server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +13,19 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	http_port_int := flag.Int("http-port", 8001, "The HTTP port to listen on.")
+
+	flag.Parse()
+
+	if *http_port_int < 1 || *http_port_int > 65535 {
+		fmt.Println("Port numbers must be between 1 and 65535, includesive.")
+		return
+	}
+
+	http_port := strconv.Itoa(*http_port_int)
+
+	fmt.Println("Listening on port " + http_port)
+
 	http.HandleFunc("/", rootHandler)
-	log.Fatal(http.ListenAndServe("0.0.0.0:8001", nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+http_port, nil))
 }
