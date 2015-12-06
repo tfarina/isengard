48932014ac0f4296ec024c920acd2729f287266a
Thiago Farina <tfarina@chromium.org>
2015-12-06 10:28:00 -0200

    version.php: rename DB_SERVER to DB_HOST
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- php/zetcodetutorial/mysql/version.php
+++ php/zetcodetutorial/mysql/version.php
@@ -1,12 +1,12 @@
 <?php
 
-define("DB_SERVER", "localhost");
+define("DB_HOST", "localhost");
 define("DB_USERNAME", "justin");
 define("DB_PASSWORD", "46656");
 
-$link = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+$link = mysql_connect(DB_HOST, DB_USERNAME, DB_PASSWORD);
 if (!$link) {
-    echo "Could not connect to server\n";
+    echo "Failed to connect to mysql server.\n";
     trigger_error(mysql_error(), E_USER_ERROR);
 } else {
     echo "Connection established\n";
