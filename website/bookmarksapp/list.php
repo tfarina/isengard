de26959d058aa2602cfa91489704d71af8cad063
Thiago Farina <tfarina@chromium.org>
2015-12-07 22:12:12 +0000

    bookmarks: separate mysql code from layout code
    keep database access separate from the rendering code.
    
    This will make it easier to use a template system like Smarty in
    the future.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/list.php
+++ website/bookmarksapp/list.php
@@ -2,6 +2,12 @@
 
 include('config.php');
 
+$result = mysql_query("SELECT * FROM bookmarks ORDER BY dt DESC") or trigger_error(mysql_error());
+
+while ($row = mysql_fetch_assoc($result)) {
+  $bookmarks[] = $row;
+}
+
 include_once("header.php");
 
 echo "<p><a href=new.php>New Bookmark</a></p>\n";
@@ -18,14 +24,12 @@ echo "</tr>\n";
 echo "</thead>\n";
 echo "<tbody>\n";
 
-$result = mysql_query("SELECT * FROM bookmarks ORDER BY dt DESC") or trigger_error(mysql_error());
-
-while ($row = mysql_fetch_assoc($result)) {
+foreach ($bookmarks as $bookmark) {
   echo "<tr>\n";
-  echo "<td valign='top'><a href=". $row['url'] .">" . $row['url'] ."</a></td>\n";
-  echo "<td valign='top'>" . $row['title'] . "</td>\n";
-  echo "<td valign='top'>" . $row['dt'] . "</td>\n";
-  echo "<td valign='top'><a href=edit.php?id={$row['id']}>Edit</a></td><td><a href=delete.php?id={$row['id']}>Delete</a></td>\n";
+  echo "<td valign='top'><a href=". $bookmark['url'] .">" . $bookmark['url'] ."</a></td>\n";
+  echo "<td valign='top'>" . $bookmark['title'] . "</td>\n";
+  echo "<td valign='top'>" . $bookmark['dt'] . "</td>\n";
+  echo "<td valign='top'><a href=edit.php?id={$bookmark['id']}>Edit</a></td><td><a href=delete.php?id={$bookmark['id']}>Delete</a></td>\n";
   echo "</tr>\n";
 }
 
