648bd58d26f267b6811b9035cce8c2e6d312a14f
Thiago Farina <tfarina@chromium.org>
2015-12-12 23:56:54 +0000

    bookmarksapp: better mysql error messages
    Also rename DB_SERVER to DB_HOST.
    
    https://github.com/paypal/rest-api-sample-app-php/blob/81d0f16f8ef04af26d741925d22750a30b848f2f/app/common/db.php
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/config.php
+++ website/bookmarksapp/config.php
@@ -1,17 +1,17 @@
 <?php
 
-define("DB_SERVER", "localhost");
+define("DB_HOST", "localhost");
 define("DB_USERNAME", "seafowl");
 define("DB_PASSWORD", "2Mnc7nXm");
 define("DB_NAME", "bkms");
 
-$link = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+$link = mysql_connect(DB_HOST, DB_USERNAME, DB_PASSWORD);
 if (!$link) {
-  die('Not connected : ' . mysql_error());
+  die('Could not connect to mysql: ' . mysql_error());
 }
 
-if (!mysql_select_db(DB_NAME)) {
-  die ('Could not select' . DB_NAME . ' : ' . mysql_error());
+if (!mysql_select_db(DB_NAME, $link)) {
+  die ('Could not select database ' . DB_NAME . ': ' . mysql_error());
 }
 
 ?>
