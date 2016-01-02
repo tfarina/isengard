3e0a44394ceafc9b2e58c34423c0fcef4627d37c
Thiago Farina <tfarina@chromium.org>
2016-01-02 21:21:11 +0000

    bookmarksapp: capitalize some things in schema.sql
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/schema.sql
+++ website/bookmarksapp/schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `bookmarks` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `hash` varchar(32) collate utf8_unicode_ci NOT NULL default '',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `hash` varchar(32) collate utf8_unicode_ci NOT NULL DEFAULT '',
   `url` text collate utf8_unicode_ci NOT NULL,
   `title` text collate utf8_unicode_ci NOT NULL,
-  `dt` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `hash` (`hash`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
