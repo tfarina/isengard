3fa1942049ffaaba9cdcb16022d5e80d95eae181
Thiago Farina <tfarina@chromium.org>
2016-01-05 18:23:31 +0000

    bookmarksapp: do not indent head and body tags
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/header.php
+++ website/bookmarksapp/header.php
@@ -1,8 +1,10 @@
 <!doctype html>
 <html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8;charset=utf-8" />
-    <title>Bookmark manager</title>
-    <link rel="stylesheet" href="main.css" type="text/css">
-  </head>
-  <body>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8;charset=utf-8" />
+  <title>Bookmark manager</title>
+  <link rel="stylesheet" href="main.css" type="text/css">
+</head>
+<body>
+  <h2>Bookmarks</h2>
+  <p><a href="list.php">My bookmarks</a> | <a href='new.php'>Add bookmark</a></p>
