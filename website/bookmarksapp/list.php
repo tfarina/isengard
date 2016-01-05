28b41081c3fadd5f6be9076f53a9e44bd491302c
Thiago Farina <tfarina@chromium.org>
2016-01-05 18:26:32 +0000

    bookmarksapp: indent table in list.php two spaces
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/list.php
+++ website/bookmarksapp/list.php
@@ -14,30 +14,28 @@ mysql_free_result($result);
 
 include_once("header.php");
 ?>
-<h2>Bookmarks</h2>
-<p><a href='new.php'>Create a new bookmark</a></p>
-<table class="list">
-  <thead>
-    <tr>
-      <th>URL</th>
-      <th>Title</th>
-      <th>Date Created</th>
-      <th>Actions</th>
-    </tr>
-  </thead>
-  <tbody>
+  <table class="list">
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>URL</th>
+        <th>Date Created</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
     <?php foreach ($bookmarks as $bookmark) { ?>
-    <tr>
-      <td><a href="<?php echo $bookmark['url']; ?>"><?php echo $bookmark['url']; ?></a></td>
-      <td><?php echo $bookmark['title']; ?></td>
-      <td><?php echo $bookmark['dt']; ?></td>
-      <td>
-        <a href="edit.php?id=<?php echo $bookmark['id']; ?>">Edit</a>  <a href="delete.php?id=<?php echo $bookmark['id']; ?>">Delete</a>
-      </td>
-    </tr>
+      <tr>
+        <td><a href="<?php echo $bookmark['url']; ?>"><?php echo $bookmark['title']; ?></a></td>
+        <td><?php echo $bookmark['url']; ?></td>
+        <td><?php echo $bookmark['dt']; ?></td>
+        <td>
+          <a href="edit.php?id=<?php echo $bookmark['id']; ?>">Edit</a>  <a href="delete.php?id=<?php echo $bookmark['id']; ?>">Delete</a>
+        </td>
+      </tr>
     <?php } ?>
-  </tbody>
-</table>
+    </tbody>
+  </table>
 <?php
 include_once("footer.php");
 ?>
