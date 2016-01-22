a57475897489edd74902da3ff499e6b13c97ce51
Thiago Farina <tfarina@chromium.org>
2016-01-22 09:26:56 -0200

    timeserver.c: rename some variables to make them easier to read
    In special opt -> reuse.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/network/timeserver.c
+++ c/apps/network/timeserver.c
@@ -42,27 +42,27 @@ static void reapchld(int sig) {
   while (waitpid(-1, NULL, WNOHANG) > 0);
 }
 
-int main() {
+int main(void) {
   struct sockaddr_in servaddr;
-  int listen_fd;
+  int sockfd;
   int client_fd;
-  int opt = 1;
+  int reuse = 1;
 
   memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
   servaddr.sin_port = htons(SERVER_PORT);
 
-  if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     die("cannot create socket");
 
-  if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) == -1)
     die("setsockopt failed");
 
-  if (bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1)
+  if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1)
     die("bind failed");
 
-  if (listen(listen_fd, BACKLOG) == -1)
+  if (listen(sockfd, BACKLOG) == -1)
     die("listen failed");
 
   fprintf(stderr,
@@ -76,7 +76,7 @@ int main() {
   sigaction(SIGCHLD, &act, 0);
 
   while (1) {
-    if ((client_fd = accept(listen_fd, NULL, NULL)) == -1) {
+    if ((client_fd = accept(sockfd, NULL, NULL)) == -1) {
       if (errno == EINTR) /* EINTR might happen on accept. */
         continue;
       die("accept failed");
@@ -84,12 +84,12 @@ int main() {
 
     switch (fork()) {
       case -1:
-        close(listen_fd);
+        close(sockfd);
         close(client_fd);
         die("fork failed");
 
       case 0:
-        close(listen_fd);
+        close(sockfd);
         doprocessing(client_fd);
 
       default:
