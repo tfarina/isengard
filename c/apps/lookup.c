a81d212a597a443c07d2c7ff7536b694b34f2c49
Thiago Farina <tfarina@chromium.org>
2016-01-25 18:44:03 -0200

    lookup.c: add more log severities
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/lookup.c
+++ c/apps/lookup.c
@@ -7,8 +7,10 @@
 #include <stdio.h>
 
 enum {
-        LOG_WARNING = 0,
-        LOG_INFO = 1,
+        LOG_INFO = 0,
+        LOG_WARNING = 1,
+        LOG_ERROR = 2,
+        LOG_FATAL = 3,
 };
 
 struct priority_info {
@@ -17,8 +19,10 @@ struct priority_info {
 };
 
 static struct priority_info log_priority_info_table[] = {
-        { LOG_WARNING, "warning" },
         { LOG_INFO, "info" },
+        { LOG_WARNING, "warning" },
+        { LOG_ERROR, "error" },
+        { LOG_FATAL, "fatal" },
         { 0, NULL }
 };
 
@@ -38,7 +42,7 @@ get_priority_info_by_id(struct priority_info table[], int id)
 int main(void) {
         struct priority_info *pinfo;
 
-        pinfo = get_priority_info_by_id(log_priority_info_table, LOG_INFO);
+        pinfo = get_priority_info_by_id(log_priority_info_table, LOG_FATAL);
 
         if (pinfo) {
                 printf("%s\n", pinfo->name);
