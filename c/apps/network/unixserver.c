2e745f54f4f85c397c25c405728ecc5826e95501
Thiago Farina <tfarina@chromium.org>
2016-03-05 17:32:44 -0300

    unixserver.c: add a constant for the size of sun_path
    Again, start simple, and let it be unused for now.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/network/unixserver.c
+++ c/apps/network/unixserver.c
@@ -1,7 +1,10 @@
+#include <stddef.h>
 #include <sys/un.h>
 
 int main(void) {
         struct sockaddr_un unix_addr;
+
+        static const size_t kPathMax = sizeof(unix_addr.sun_path);
 
         return 0;
 }
