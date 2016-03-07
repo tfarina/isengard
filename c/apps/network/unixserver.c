e0cb11ce366d9afcbd630a609e019845c6415aaf
Thiago Farina <tfarina@chromium.org>
2016-03-07 17:59:36 -0300

    unixserver.c: set the memory for |unix_addr|
    While at it set the properly value for sun_family field, which is
    AF_UNIX. Other variants are: AF_LOCAL and PF_UNIX.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/network/unixserver.c
+++ c/apps/network/unixserver.c
@@ -1,10 +1,15 @@
 #include <stddef.h>
+#include <sys/socket.h>
 #include <sys/un.h>
+#include <string.h>
 
 int main(void) {
         struct sockaddr_un unix_addr;
 
         static const size_t kPathMax = sizeof(unix_addr.sun_path);
+
+        memset(&unix_addr, 0, sizeof(unix_addr));
+        unix_addr.sun_family = AF_UNIX;
 
         return 0;
 }
