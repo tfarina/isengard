127e6164cded9b12059f50187eb88fcb4fffd536
Thiago Farina <tfarina@chromium.org>
2016-03-10 15:38:59 -0300

    unixserver.c: make the socket that will be passed to bind()
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/network/unixserver.c
+++ c/apps/network/unixserver.c
@@ -1,10 +1,19 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <string.h>
 
 int main(void) {
+        int socket_fd;
         struct sockaddr_un unix_addr;
+
+        socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+        if (socket_fd == -1) {
+                fprintf(stderr, "failed to create AF_UNIX socket\n");
+                exit(EXIT_FAILURE);
+        }
 
         static const size_t kPathMax = sizeof(unix_addr.sun_path);
 
