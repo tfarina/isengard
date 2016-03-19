d361bed4f65809e3f5a6075fb616b9850e251b02
Thiago Farina <tfarina@chromium.org>
2016-03-19 17:40:40 -0300

    java: use 'src/main/java' as the sourcepath
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- java/tutorial/zetcode/build.sh
+++ java/tutorial/zetcode/build.sh
@@ -3,13 +3,22 @@
 # https://ant.apache.org/manual/tutorial-HelloWorldWithAnt.html
 
 javafiles="src/com/zetcode/HelloWorld.java"
+javafiles_new="src/main/java/com/zetcode/HelloWorld.java"
 
 JAVA_VERSION=1.8
 
 mkdir -p output/classes
+
 javac -sourcepath src \
       -d output/classes\
       -source ${JAVA_VERSION} \
       -target ${JAVA_VERSION} \
       -encoding UTF-8 \
       $javafiles
+
+javac -sourcepath src/main/java \
+      -d output/classes\
+      -source ${JAVA_VERSION} \
+      -target ${JAVA_VERSION} \
+      -encoding UTF-8 \
+      $javafiles_new
