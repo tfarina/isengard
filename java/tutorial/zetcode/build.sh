456a5c9b276bb4be0f7f26a8a0ccbe372b6bf205
Thiago Farina <tfarina@chromium.org>
2016-03-19 17:51:19 -0300

    java: refactor the path to output/classes into a constant
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- java/tutorial/zetcode/build.sh
+++ java/tutorial/zetcode/build.sh
@@ -6,18 +6,19 @@ javafiles="src/com/zetcode/HelloWorld.java"
 javafiles_new="src/main/java/com/zetcode/HelloWorld.java"
 
 JAVA_VERSION=1.8
+OUTPUT_CLASSES_DIR=output/classes
 
-mkdir -p output/classes
+mkdir -p ${OUTPUT_CLASSES_DIR}
 
 javac -sourcepath src \
-      -d output/classes\
+      -d ${OUTPUT_CLASSES_DIR} \
       -source ${JAVA_VERSION} \
       -target ${JAVA_VERSION} \
       -encoding UTF-8 \
       $javafiles
 
 javac -sourcepath src/main/java \
-      -d output/classes\
+      -d ${OUTPUT_CLASSES_DIR} \
       -source ${JAVA_VERSION} \
       -target ${JAVA_VERSION} \
       -encoding UTF-8 \
