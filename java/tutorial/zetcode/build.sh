3c2fe82546ce43cd95d2cdcebc70c6460d3bc84a
Thiago Farina <tfarina@chromium.org>
2016-03-23 17:54:14 -0300

    java: pass the files to build to javac through a param file
    That way we don't have to list one-by-one.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- java/tutorial/zetcode/build.sh
+++ java/tutorial/zetcode/build.sh
@@ -3,14 +3,15 @@
 # https://ant.apache.org/manual/tutorial-HelloWorldWithAnt.html
 
 javafiles="src/com/zetcode/HelloWorld.java"
-javafiles_new="src/main/java/com/zetcode/HelloWorld.java"
 
 JAVA_VERSION=1.8
 OUTPUT_CLASSES_DIR=output/classes
 
+PARAMFILE=paramfile
+
 mkdir -p ${OUTPUT_CLASSES_DIR}
 
-find src/main/java -name "*.java" > paramfile
+find src/main/java -name "*.java" > $PARAMFILE
 
 javac -sourcepath src \
       -d ${OUTPUT_CLASSES_DIR} \
@@ -24,4 +25,4 @@ javac -sourcepath src/main/java \
       -source ${JAVA_VERSION} \
       -target ${JAVA_VERSION} \
       -encoding UTF-8 \
-      $javafiles_new
+      "@${PARAMFILE}"
