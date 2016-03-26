46b80eb85fb7b6c780aca9a31b22f71e5a51401b
Thiago Farina <tfarina@chromium.org>
2016-03-26 17:12:10 -0300

    java: remove the old javac command
    It was the first and compiles a single file. That does not scale
    well.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- java/tutorial/zetcode/build.sh
+++ java/tutorial/zetcode/build.sh
@@ -2,23 +2,13 @@
 
 # https://ant.apache.org/manual/tutorial-HelloWorldWithAnt.html
 
-javafiles="src/com/zetcode/HelloWorld.java"
-
-JAVA_VERSION=1.8
 OUTPUT_CLASSES_DIR=output/classes
-
+JAVA_VERSION=1.8
 PARAMFILE=paramfile
 
 mkdir -p ${OUTPUT_CLASSES_DIR}
 
 find src/main/java -name "*.java" > $PARAMFILE
-
-javac -sourcepath src \
-      -d ${OUTPUT_CLASSES_DIR} \
-      -source ${JAVA_VERSION} \
-      -target ${JAVA_VERSION} \
-      -encoding UTF-8 \
-      $javafiles
 
 javac -sourcepath src/main/java \
       -d ${OUTPUT_CLASSES_DIR} \
