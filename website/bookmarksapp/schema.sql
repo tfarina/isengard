99dfbb99a26b3a8892daabb61c99b0ae06ad1819
Thiago Farina <tfarina@chromium.org>
2016-04-25 08:07:59 +0000

    bookmarksapp: capitalize "unsigned" keyword
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/schema.sql
+++ website/bookmarksapp/schema.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `bookmarks` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `hash` varchar(32) collate utf8_unicode_ci NOT NULL DEFAULT '',
   `url` text collate utf8_unicode_ci NOT NULL,
   `title` text collate utf8_unicode_ci NOT NULL,
   `dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
   PRIMARY KEY  (`id`),
   UNIQUE KEY `hash` (`hash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
