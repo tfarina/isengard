f049824dd6ccabe94154bfc8d3c72188a51931d4
Thiago Farina <tfarina@chromium.org>
2016-04-27 19:07:34 +0000

    bookmarksapp: some changes to header
    * Rename main.css to bookmarksapp.css
    * Complete the link tag
    * Add header and nav divs
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/header.php
+++ website/bookmarksapp/header.php
@@ -2,9 +2,13 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8;charset=utf-8" />
-  <title>Bookmark manager</title>
-  <link rel="stylesheet" href="main.css" type="text/css">
+  <title>Bookmarksapp - Online bookmark manager</title>
+  <link rel="stylesheet" type="text/css" title"Default style" href="bookmarksapp.css">
 </head>
 <body>
-  <h2>Bookmarks</h2>
-  <p><a href="list.php">My bookmarks</a> | <a href="new.php">Add bookmark</a></p>
+  <div class="header">
+    <h2>Bookmarks</h2>
+  </div>
+  <div class="nav">
+    <p><a href="list.php">My bookmarks</a> | <a href="new.php">Add bookmark</a></p>
+  </div>
