a91c036d097ea382092acfdea773feae38074e81
Thiago Farina <tfarina@chromium.org>
2016-05-25 11:02:26 +0000

    bookmarksapp: split pure css into its own file
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/header.php
+++ website/bookmarksapp/header.php
@@ -4,6 +4,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8;charset=utf-8" />
   <title>Bookmarksapp - Online bookmark manager</title>
   <link rel="stylesheet" type="text/css" title"Default style" href="bookmarksapp.css">
+  <link rel="stylesheet" type="text/css" title"Default style" href="pure.css">
 </head>
 <body>
   <div class="header">
