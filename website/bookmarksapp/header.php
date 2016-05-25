1cd1848d9948493cca60aaa827f9f568358083fa
Thiago Farina <tfarina@chromium.org>
2016-05-25 12:45:07 +0000

    bookmarksapp: rename pure.css to pure-0.6.0-tables-min.css
    this is in preparation for adding the buttons css file,
    that will be used to style the button in the new.php form.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/header.php
+++ website/bookmarksapp/header.php
@@ -4,7 +4,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8;charset=utf-8" />
   <title>Bookmarksapp - Online bookmark manager</title>
   <link rel="stylesheet" type="text/css" title"Default style" href="bookmarksapp.css">
-  <link rel="stylesheet" type="text/css" title"Default style" href="pure.css">
+  <link rel="stylesheet" type="text/css" title"Default style" href="pure-0.6.0-tables-min.css">
 </head>
 <body>
   <div class="header">
