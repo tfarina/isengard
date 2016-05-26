fedb19271042a09450c21a38181661e952e5a05d
Thiago Farina <tfarina@chromium.org>
2016-05-26 09:15:54 +0000

    bookmarksapp: make delete.php redirect to the list page
    That seems to be a better experience.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/delete.php
+++ website/bookmarksapp/delete.php
@@ -1,25 +1,9 @@
 <?php
 
-include('config.php');
+require_once("config.php");
 
 $query = "DELETE FROM bookmarks WHERE id='" . $_GET['id'] . "'";
 mysql_query($query) or die(mysql_error());
 
-if (mysql_affected_rows()) {
-  $message = "The bookmark has been deleted successfully.\n";
-} else {
-  $message = "Nothing deleted.\n";
-}
-
-?>
-<?php
-include_once("header.php");
-?>
-<?php if (isset($message)) { ?>
-  <div class="message">
-    <?php echo $message; ?>
-  </div>
-<?php } ?>
-<?php
-include_once("footer.php");
+header("Location: list.php");
 ?>
