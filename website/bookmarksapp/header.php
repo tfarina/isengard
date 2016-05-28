c19e2061586268b928d77f25813482d284b2df99
Thiago Farina <tfarina@chromium.org>
2016-05-28 15:04:51 +0000

    bookmarksapp: check-in pure buttons css file
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/header.php
+++ website/bookmarksapp/header.php
@@ -4,8 +4,9 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8;charset=utf-8" />
   <title>Bookmarksapp - Online bookmark manager</title>
   <link rel="stylesheet" type="text/css" title"Default style" href="bookmarksapp.css">
-  <link rel="stylesheet" type="text/css" title"Default style" href="pure-0.6.0-tables-min.css">
-  <link rel="stylesheet" type="text/css" title"Default style" href="http://yui.yahooapis.com/pure/0.6.0/buttons-min.css">
+  <link rel="stylesheet" type="text/css" href="pure-0.6.0-tables-min.css">
+  <link rel="stylesheet" type="text/css" href="pure-0.6.0-buttons-min.css">
+  <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
 </head>
 <body>
   <div class="header">
