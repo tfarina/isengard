1ca7a35acb8adaaac1bb2d066cbd4b61f95df935
Thiago Farina <tfarina@chromium.org>
2016-06-16 15:08:09 +0000

    bookmarksapp: remove the two other forms from new.php page
    Let's go with the bootstrap form like we are do in edit.php page.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/new.php
+++ website/bookmarksapp/new.php
@@ -38,53 +38,7 @@ if (!empty($_POST) && isset($_POST['submit'])) {
 <?php
 include_once("header.php");
 ?>
-  <?php if (isset($message)) { ?>
-  <div class="message">
-    <?php echo $message; ?>
-  </div>
-  <?php } ?>
   <h3>Create New Bookmark</h3>
-  <form name="newBookmark" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <table>
-      <tbody>
-      <tr>
-        <td align="right">Title:</td>
-        <td><input type="text" name="title"/></td>
-      </tr>
-      <tr>
-        <td align="right">URL:</td>
-        <td><input type="text" name="url"/></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td style="float: right;">
-          <a class="btn btn-default" href="list.php">Cancel</a>
-          <button type="submit" name="submit" class="btn btn-primary">Create</button>
-        </td>
-      </tr>
-      </tbody>
-    </table>
-  </form>
-<div class="container">
-  <div class="col-sm-offset-9 col-sm-8">
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      Create new bookmark
-    </div>
-    <div class="panel-body">
-      <form action="" method="post" class="form-horizontal">
-        <div class="form-group">
-          <label for="bookmark-title" class="col-sm-3 control-label">Title</label>
-          <div class="col-sm-6">
-            <input type="text" name="title" id="bookmark-title" class="form-control">
-          </div>
-        </div>
-      </form>
-    </div> 
-  </div>        
-</div> 
-</div>        
-
   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="form-horizontal">
     <div class="form-group <?php echo !empty($titleError) ? 'has-error' : ''; ?>">
       <label for="bookmark-title" class="col-sm-2 control-label">Title</label>
