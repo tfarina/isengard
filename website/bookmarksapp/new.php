84dfcc89f47e3a490af5a04c0ff6e4e1702f7bef
Thiago Farina <tfarina@chromium.org>
2016-06-16 15:26:29 +0000

    bookmarksapp: add sidebar to new.php
    This makes it match the layout of the edit.php page.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/new.php
+++ website/bookmarksapp/new.php
@@ -50,37 +50,50 @@ include_once("header.php");
       </div>
     </div>
   </nav>
-  <h3>Create New Bookmark</h3>
-  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="form-horizontal">
-    <div class="form-group <?php echo !empty($titleError) ? 'has-error' : ''; ?>">
-      <label for="bookmark-title" class="col-sm-2 control-label">Title</label>
-      <div class="col-sm-4">
-        <input type="text" name="title" id="bookmark-title" class="form-control" />
-        <?php if (!empty($titleError)) { ?>
-        <span class="help-block"><?php echo $titleError; ?></span>
-        <?php } ?>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-3 col-md-2 sidebar">
+        <ul class="nav nav-sidebar">
+          <li class="active">
+            <a href="list.php">My bookmarks</a>
+          </li>
+        </ul>
       </div>
-    </div>
+      <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <h3>Create New Bookmark</h3>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="form-horizontal">
+          <div class="form-group <?php echo !empty($titleError) ? 'has-error' : ''; ?>">
+            <label for="bookmark-title" class="col-sm-2 control-label">Title</label>
+            <div class="col-sm-4">
+              <input type="text" name="title" id="bookmark-title" class="form-control" />
+              <?php if (!empty($titleError)) { ?>
+              <span class="help-block"><?php echo $titleError; ?></span>
+              <?php } ?>
+            </div>
+          </div>
 
-    <div class="form-group <?php echo !empty($urlError) ? 'has-error' : ''; ?>">
-      <label for="bookmark-url" class="col-sm-2 control-label">URL</label>
-      <div class="col-sm-4">
-        <input type="url" name="url" id="bookmark-url" class="form-control" />
-        <?php if (!empty($urlError)) { ?>
-        <span class="help-block"><?php echo $urlError; ?></span>
-        <?php } ?>
-      </div>
-    </div>
+          <div class="form-group <?php echo !empty($urlError) ? 'has-error' : ''; ?>">
+            <label for="bookmark-url" class="col-sm-2 control-label">URL</label>
+            <div class="col-sm-4">
+              <input type="url" name="url" id="bookmark-url" class="form-control" />
+              <?php if (!empty($urlError)) { ?>
+              <span class="help-block"><?php echo $urlError; ?></span>
+              <?php } ?>
+            </div>
+          </div>
 
-    <div class="form-group">
-      <div class="col-sm-offset-2 col-sm-4">
-        <div class="pull-right">
-          <a class="btn btn-default" href="list.php">Cancel</a>
-          <button type="submit" name="submit" class="btn btn-primary">Create</button>
-        </div>
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-4">
+              <div class="pull-right">
+                <a class="btn btn-default" href="list.php">Cancel</a>
+                <button type="submit" name="submit" class="btn btn-primary">Create</button>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
-  </form>
+  </div>
 <?php
 include_once("footer.php");
 ?>
