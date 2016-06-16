db921f52344cc5885a18a681c8052d81678dfcaf
Thiago Farina <tfarina@chromium.org>
2016-06-16 21:50:20 +0000

    bookmarksapp: do some work on $_GET['id'] in delete.php page
    This is in preparation for asking the user if he really wants to delete
    the bookmark before actually deleting it.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/delete.php
+++ website/bookmarksapp/delete.php
@@ -2,8 +2,21 @@
 
 require_once 'config.php';
 
-$query = "DELETE FROM bookmarks WHERE id='" . $_GET['id'] . "'";
+$id = -1;
+
+if (!empty($_GET['id'])) {
+  $id = $_GET['id'];
+}
+
+if ($id == -1) {
+  header('Location: list.php');
+}
+
+$query = "DELETE FROM bookmarks WHERE id='" . $id . "'";
 mysql_query($query) or die(mysql_error());
 
+$message = "Bookmark has been deleted successfully.";
+$_SESSION['msg-success'] = $message;
 header("Location: list.php");
+exit;
 ?>
