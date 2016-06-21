4fcaa80d4e3a8a71eabb517ab08a75841c88ee22
Thiago Farina <tfarina@chromium.org>
2016-06-21 12:58:21 +0000

    bookmarksapp: add a simple footer to account page
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/account.php
+++ website/bookmarksapp/account.php
@@ -57,6 +57,12 @@ include_once("header.php");
     </div>
   </div>
 </div>
+
+<footer class="footer">
+  <div class="container">
+    <p class="text-muted">&copy; 2016 Bookmarks</p>
+  </div>
+</footer>
 <?php
 include_once("footer.php");
 ?>
