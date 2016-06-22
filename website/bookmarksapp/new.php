231303e6bfaa5c61c807e5bd627185145e779b38
Thiago Farina <tfarina@chromium.org>
2016-06-22 13:37:46 +0000

    bookmarksapp: sync the user menu code with the one in list.php
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/new.php
+++ website/bookmarksapp/new.php
@@ -48,7 +48,17 @@ include_once("header.php");
       </div>
       <div id="navbar" class="navbar-collapse collapse">
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="account.php">My Account</a></li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+              <i class="fa fa-user fa-fw"></i>
+              <i class="fa fa-caret-down"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-user">
+              <li><a href="#"><i class="fa fa-gear fa-fw"></i> My Account</a></li>
+              <li class="divider"></li>
+              <li><a href="#"><i class="fa fa-sign-out fa-fw"></i> Log Out</a></li>
+            </ul>
+          </li>
         </ul>
       </div>
     </div>
