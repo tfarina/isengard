3cc76fd7131bd01e0d10631303250b0bca02ac8c
Thiago Farina <tfarina@chromium.org>
2016-06-22 17:26:13 +0000

    bookmarksapp: add button menu to navbar to handle mobile device accesses
    It adds a nice menu button with three horizontal lines like the Google
    Chrome app menu.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/new.php
+++ website/bookmarksapp/new.php
@@ -44,6 +44,13 @@ include_once("header.php");
   <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
       <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
         <a class="navbar-brand" href="#">Bookmarks</a>
       </div>
       <div id="navbar" class="navbar-collapse collapse">
