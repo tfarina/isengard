02298ff09f9e39619908672277897b7c92e4848c
Thiago Farina <tfarina@chromium.org>
2016-06-23 12:00:36 +0000

    bookmarksapp: big directory cleanup reorganization
    To make things more sane and easier to deal with.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- website/bookmarksapp/footer.php
+++ website/bookmarksapp/footer.php
@@ -3,6 +3,6 @@
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-<script src="bootstrap.min.js"></script>
+<script src="res/js/bootstrap.min.js"></script>
 </body>
 </html>
--- website/bookmarksapp/header.php
+++ website/bookmarksapp/header.php
@@ -1,18 +1,23 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8;charset=utf-8" />
-
   <title>Bookmarksapp - Online bookmark manager</title>
 
-  <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="pure-0.6.0-tables-min.css">
-  <link rel="stylesheet" type="text/css" href="pure-0.6.0-buttons-min.css">
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8;charset=utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta name="description" content="Bookmarks provides bookmark manager services that make your life easier">
+
+  <link rel="author" href="/humans.txt" />
+  <link rel="shortcut icon" href="/favicon.png" />
+
+  <link rel="stylesheet" type="text/css" href="res/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="res/css/font-awesome-4.6.3/css/font-awesome.min.css">
 
   <!-- Custom styles for this layout. -->
-  <link rel="stylesheet" type="text/css" href="dashboard.css">
-  <link rel="stylesheet" type="text/css" href="login.css">
-  <!--<link rel="stylesheet" type="text/css" title"Default style" href="bookmarksapp.css">-->
+  <link rel="stylesheet" type="text/css" href="res/css/dashboard.css">
+  <link rel="stylesheet" type="text/css" href="res/css/login.css">
 </head>
 <body>
