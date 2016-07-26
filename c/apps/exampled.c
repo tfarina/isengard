cef3a98fe25bc75f4a39848fb427355f31c6122e
Thiago Farina <tfarina@chromium.org>
2016-07-26 16:38:46 -0300

    add an example of how to make a daemon process
    Reference: http://man7.org/linux/man-pages/man3/daemon.3.html
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ c/apps/exampled.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/syslog.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+  if (daemon(0, 0) == -1) {
+    fprintf(stderr, "%s: unable to daemonize: %s\n", argv[0], strerror(errno)); 
+    exit(EXIT_FAILURE);
+  }
+
+  openlog("exampled", LOG_PID | LOG_NDELAY, LOG_DAEMON);
+  syslog(LOG_INFO, "I'M A DAEMON PROCESS");
+  closelog();
+
+  while (1) {
+    sleep(1);
+  }
+
+  return 0;
+}
