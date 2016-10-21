5cb8ef513a03d96fe4eb4024aeac6b9c3a0ae93b
Thiago Farina <tfarina@chromium.org>
2016-10-20 22:41:57 -0200

    sbuf: improve the way documentation comments are formatted
    This way it is way easier to expand later on.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/libs/sbuf/sbuf.h
+++ c/libs/sbuf/sbuf.h
@@ -3,30 +3,44 @@
 
 #include <stddef.h>
 
-/* A sbuf is a byte buffer. */
+/*
+ * A sbuf is a byte buffer.
+ */
 typedef struct {
 	char *data; /* The data contained in the buffer. */
 	size_t len;
 	size_t cap; /* The amount of data the buffer can contain. */
 } sbuf;
 
-/* sbuf_init prepares an uninitialized buffer for use. */
+/*
+ * sbuf_init prepares an uninitialized buffer for use.
+ */
 void sbuf_init(sbuf *b);
 
-/* sbuf_reset truncates the buffer back to zero length. */
+/*
+ * sbuf_reset truncates the buffer back to zero length.
+ */
 void sbuf_reset(sbuf *b);
 
-/* sbuf_free frees the storage associated with a buffer. */
+/*
+ * sbuf_free frees the storage associated with a buffer.
+ */
 void sbuf_free(sbuf *b);
 
-/* sbuf_grow ensures that the buffer has at least |extra| more bytes between its
- * len and cap. */
+/*
+ * sbuf_grow ensures that the buffer has at least |extra| more bytes between its
+ * len and cap.
+ */
 void sbuf_grow(sbuf *b, size_t extra);
 
-/* sbuf_append appends the |len| bytes at |data| to the buffer. */
+/*
+ * sbuf_append appends the |len| bytes at |data| to the buffer.
+ */
 void sbuf_append(sbuf *b, const void *data, size_t len);
 
-/* sbuf_append_str appends |str| to the buffer. */
+/*
+ * sbuf_append_str appends |str| to the buffer.
+ */
 void sbuf_append_str(sbuf *b, const char *str);
 
 #endif  /* SBUF_H_ */
