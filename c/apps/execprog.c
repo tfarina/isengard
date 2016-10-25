5a7b2d16a60022b7cb843ac0b277d18d52ed5cdf
Thiago Farina <tfarina@chromium.org>
2016-10-25 19:27:12 -0200

    execprog.c: improve error message
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/execprog.c
+++ c/apps/execprog.c
@@ -1,4 +1,6 @@
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 static void
@@ -6,12 +8,13 @@ print_java_version(void)
 {
         char *cmd[] = { "java", "-version", (char *)0 };
         execv("/usr/bin/java", cmd);
+
+        /* If we reach here, an error has occurred. */
+        fprintf(stderr, "execv /usr/bin/java failed: %s\n", strerror(errno));
 }
 
 int main(void) {
         print_java_version();
-
-        printf("unreachable\n");
 
         return 0;
 }
