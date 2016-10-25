57891994bc179e6e01a5e5f560f49c5d5842d59a
Thiago Farina <tfarina@chromium.org>
2016-10-25 19:01:56 -0200

    vec: reformat comments using C89 style
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- vec/vec.h
+++ vec/vec.h
@@ -1,30 +1,44 @@
 #ifndef VEC_H_
 #define VEC_H_
 
-// The invariant with the vectors is that v->strs[0:v->len] is allocated
-// strings that are owned by the vector. The data beyond v->len may be
-// garbage.
+/*
+ * The invariant with the vectors is that v->strs[0:v->len] is allocated
+ * strings that are owned by the vector. The data beyond v->len may be
+ * garbage.
+ */
 
-// A Vec is a string vector, like Go's []string.
+/*
+ * A Vec is a string vector, like Go's []string.
+ */
 typedef struct {
 	char **strs;
 	int len;
 	int cap;
 } Vec;
 
-// vec_init prepares an uninitialized vector for use.
+/*
+ * vec_init prepares an uninitialized vector for use.
+ */
 void vec_init(Vec *v);
 
-// vec_reset truncates the vector back to zero length.
+/*
+ * vec_reset truncates the vector back to zero length.
+ */
 void vec_reset(Vec *v);
 
-// vec_free frees the storage associated with the vector.
+/* 
+ * vec_free frees the storage associated with the vector.
+ */
 void vec_free(Vec *v);
 
-// vec_add adds a copy of the string p to the vector.
+/* 
+ * vec_add adds a copy of the string p to the vector.
+ */
 void vec_add(Vec *v, char *p);
 
-// vec_copy copies the srclen strings at src into the vector.
+/*
+ * vec_copy copies the srclen strings at src into the vector.
+ */
 void vec_copy(Vec *dst, char **src, int n);
 
-#endif  // VEC_H_
+#endif  /* VEC_H_ */
