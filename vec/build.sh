88a17d4aeb73a13f45b50b0e7ab2f448c596a84c
Thiago Farina <tfarina@chromium.org>
2016-10-27 21:12:58 -0200

    vec: remove all build files except Bazel BUILD file
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- vec/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-clang -c os_unix.c -o os_unix.o
-clang -c vec.c -o vec.o
-clang -c vec_test.c -o vec_test.o
-
-clang os_unix.o vec.o vec_test.o -o vec_test
--- vec/clean.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rm -f vec_test *.o
