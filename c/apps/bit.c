a8d624ffca4038f98ca4ef7e873511f6ca5c8349
Thiago Farina <tfarina@chromium.org>
2016-12-05 20:34:37 -0200

    bitwise AND (&) operator
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/bit.c
+++ c/apps/bit.c
@@ -4,5 +4,9 @@ int main(void) {
   const uint8_t initial_bits = 0b00001111;
   const uint8_t inverted_bits = ~initial_bits; /* equals to 0b11110000 */
 
+  const uint8_t first_six_bits = 0b11111100;
+  const uint8_t last_six_bits  = 0b00111111;
+  const uint8_t middle_four_bits = first_six_bits & last_six_bits; /* equals to 0b00111100 */
+
   return 0;
 }
