e75f883ecd06768cfdb5c601410c05e2b8f98022
Thiago Farina <tfarina@chromium.org>
2016-12-06 14:59:11 -0200

    bitwise OR (|) operator
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/bit.c
+++ c/apps/bit.c
@@ -8,5 +8,9 @@ int main(void) {
   const uint8_t last_six_bits  = 0b00111111;
   const uint8_t middle_four_bits = first_six_bits & last_six_bits; /* equals to 0b00111100 */
 
+  const uint8_t some_bits = 0b10110010;
+  const uint8_t more_bits = 0b01011110;
+  const uint8_t combined_bits = some_bits | more_bits; /* equals to 0b11111110 */ 
+
   return 0;
 }
