0e538aea8ca66ec6ebb2c7dcb407f4fcbe0a610e
Thiago Farina <tfarina@chromium.org>
2017-03-12 00:48:09 -0300

    dnsclient: use "name" instead of "domain" in usage message
    Like kdig.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/network/dnsclient.c
+++ c/apps/network/dnsclient.c
@@ -179,7 +179,7 @@ int main(int argc, char **argv) {
   ssize_t rlen;
 
   if (argc != 2) {
-    fprintf(stderr, "usage: %s domain\n", argv[0]);
+    fprintf(stderr, "usage: %s name\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
