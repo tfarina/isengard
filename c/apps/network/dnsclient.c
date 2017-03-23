d7e40f115651e2c67c11301ed5181e53d3d27ffe
Thiago Farina <tfarina@chromium.org>
2017-03-22 22:47:02 -0300

    dnsclient: get rid of reply_header variable
    Reuse |header| instead.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/network/dnsclient.c
+++ c/apps/network/dnsclient.c
@@ -233,7 +233,6 @@ static uint8_t *dname_from_str(const char *name) {
 int main(int argc, char **argv) {
   char owner[DNS_DNAME_MAXLEN];
   struct dnsheader *header;
-  struct dnsheader *reply_header;
   struct dnsquestion *question;
   uint8_t *query_pkt;
   char *server_name;
@@ -298,7 +297,6 @@ int main(int argc, char **argv) {
   // Prepare the packet (header + question) with the query that will be sent to
   // the DNS server.
   size_t query_pktlen = sizeof(*header) + get_question_size(question);
-
   query_pkt = malloc(query_pktlen);
 
   int offset = 0;
@@ -366,20 +364,23 @@ int main(int argc, char **argv) {
 
   freeaddrinfo(addrlist);
 
+  free(header);
+
   // Parse reply to the dnsheader structure.
-  reply_header = malloc(sizeof(struct dnsheader));
+  header = malloc(sizeof(*header));
+  memset(header, 0, sizeof(*header));
 
-  reply_header->id = read_uint16(reply_pkt + DNS_OFFSET_ID);
-  reply_header->flags = read_uint16(reply_pkt + DNS_OFFSET_FLAGS);
-  reply_header->qdcount = read_uint16(reply_pkt + DNS_OFFSET_QDCOUNT);
-  reply_header->ancount = read_uint16(reply_pkt + DNS_OFFSET_ANCOUNT);
-  reply_header->nscount = read_uint16(reply_pkt + DNS_OFFSET_NSCOUNT);
-  reply_header->arcount = read_uint16(reply_pkt + DNS_OFFSET_ARCOUNT);
+  header->id = read_uint16(reply_pkt + DNS_OFFSET_ID);
+  header->flags = read_uint16(reply_pkt + DNS_OFFSET_FLAGS);
+  header->qdcount = read_uint16(reply_pkt + DNS_OFFSET_QDCOUNT);
+  header->ancount = read_uint16(reply_pkt + DNS_OFFSET_ANCOUNT);
+  header->nscount = read_uint16(reply_pkt + DNS_OFFSET_NSCOUNT);
+  header->arcount = read_uint16(reply_pkt + DNS_OFFSET_ARCOUNT);
 
-  uint16_t opcode_id = (reply_header->flags & OPCODE_MASK) >> OPCODE_SHIFT;
+  uint16_t opcode_id = (header->flags & OPCODE_MASK) >> OPCODE_SHIFT;
   const struct lookup_table *opcode = lookup_by_id(opcodes, opcode_id);
 
-  uint16_t rcode_id = reply_header->flags & RCODE_MASK;
+  uint16_t rcode_id = header->flags & RCODE_MASK;
   const struct lookup_table *rcode = lookup_by_id(rcodes, rcode_id);
 
   printf(";; ->>HEADER<<- ");
@@ -396,35 +397,35 @@ int main(int argc, char **argv) {
     printf("rcode: ?? (%u), ", rcode_id);
   }
 
-  printf("id: %d\n", reply_header->id);
+  printf("id: %d\n", header->id);
   printf(";; flags:");
-  if ((reply_header->flags & FLAG_QR) != 0) {
+  if ((header->flags & FLAG_QR) != 0) {
     printf(" qr");
   }
-  if ((reply_header->flags & FLAG_AA) != 0) {
+  if ((header->flags & FLAG_AA) != 0) {
     printf(" aa");
   }
-  if ((reply_header->flags & FLAG_TC) != 0) {
+  if ((header->flags & FLAG_TC) != 0) {
     printf(" tc");
   }
-  if ((reply_header->flags & FLAG_RD) != 0) {
+  if ((header->flags & FLAG_RD) != 0) {
     printf(" rd");
   }
-  if ((reply_header->flags & FLAG_CD) != 0) {
+  if ((header->flags & FLAG_CD) != 0) {
     printf(" cd");
   }
-  if ((reply_header->flags & FLAG_RA) != 0) {
+  if ((header->flags & FLAG_RA) != 0) {
     printf(" ra");
   }
-  if ((reply_header->flags & FLAG_AD) != 0) {
+  if ((header->flags & FLAG_AD) != 0) {
     printf(" ad");
   }
 
   printf("; ");
-  printf("QUERY: %u, ", reply_header->qdcount);
-  printf("ANSWER: %u, ", reply_header->ancount);
-  printf("AUTHORITY: %u, ", reply_header->nscount);
-  printf("ADDITIONAL: %u\n", reply_header->arcount);
+  printf("QUERY: %u, ", header->qdcount);
+  printf("ANSWER: %u, ", header->ancount);
+  printf("AUTHORITY: %u, ", header->nscount);
+  printf("ADDITIONAL: %u\n", header->arcount);
 
   printf("\n");
 
