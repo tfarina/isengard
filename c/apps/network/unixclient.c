e277b111bfd8503c8581051d16d47980eeef9e80
Thiago Farina <tfarina@chromium.org>
2017-04-13 22:15:36 -0300

    add the client side of unixserver
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ c/apps/network/unixclient.c
@@ -0,0 +1,42 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#define BUFSIZE 1024
+
+int main(void) {
+        int socket_fd;
+        struct sockaddr_un unix_addr;
+        char buf[BUFSIZE];
+        ssize_t n;
+
+        socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+        if (socket_fd == -1) {
+                fprintf(stderr, "failed to create AF_UNIX socket\n");
+                return EXIT_FAILURE;
+        }
+
+        memset(&unix_addr, 0, sizeof(unix_addr));
+        unix_addr.sun_family = AF_UNIX;
+        strncpy(unix_addr.sun_path, "server.socket", sizeof(unix_addr.sun_path));
+
+        if (connect(socket_fd, (struct sockaddr *)&unix_addr, sizeof(unix_addr)) == -1) {
+        }
+
+        /* TODO: send the command to the server.*/
+        char pre[10];
+        snprintf(pre, sizeof(pre), "UNIXCT%d ", 1);
+
+        write(socket_fd, pre, strlen(pre));
+
+        while ((n = read(socket_fd, buf, BUFSIZE)) > 0 ) {
+                buf[n] = 0;
+                fputs(buf, stdout);
+        }
+
+        return EXIT_SUCCESS;
+}
