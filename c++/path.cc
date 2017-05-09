574153fbe44c521f4698ddc8ff15309ca375c6d3
Thiago Farina <tfarina@chromium.org>
2017-05-09 19:29:16 -0300

    add the source file with implementation for path.h
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ c++/path.cc
@@ -0,0 +1,19 @@
+#include "path.h"
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+std::string MakeAbsolute(const char *path) {
+  char full_path[PATH_MAX];
+  if (realpath(path, full_path) == NULL) {
+    fprintf(stderr, "realpath('%s') failed\n", path);
+    return std::string();
+  }
+  return std::string(full_path);
+}
+
+bool PathExists(const std::string& path) {
+  return access(path.c_str(), F_OK) == 0;
+}
