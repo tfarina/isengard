2893db27770305da782d0aca2ef359a06de8a7a0
Thiago Farina <tfarina@chromium.org>
2017-05-24 11:36:50 -0300

    base: implement CreateDirectory() function
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c++/libs/base/directory.cc
+++ c++/libs/base/directory.cc
@@ -2,7 +2,48 @@
 
 #include <sys/stat.h>
 
+#include <vector>
+
+#include "base/path.h"
+
 bool IsDirectory(const std::string& path) {
   struct stat buf;
   return stat(path.c_str(), &buf) == 0 && S_ISDIR(buf.st_mode);
+}
+
+bool CreateDirectory(const std::string& full_path) {
+  std::vector<std::string> subpaths;
+
+  // Collect a list of all parent directories.
+  std::string last_path = full_path;
+  subpaths.push_back(full_path);
+
+  for (std::string path = GetDirectoryName(full_path);
+       path != last_path;
+       path = GetDirectoryName(path)) {
+    subpaths.push_back(path);
+    last_path = path;
+  }
+
+  // Iterate through the parents and create the missing ones.
+  for (std::vector<std::string>::reverse_iterator i = subpaths.rbegin(); i != subpaths.rend(); i++) {
+    if (IsDirectory(*i)) {
+      continue;
+    }
+
+    if (mkdir(i->c_str(), 0700) == 0) {
+      continue;
+    }
+
+    // mkdir failed, but it might be due to the directory
+    // appearing out of thin air. This can occur if two processes
+    // are trying to create the same file system tree at the same
+    // time. Check to see if it exists and make sure it is a
+    // directory.
+    if (!IsDirectory(*i)) {
+      return false;
+    }
+  }
+
+  return true;
 }
--- c++/libs/base/directory.h
+++ c++/libs/base/directory.h
@@ -6,4 +6,6 @@
 // Returns whether the given path is a directory.
 bool IsDirectory(const std::string& path);
 
+bool CreateDirectory(const std::string& full_path);
+
 #endif  // BASE_DIRECTORY_H_
