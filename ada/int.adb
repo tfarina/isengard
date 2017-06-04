0f04764ef64a987ad4046076330c207f671725a6
Thiago Farina <tfarina@chromium.org>
2017-06-04 17:26:48 -0300

    ada: add a simple example showing how to declare an integer
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ ada/int.adb
@@ -0,0 +1,17 @@
+-- http://perso.telecom-paristech.fr/~pautet/Ada95/e_c03_p1.ada
+
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use Ada.Text_IO, Ada.Integer_Text_IO;
+
+procedure Int is
+   Index : INTEGER;           -- A simple Integer type
+begin
+   Index := 23;
+   Put("The value of Index is");
+   Put(Index);                -- The default field width is 11 columns
+   New_Line;
+   Index := Index + 12;
+   Put("The value of Index is");
+   Put(Index, 8);
+   New_Line;
+end Int;
