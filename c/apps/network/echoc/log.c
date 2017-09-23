f543b91feb01f8255d4de0ab472ee0a327435f0d
Thiago Farina <tfransosi@gmail.com>
2017-09-23 10:17:49 -0300

    create a separate directory for echo client
    In order to separate it from echod daemon server counter-part.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/network/echoc/log.c
@@ -0,0 +1,46 @@
+#include "log.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void reportf(const char *prefix, const char *fmt, va_list args) {
+    fflush(stderr);
+    fputs(prefix, stderr);
+    vfprintf(stderr, fmt, args);
+    fputc('\n', stderr);
+}
+
+void fatal(const char *fmt, ...) {
+    va_list args;
+
+    va_start(args, fmt);
+    reportf("fatal: ", fmt, args);
+    va_end(args);
+
+    exit(EXIT_FAILURE);
+}
+
+void error(const char *fmt, ...) {
+    va_list args;
+
+    va_start(args, fmt);
+    reportf("error: ", fmt, args);
+    va_end(args);
+}
+
+void warning(const char *fmt, ...) {
+    va_list args;
+
+    va_start(args, fmt);
+    reportf("warning: ", fmt, args);
+    va_end(args);
+}
+
+void info(const char *fmt, ...) {
+    va_list args;
+
+    va_start(args, fmt);
+    reportf("", fmt, args);
+    va_end(args);
+}
