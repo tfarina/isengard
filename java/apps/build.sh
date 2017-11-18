f31f3bd2021c83953a2efb7c44d7ab9d8c97cc4f
Thiago Farina <tfransosi@gmail.com>
2017-11-18 15:11:27 -0200

    move fibonacci app into its own directory
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- java/apps/build.sh
+++ java/apps/build.sh
@@ -3,4 +3,4 @@
 # https://ant.apache.org/manual/tutorial-HelloWorldWithAnt.html
 
 mkdir -p build/classes
-javac -sourcepath src -d build/classes src/Hello.java src/Fibonacci.java
+javac -sourcepath src -d build/classes src/Hello.java
