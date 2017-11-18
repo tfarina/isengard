1b37009d9d5801cd1646bd910b0722291cd6ab8e
Thiago Farina <tfransosi@gmail.com>
2017-11-18 17:24:03 -0200

    remove a copy of HelloWorld.java
    It now lives is src/main/java.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- java/tutorial/zetcode/src/com/zetcode/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-// http://zetcode.com/lang/java/java/
-
-package com.zetcode;
-
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, world!");
-  }
-}
