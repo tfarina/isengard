c27ad30f8f86b71071e571b0c6533451cd254d16
Thiago Farina <tfarina@chromium.org>
2017-11-20 09:52:51 -0200

    add a program that prints ith 'hello' up to 10
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ java/apps/tenhellos/src/TenHellos.java
@@ -0,0 +1,11 @@
+public class TenHellos {
+    public static void main(String[] args) {
+	System.out.println("1st hello");
+	System.out.println("2nd hello");
+	System.out.println("3rd hello");
+
+	for (int i = 4; i <= 10; i++) {
+	    System.out.println(i + "th hello");
+	}
+    }
+}
