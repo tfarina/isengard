397f26499d594c00d62466800320a9853764d43e
Thiago Farina <tfarina@chromium.org>
2017-11-22 11:22:10 -0200

    add a simple program to show the eight primitive data types builtin the java programming language
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ java/apps/datatypes/src/DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args) {
+	byte b = 0;
+	short s = 0;
+	int i = 0;
+	long l = 0L;
+	float f = 0.0f;
+	double d = 0.0d;
+	boolean result = false;
+	char c = '\u0000';
+
+	System.out.println("byte value is: " + b);
+	System.out.println("short value is: " + s);
+	System.out.println("int value is: " + i);
+	System.out.println("long value is: " + l);
+	System.out.println("float value is: " + f);
+	System.out.println("double value is: " + d);
+	System.out.println("boolean value is: " + result);
+	System.out.println("char value is: " + c);
+    }
+}
