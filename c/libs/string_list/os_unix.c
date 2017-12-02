2f8e49a98f75e30324ab4b94503d1bf3a2ba596b
Thiago Farina <tfransosi@gmail.com>
2017-12-02 17:41:20 -0200

    reimplement fatal() in a more sofisticated way
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/libs/string_list/os_unix.c
+++ c/libs/string_list/os_unix.c
@@ -1,9 +1,38 @@
 #include "os.h"
 
+#define _GNU_SOURCE
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+static void vwrite(const char *msg, va_list ap)
+{
+        char *fmt;
+
+        if (asprintf(&fmt, "%s\n", msg) == -1)
+	        exit(1);
+
+        if (vfprintf(stderr, fmt, ap) == -1)
+                exit(1);
+
+        fflush(stderr);
+        free(fmt);
+}
+
+static void vfatal(const char *msg, va_list ap)
+{
+        char *fmt;
+
+        if (asprintf(&fmt, "fatal: %s", msg) == -1)
+                exit(1);
+
+        vwrite(fmt, ap);
+
+        free(fmt);
+
+	exit(1);
+}
 
 void fatal(const char *msg, ...)
 {
@@ -11,13 +40,7 @@ void fatal(const char *msg, ...)
 
         va_start(args, msg);
 
-        fprintf(stderr, "fatal: ");
-        vfprintf(stderr, msg, args);
-        fprintf(stderr, "\n");
-
-        va_end(args);
-
-        exit(EXIT_FAILURE);
+	vfatal(msg, args);
 }
 
 char* xstrdup(char *p)
