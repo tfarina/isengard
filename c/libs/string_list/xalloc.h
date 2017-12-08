e3c8bc97ee712693393d652a48d4890e0766f70b
Thiago Farina <tfransosi@gmail.com>
2017-12-08 10:23:44 -0200

    use the /**/ style of comments
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/libs/string_list/xalloc.h
+++ c/libs/string_list/xalloc.h
@@ -3,13 +3,17 @@
 
 #include <stddef.h>
 
-// xrealloc grows the allocation p to n bytes and
-// returns the new (possibly moved) pointer.
-// It calls fatal if it runs out of memory.
+/**
+ * xrealloc grows the allocation p to n bytes and
+ * returns the new (possibly moved) pointer.
+ * It calls fatal if it runs out of memory.
+ */
 void *xrealloc(void *oldptr, size_t size);
 
-// xstrdup returns a newly allocated copy of s.
-// It calls fatal if it runs out of memory.
+/**
+ * xstrdup returns a newly allocated copy of s.
+ * It calls fatal if it runs out of memory.
+ */
 char *xstrdup(char *s);
 
 #endif  /* XALLOC_H_ */
