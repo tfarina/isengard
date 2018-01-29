490056c166cd93402f892358918d747f71da7de2
Thiago Farina <tfransosi@gmail.com>
2018-01-29 19:34:14 -0200

    move database configuration settings to a ini file
    That way database passwords are not stored in source code, and
    when changing no need to recompile in order to changes to take effect.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/bookmarks/insert.c
+++ c/apps/bookmarks/insert.c
@@ -6,22 +6,58 @@
 #include <stdlib.h>
 #include <mysql/mysql.h>
 
-static const char kDBHost[] = "localhost";
-static const char kDBUser[] = "ken";
-static const char kDBPassword[] = "194304";
-static const char kDBName[] = "ctestdb";
+#include "iniparser.h"
+
+#define USERCONFFILE ".bookmarksrc"
+#define PATH_SEP '/'
+
+static char *get_homedir(void)
+{
+  return getenv("HOME");
+}
+
+/* Code from msmtp:src/tools.c:get_filename */
+static char *make_file_path(const char *directory, const char *name)
+{
+  char *path;
+  size_t dirlen;
+
+  dirlen = strlen(directory);
+  path = malloc((dirlen + strlen(name) + 2) * sizeof(char));
+  strcpy(path, directory);
+  if (dirlen == 0 || path[dirlen - 1] != PATH_SEP) {
+    path[dirlen++] = PATH_SEP;
+  }
+  strcpy(path + dirlen, name);
+  return path;
+}
 
 int main(int argc, char **argv) {
+  char *homedir;
+  char *userconffile;
+  dictionary *ini;
+  const char *host;
+  const char *user;
+  const char *password;
+  const char *dbname;
   MYSQL *conn = NULL;
   unsigned int port = 0;
   char query[256];
+
+  homedir = get_homedir();
+  userconffile = make_file_path(homedir, USERCONFFILE);
+  ini = iniparser_load(userconffile);
+  host = iniparser_getstring(ini, "mysql:host", NULL);
+  user = iniparser_getstring(ini, "mysql:user", NULL);
+  password = iniparser_getstring(ini, "mysql:password", NULL);
+  dbname = iniparser_getstring(ini, "mysql:dbname", NULL);
 
   if ((conn = mysql_init(NULL)) == NULL) {
     fprintf(stderr, "mysql: unable to allocate memory for database connection.\n");
     return -1;
   }
 
-  if (mysql_real_connect(conn, kDBHost, kDBUser, kDBPassword, kDBName, port,
+  if (mysql_real_connect(conn, host, user, password, dbname, port,
                          NULL, 0) == NULL) {
     fprintf(stderr, "mysql: connection to database failed: %s\n", mysql_error(conn));
     mysql_close(conn);
