078d86bd7932f38577813f615dab448af3cc6a47
Thiago Farina <tfransosi@gmail.com>
2018-02-12 19:13:59 -0200

    rename percentage_increase to percentage_change
    Because it can be used to calculate percentage decrease, which is just
    a negative value
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/percentage.c
+++ c/apps/percentage.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int percentage_increase(int old_value, int new_value) {
+static int percentage_change(int old_value, int new_value) {
   int increase = new_value - old_value;
 
   return (increase / old_value) * 100;
 }
 
 int main(int argc, char **argv) {
-  int increase = percentage_increase(2, 10);
+  int change = percentage_change(2, 10);
 
-  printf("%d%%\n", increase);
+  printf("%d%%\n", change);
 
   return 0;
 }
