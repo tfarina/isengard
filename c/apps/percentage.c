4bb14a8a72e4764ef4626f6129ae18607822b717
Thiago Farina <tfransosi@gmail.com>
2018-02-14 22:09:01 -0200

    change percentage_change() to work on doubles
    That way it returns decimal values.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/percentage.c
+++ c/apps/percentage.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-static int percentage_change(int old_value, int new_value) {
-  int increase = new_value - old_value;
+static double percentage_change(double old_value, double new_value) {
+  double increase = new_value - old_value;
 
   return (increase / old_value) * 100;
 }
 
 int main(int argc, char **argv) {
-  int change = percentage_change(2, 10);
+  double change = percentage_change(2.5, 2.61);
 
-  printf("%d%%\n", change);
+  printf("%f%%\n", change);
 
   return 0;
 }
