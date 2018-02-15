cb63d14c69ba8c1cf5cfd14a3ef1e5910dfcb74c
Thiago Farina <tfransosi@gmail.com>
2018-02-15 08:46:26 -0200

    format the percentage output to two decimal places
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/percentage.c
+++ c/apps/percentage.c
@@ -9,7 +9,7 @@ static double percentage_change(double old_value, double new_value) {
 int main(int argc, char **argv) {
   double change = percentage_change(2.5, 2.61);
 
-  printf("%f%%\n", change);
+  printf("%.2f%%\n", change);
 
   return 0;
 }
