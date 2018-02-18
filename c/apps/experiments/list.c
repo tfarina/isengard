0f7b93e0630222ed84d1170e045c16c19bfd9797
Thiago Farina <tfransosi@gmail.com>
2018-02-18 13:18:59 -0300

    change the output of product_list to produce a tabular output
    Much like the output mysql client produces.
    
    The algorithm was taken from http://www.kitebird.com/mysql-book/ch06-3ed.pdf.
    
    The general outline of the display algorithm is as follows:
    1.  Determine the display width of each column.
    2.  Print a row of boxed column labels (delimited by vertical bars and preceded and
    followed by rows of dashes).
    3.  Print the values in each row of the result set, with each column boxed (delimited
    by vertical bars) and lined up vertically. In addition, print numbers right justified
    and print the  for
    NULL
    values.
    4.  At the end, print a count of the number of rows retrieved.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/experiments/list.c
+++ c/apps/experiments/list.c
@@ -53,15 +53,43 @@ static MYSQL *mysql_connect(const char *host, const char *user,
   return conn;
 }
 
+/*
+ * http://www.kitebird.com/mysql-book/ch06-3ed.pdf
+ */
+static void print_dashes(MYSQL_RES *res)
+{
+  MYSQL_FIELD *field;
+  unsigned int i, j;
+  unsigned int num_fields;
+
+  mysql_field_seek(res, 0);
+
+  fputc('+', stdout);
+
+  num_fields = mysql_num_fields(res);
+
+  for (i = 0; i < num_fields; i++) {
+    field = mysql_fetch_field(res);
+    for (j = 0; j < field->max_length +2; j++) {
+      fputc('-', stdout);
+    }
+    fputc('+', stdout);
+  }
+
+  fputc('\n', stdout);
+}
+
 static int product_list(MYSQL *conn)
 {
   char query[256];
   MYSQL_RES *res = NULL;
   unsigned int num_fields;
+  unsigned long col_len;
+  MYSQL_FIELD *field;
   MYSQL_ROW row;
   int i;
 
-  sprintf(query, "SELECT * FROM products");
+  sprintf(query, "SELECT productCode, productName, productLine FROM products");
 
   if (mysql_query(conn, query)) {
     fprintf(stderr, "mysql: sql select failed: %s\n", mysql_error(conn));
@@ -77,15 +105,48 @@ static int product_list(MYSQL *conn)
 
   num_fields = mysql_num_fields(res);
 
-  while ((row = mysql_fetch_row(res))) {
-    for (i = 0; i < num_fields; i++) {
-      if (i > 0) {
-        fputc('\t', stdout);
-      }
-      printf("%s", row[i] ? row[i] : "NULL");
-    }
-    printf("\n");
+  /*
+   * Determine column display widths -- requires result set to be
+   * generated with mysql_store_result(), not mysql_use_result().
+   */
+  mysql_field_seek (res, 0);
+  for (i = 0; i < mysql_num_fields (res); i++) {
+    field = mysql_fetch_field (res);
+    col_len = strlen (field->name);
+    if (col_len < field->max_length)
+      col_len = field->max_length;
+    if (col_len < 4 && !IS_NOT_NULL (field->flags))
+      col_len = 4;    /* 4 = length of the word "NULL" */
+    field->max_length = col_len;    /* reset column info */
   }
+
+  print_dashes(res);
+  fputc('|', stdout);
+  mysql_field_seek(res, 0);
+  for(i = 0; i < num_fields; i++) {
+    field = mysql_fetch_field(res);
+    printf(" %-*s |", (int)field->max_length, field->name);
+  }
+  fputc('\n', stdout);
+  print_dashes(res);
+
+  while ((row = mysql_fetch_row(res))) {
+    mysql_field_seek(res, 0);
+    fputc('|', stdout);
+    for (i = 0; i < num_fields; i++) {
+      field = mysql_fetch_field(res);
+      if (row[i] == NULL) {
+        printf(" %-*s |", (int)field->max_length, "NULL");
+      } else if(IS_NUM(field->type)) {
+        printf(" %*s |", (int)field->max_length, row[i]);
+      } else {
+        printf(" %-*s |", (int)field->max_length, row[i]);
+      }
+    }
+    fputc('\n', stdout);
+  }
+
+  print_dashes(res);
 
   mysql_free_result(res);
 
