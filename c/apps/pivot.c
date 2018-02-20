5a0b2abe2932ec1d2142d02d7737efad9ed3c6c1
Thiago Farina <tfransosi@gmail.com>
2018-02-20 09:38:15 -0300

    calculate the first area of support
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/pivot.c
+++ c/apps/pivot.c
@@ -1,5 +1,7 @@
 /**
  * Pivot point = P = (H + L + C) / 3
+ * First area of resistance = R1 = 2P - L
+ * First area of support = S1 = 2P - H
  */
 
 #include <stdio.h>
@@ -14,11 +16,17 @@ static float resistance1(float pivot_point, float low)
   return (2 * pivot_point) - low;
 }
 
+static float support1(float pivot_point, float high)
+{
+  return (2 * pivot_point) - high;
+}
+
 int main(int argc, char **argv)
 {
   float high, low, close;
   float pp;
   float r1;
+  float s1;
 
   high = 60.82;
   low = 59.74;
@@ -26,8 +34,9 @@ int main(int argc, char **argv)
 
   pp = pivot_point(high, low, close);
   r1 = resistance1(pp, low);
+  s1 = support1(pp, high);
 
-  printf("%.4f\t%.4f\n", pp, r1);
+  printf("%.4f\t%.4f\t%.4f\n", s1, pp, r1);
 
   return 0;
 }
