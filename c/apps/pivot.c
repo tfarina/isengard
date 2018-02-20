28e571a08b71386c9d23607d2acb72b3407b2402
Thiago Farina <tfransosi@gmail.com>
2018-02-20 09:32:31 -0300

    calculate the first area of resistance
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/pivot.c
+++ c/apps/pivot.c
@@ -9,18 +9,25 @@ static float pivot_point(float high, float low, float close)
   return (high + low + close) / 3;
 }
 
+static float resistance1(float pivot_point, float low)
+{
+  return (2 * pivot_point) - low;
+}
+
 int main(int argc, char **argv)
 {
   float high, low, close;
-  float ppoint;
+  float pp;
+  float r1;
 
   high = 60.82;
   low = 59.74;
   close = 60.16;
 
-  ppoint = pivot_point(high, low, close);
+  pp = pivot_point(high, low, close);
+  r1 = resistance1(pp, low);
 
-  printf("%.4f\n", ppoint);
+  printf("%.4f\t%.4f\n", pp, r1);
 
   return 0;
 }
