6ea1ceebdf4ba6d9849ee1345fd7046176cdfe0f
Thiago Farina <tfransosi@gmail.com>
2018-02-20 09:15:19 -0300

    add a program to calculate the pivot point
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/pivot.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+static float pivot_point(float high, float low, float close)
+{
+  return (high + low + close) / 3;
+}
+
+int main(int argc, char **argv)
+{
+  float high, low, close;
+  float ppoint;
+
+  high = 60.82;
+  low = 59.74;
+  close = 60.16;
+
+  ppoint = pivot_point(high, low, close);
+
+  printf("%.4f\n", ppoint);
+
+  return 0;
+}
