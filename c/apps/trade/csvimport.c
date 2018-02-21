e29e02f4605a031eb718795e8db9b4d912791720
Thiago Farina <tfransosi@gmail.com>
2018-02-21 06:05:43 -0300

    stop importing the same record more than once
    First check to see if it exist before deciding if it is an insert operation, otherwise just update the existing record.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/trade/csvimport.c
+++ c/apps/trade/csvimport.c
@@ -220,15 +220,38 @@ void process_row(int delim __attribute__((unused)), void *ctx) {
 static int tick_add(MYSQL *conn, tickerdata *tick)
 {
   char query[256];
+  MYSQL_RES *res;
 
-  sprintf(query, "INSERT INTO historicaldata (symbol, date, open, high, low, close, adjClose, volume) VALUES ('%s', '%s', %f, %f, %f, %f, %f, %d)",
-          tick->symbol, tick->date, tick->open, tick->high, tick->low, tick->close, tick->adj_close, tick->volume);
- 
+  /* Determine if this is an insert or update operation. */
+  sprintf(query, "SELECT symbol FROM historicaldata WHERE symbol = \"%s\" and date = \"%s\"", tick->symbol, tick->date);
   if (mysql_query(conn, query)) {
     fprintf(stderr, "mysql: sql insert failed: %s\n", mysql_error(conn));
     mysql_close(conn);
     return -1;
   }
+
+  res = mysql_store_result(conn);
+  if (res == NULL) {
+    fprintf(stderr, "mysql: mysql_store_result failed: %s\n", mysql_error(conn));
+    mysql_close(conn);
+    return -1;
+  }
+
+  /* If there is a record with this symbol and date, update it, otherwise insert new record. */
+  if (mysql_num_rows(res)) {
+    sprintf(query, "UPDATE historicaldata SET open=%f, high=%f, low=%f, close=%f, adjClose=%f, volume=%d WHERE symbol = \"%s\" and date = \"%s\"", tick->open, tick->high, tick->low, tick->close, tick->adj_close, tick->volume, tick->symbol, tick->date);
+  } else {
+    sprintf(query, "INSERT INTO historicaldata (symbol, date, open, high, low, close, adjClose, volume) VALUES ('%s', '%s', %f, %f, %f, %f, %f, %d)",
+            tick->symbol, tick->date, tick->open, tick->high, tick->low, tick->close, tick->adj_close, tick->volume);
+  }
+
+  if (mysql_query(conn, query)) {
+    fprintf(stderr, "mysql: sql operation failed: %s\n", mysql_error(conn));
+    mysql_close(conn);
+    return -1;
+  }
+
+  printf("%s\n", query);
 
   return 0;
 }
@@ -300,6 +323,7 @@ int main(int argc, char **argv) {
     return -1;
   }
 
+  /* NOTE: This will overwrite existing data. */
   printf("Importing records...\n");
 
   for (i = 0; i < tdr.ticks_used; i++) {
