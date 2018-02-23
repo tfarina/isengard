f0a7882f15b0e43603145f44ca49203f14be7050
Thiago Farina <tfransosi@gmail.com>
2018-02-23 13:58:45 -0300

    add a program in Java to calculate the mean of a set of values
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ java/apps/stats/src/Stats.java
@@ -0,0 +1,27 @@
+public class Stats {
+    public static double Sum(double[] a) {
+        double sum = 0.0;
+
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+	}
+
+        return sum;
+    }
+
+    public static double Mean(double[] a) {
+        if (a.length == 0) {
+            return Double.NaN;
+	}
+
+        double sum = Sum(a);
+
+        return sum / a.length;  
+    }
+
+    public static void main(String[] args) {
+        double[] a = { 2.4, 2.3, 2.7, 2.6 };
+
+	System.out.printf("Mean: %10.2f\n", Mean(a));
+    }
+}
