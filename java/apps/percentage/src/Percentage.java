f6cab7cda423e8539bccfb496a1635812b4e09a5
Thiago Farina <tfransosi@gmail.com>
2018-02-23 06:47:23 -0300

    rename 'percentage_increase' to 'PercentageChange'
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- java/apps/percentage/src/Percentage.java
+++ java/apps/percentage/src/Percentage.java
@@ -1,13 +1,13 @@
 public class Percentage {
-    public static int percentage_increase(int oldValue, int newValue) {
-	int increase = newValue - oldValue;
+    public static int PercentageChange(int oldValue, int newValue) {
+	int change = newValue - oldValue;
 
-	return (increase / oldValue) * 100;
+	return (change / oldValue) * 100;
     }
 
     public static void main(String[] args) {
-	int increase = percentage_increase(2, 10);
+	int change = PercentageChange(2, 10);
 
-	System.out.println(increase + "%");
+	System.out.println(change + "%");
     }
 }
