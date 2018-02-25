5329385f2243f1246768245fb6687d79c308ad9e
Thiago Farina <tfransosi@gmail.com>
2018-02-25 05:27:18 -0300

    add function to calculate the variance
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- java/apps/stats/src/Stats.java
+++ java/apps/stats/src/Stats.java
@@ -19,9 +19,28 @@ public class Stats {
         return sum / a.length;  
     }
 
+    /**
+     * Returns the sample variance in the specified array.
+     */
+    public static double Variance(double[] a) {
+        if (a.length == 0) {
+            return Double.NaN;
+	}
+
+        double avg = Mean(a);
+        double sum = 0.0;
+
+        for (int i = 0; i < a.length; i++) {
+            sum += (a[i] - avg) * (a[i] - avg);
+	}
+
+        return sum / (a.length - 1);
+    }
+
     public static void main(String[] args) {
         double[] a = { 2.4, 2.3, 2.7, 2.6 };
 
 	System.out.printf("Mean: %10.2f\n", Mean(a));
+	System.out.printf("Variance: %10.2f\n", Variance(a));
     }
 }
