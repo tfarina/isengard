a1d8d6f39ca23dd1cca40f5fd213e22ae6a5bfb7
Thiago Farina <tfransosi@gmail.com>
2018-02-27 08:39:20 -0300

    add functions to calculate the min and max of a range of values
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- java/apps/stats/src/Stats.java
+++ java/apps/stats/src/Stats.java
@@ -1,4 +1,42 @@
 public class Stats {
+    /**
+     * Returns the minimum value in the specified array.
+     */
+    public static double Min(double[] a) {
+        double min = Double.POSITIVE_INFINITY;
+
+        for (int i = 0; i < a.length; i++) {
+            if (Double.isNaN(a[i])) {
+                return Double.NaN;
+	    }
+
+            if (a[i] < min) {
+                min = a[i];
+	    }
+	}
+
+	return min;
+    }
+
+    /**
+     * Returns the maximum value in the specified array.
+     */
+    public static double Max(double[] a) {
+        double max = Double.NEGATIVE_INFINITY;
+
+        for (int i = 0; i < a.length; i++) {
+            if (Double.isNaN(a[i])) {
+                return Double.NaN;
+	    }
+
+            if (a[i] > max) {
+                max = a[i];
+	    }
+	}
+
+	return max;
+    }
+
     public static double Sum(double[] a) {
         double sum = 0.0;
 
@@ -47,6 +85,8 @@ public class Stats {
     public static void main(String[] args) {
         double[] a = { 2.4, 2.3, 2.7, 2.6 };
 
+	System.out.printf("Min: %10.2f\n", Min(a));
+	System.out.printf("Max: %10.2f\n", Max(a));
 	System.out.printf("Mean: %10.2f\n", Mean(a));
 	System.out.printf("Variance: %10.2f\n", Variance(a));
 	System.out.printf("Standard deviation: %10.2f\n", StandardDeviation(a));
