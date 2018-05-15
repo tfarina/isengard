ac39690f557ce72f7b1350b2adc079f4c9967a3e
Thiago Farina <tfransosi@gmail.com>
2018-05-15 12:16:25 -0300

    constantify the 'filename' parameter of 'readfile' function
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/trade/file.h
+++ c/apps/trade/file.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 
-char *readfile(char *filename, size_t *len);
+char *readfile(const char *filename, size_t *len);
 
 int write_file(const char *filename, const char *data, size_t size);
 
