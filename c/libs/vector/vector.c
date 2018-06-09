ac2bb4a056415201b3bf2c1bec16fa5e1af09e9d
Thiago Farina <tfransosi@gmail.com>
2018-06-09 20:31:08 -0300

    add implementation for 'vector_alloc' function
    Based on 'wget_vector_create'.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/libs/vector/vector.c
@@ -0,0 +1,63 @@
+#include "vector.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(__GNUC__)
+#define NORETURN __attribute__((__noreturn__))
+#else
+#define NORETURN
+#endif
+
+static void NORETURN fatal(const char *msg, ...)
+{
+        va_list args;
+
+        va_start(args, msg);
+
+        fprintf(stderr, "fatal: ");
+        vfprintf(stderr, msg, args);
+        fprintf(stderr, "\n");
+
+        va_end(args);
+
+        exit(EXIT_FAILURE);
+}
+
+static void *xmalloc(size_t size)
+{
+        void *ptr;
+
+        if (size == 0) {
+          fatal("zero size");
+	}
+
+        if ((ptr = malloc(size)) == NULL) {
+	  fatal("Out of memory, malloc failed, tried to allocate %lu bytes", (unsigned long)size);
+        }
+
+        return ptr;
+}
+
+static void *xcalloc(size_t nmemb, size_t size)
+{
+        void *ptr;
+
+        if ((ptr = calloc(nmemb, size)) == NULL) {
+	        fatal("xcalloc failed");
+	}
+
+        return ptr;
+}
+
+vector_t * vector_alloc(int capacity)
+{
+        vector_t *v = xcalloc(1, sizeof(vector_t));
+
+        v->entries = xmalloc(capacity * sizeof(void *));
+        v->capacity = capacity;
+
+        return v;
+}
---
+++ c/libs/vector/vector_test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "vector.h"
+
+int main(void) {
+	vector_t *vp;
+
+        vp = vector_alloc(2);
+
+        return 0;
+}
