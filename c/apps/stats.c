64d70fd3b50edb8dca5f1db2ac817a72c275ebaa
Thiago Farina <tfransosi@gmail.com>
2018-07-06 12:10:51 -0300

    add "Mean" function to stats
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/stats.c
+++ c/apps/stats.c
@@ -15,14 +15,26 @@ static double Sum(double a[], size_t size)
   return sum;
 }
 
+static double Mean(double a[], size_t size)
+{
+  double sum = 0.0;
+
+  sum = Sum(a, size);
+
+  return sum / size;
+}
+
 int main(void)
 {
   double a[] = {1, 2, 3};
   double sum = 0.0;
+  double mean = 0.0;
 
   sum = Sum(a, ARRAY_SIZE(a));
+  mean = Mean(a, ARRAY_SIZE(a));
 
   printf("%f\n", sum);
+  printf("%f\n", mean);
 
   return 0;
 }
