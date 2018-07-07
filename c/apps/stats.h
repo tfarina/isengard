30a1f2f9cbb22309b4b2520e1cb43bbbc7936665
Thiago Farina <tfransosi@gmail.com>
2018-07-07 18:43:33 -0300

    add documentation to stats functions
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/stats.h
+++ c/apps/stats.h
@@ -3,8 +3,19 @@
 
 #include <stddef.h>
 
+/**
+ * Returns the sum of the values in the input array.
+ */
 double stats_sum(double a[], size_t size);
+
+/**
+ * Returns the arithmetic mean of the entries in the input array.
+ */
 double stats_mean(double a[], size_t size);
+
+/**
+ * Returns the population variance of the entries in the input array.
+ */
 double stats_variance(double a[], size_t size);
 
 #endif /* STATS_H_ */
