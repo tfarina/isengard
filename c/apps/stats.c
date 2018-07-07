e9f399716eba0de2b516c46467c519c16dcdbcbf
Thiago Farina <tfransosi@gmail.com>
2018-07-07 18:42:15 -0300

    move the main function into stats_tests.c
    This way stats.{c,h} can be easily copied and integrated
    into other projects.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/stats.c
+++ c/apps/stats.c
@@ -1,8 +1,4 @@
-#include <stdio.h>
-
 #include "stats.h"
-
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
 double stats_sum(double a[], size_t size)
 {
@@ -38,22 +34,4 @@ double stats_variance(double a[], size_t size)
   }
 
   return sum / (size - 1);
-}
-
-int main(void)
-{
-  double a[] = {1.6, 2.7, 3.8, 4.9};
-  double sum = 0.0;
-  double mean = 0.0;
-  double variance = 0.0;
-
-  sum = stats_sum(a, ARRAY_SIZE(a));
-  mean = stats_mean(a, ARRAY_SIZE(a));
-  variance = stats_variance(a, ARRAY_SIZE(a));
-
-  printf("%f\n", sum);
-  printf("%f\n", mean);
-  printf("%f\n", variance);
-
-  return 0;
 }
---
+++ c/apps/stats_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "stats.h"
+
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
+int main(void)
+{
+  double a[] = {1.6, 2.7, 3.8, 4.9};
+  double sum = 0.0;
+  double mean = 0.0;
+  double variance = 0.0;
+
+  sum = stats_sum(a, ARRAY_SIZE(a));
+  mean = stats_mean(a, ARRAY_SIZE(a));
+  variance = stats_variance(a, ARRAY_SIZE(a));
+
+  printf("%f\n", sum);
+  printf("%f\n", mean);
+  printf("%f\n", variance);
+
+  return 0;
+}
