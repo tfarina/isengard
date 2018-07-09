aec6cd5c10abd793b0dc388df2f4a2eb63ce0210
Thiago Farina <tfransosi@gmail.com>
2018-07-09 07:05:22 -0300

    add 'bin2dec' function
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/bin.c
+++ c/apps/bin.c
@@ -2,6 +2,7 @@
  * Code from: www.geeksforgeeks.org/binary-representation-of-a-given-number
  */
 
+#include <math.h>
 #include <stdio.h>
 
 /* To print binary representation of unsigned integer, start from 31th bit,
@@ -15,9 +16,32 @@ static void bin(unsigned n) {
   }
 }
 
+/**
+ * Converts a binary number to decimal.
+ *
+ * Algorithm from http://www.c4learn.com/c-programs/program-to-convert-binary-to-decimal.html
+ *
+ */
+static long int bin2dec(long int num) {
+  long int rem, sum = 0, power = 0;
+
+  while (num > 0) {
+    rem = num % 10;
+    num = num / 10;
+    sum = sum + rem * pow(2, power);
+    power++;
+  }
+
+  return sum;
+}
+
 int main(int argc, char **argv) {
+  long int five = 0b101;
+
   bin(120);
   printf("\n");
+
+  printf("%ld\n", bin2dec(five));
 
   return 0;
 }
