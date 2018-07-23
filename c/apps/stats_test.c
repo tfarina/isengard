7596f0d950edc00969f1a1ed04b62b266e5ada16
Thiago Farina <tfransosi@gmail.com>
2018-07-23 07:10:44 -0300

    stats_test: print the array before displaying the calculations
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/stats_test.c
+++ c/apps/stats_test.c
@@ -4,6 +4,17 @@
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
+static void print_array(double a[], size_t size)
+{
+  size_t i;
+
+  for (i = 0; i < size; i++) {
+    printf("%f ", a[i]);
+  }
+
+  printf("\n\n");
+}
+
 int main(void)
 {
   double a[] = {1.6, 2.7, 3.8, 4.9};
@@ -12,15 +23,18 @@ int main(void)
   double variance = 0.0;
   double stdev = 0.0;
 
+  printf("Array:\n");
+  print_array(a, ARRAY_SIZE(a));
+
   sum = stats_sum(a, ARRAY_SIZE(a));
   avg = stats_average(a, ARRAY_SIZE(a));
   variance = stats_var(a, ARRAY_SIZE(a));
   stdev = stats_stdev(a, ARRAY_SIZE(a));
 
-  printf("sum %f\n", sum);
-  printf("average %f\n", avg);
-  printf("variance %f\n", variance);
-  printf("standard deviation %f\n", stdev);
+  printf("The sum is %f\n", sum);
+  printf("The average is %f\n", avg);
+  printf("The variance is %f\n", variance);
+  printf("The standard deviation is %f\n", stdev);
 
   return 0;
 }
