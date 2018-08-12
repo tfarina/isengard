84e0685f54bd4c19784c2ed2f0f30e4c07c931ab
Thiago Farina <tfransosi@gmail.com>
2018-08-12 15:04:46 -0300

    network/log.h: move macros into their own header file
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/log.h
+++ c/apps/network/log.h
@@ -1,17 +1,7 @@
 #ifndef LOG_H_
 #define LOG_H_
 
-#if defined(__GNUC__)
-#define NORETURN __attribute__((__noreturn__))
-#elif defined(_MSC_VER)
-#define NORETURN __declspec(noreturn)
-#else
-#define NORETURN
-#endif
-
-#ifndef ATTRIBUTE_PRINTF
-#define ATTRIBUTE_PRINTF(m, n) __attribute__ ((__format__ (__printf__, m, n)))
-#endif
+#include "macros.h"
 
 NORETURN void fatal(const char *fmt, ...) ATTRIBUTE_PRINTF(1, 2);
 void error(const char *fmt, ...) ATTRIBUTE_PRINTF(1, 2);
