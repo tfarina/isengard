516e8df08cf80f403fbfdbac4f434ab428d5ae1f
Thiago Farina <tfransosi@gmail.com>
2018-09-03 07:20:46 -0300

    add 'GCC_VERSION_AT_LEAST' macro
    Copied from https://github.com/rockdaboot/wget2.
    
    It will be used to enhace the definition of NORETURN macro.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/libs/buffer/macros.h
+++ c/libs/buffer/macros.h
@@ -1,6 +1,12 @@
 #ifndef MACROS_H_
 #define MACROS_H_
 
+#if defined __GNUC__ && defined __GNUC_MIOR__
+#define GCC_VERSION_AT_LEAST(major, minor) ((__GNUC__ > (major) || (__GNUC__ == (major) && __GNUC__ >= (minor)))
+#else
+#define GCC_VERSION_AT_LEAST(major, minor) 0
+#endif
+
 #if defined(__GNUC__)
 #define NORETURN __attribute__((__noreturn__))
 #else
