7fa8e378b8b73c6d18cdaac7c0be0851a8bba0cf
Thiago Farina <tfransosi@gmail.com>
2018-09-09 07:37:57 -0300

    have only 'name' and 'url' entries in the dialog
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/gui/gtk/entry.c
+++ c/apps/gui/gtk/entry.c
@@ -7,43 +7,35 @@ int main(int argc, char *argv[])
   GtkWidget *table;
 
   GtkWidget *name_label;
-  GtkWidget *email_label;
-  GtkWidget *password_label;
+  GtkWidget *url_label;
 
   GtkWidget *name_entry;
-  GtkWidget *email_entry;
-  GtkWidget *password_entry;
+  GtkWidget *url_entry;
 
   gtk_init(&argc, &argv);
 
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-  gtk_window_set_title(GTK_WINDOW(window), "Register");
+  gtk_window_set_title(GTK_WINDOW(window), "Create");
   gtk_container_set_border_width(GTK_CONTAINER(window), 10);
 
   table = gtk_table_new(3, 2, FALSE);
   gtk_container_add(GTK_CONTAINER(window), table);
 
   name_label = gtk_label_new("Name");
-  email_label = gtk_label_new("Email");
-  password_label = gtk_label_new("Password");
+  url_label = gtk_label_new("URL");
 
   gtk_table_attach(GTK_TABLE(table), name_label, 0, 1, 0, 1,
 		   GTK_FILL | GTK_SHRINK, GTK_FILL | GTK_SHRINK, 5, 5);
-  gtk_table_attach(GTK_TABLE(table), email_label, 0, 1, 1, 2,
-		   GTK_FILL | GTK_SHRINK, GTK_FILL | GTK_SHRINK, 5, 5);
-  gtk_table_attach(GTK_TABLE(table), password_label, 0, 1, 2, 3,
+  gtk_table_attach(GTK_TABLE(table), url_label, 0, 1, 1, 2,
 		   GTK_FILL | GTK_SHRINK, GTK_FILL | GTK_SHRINK, 5, 5);
 
   name_entry = gtk_entry_new();
-  email_entry = gtk_entry_new();
-  password_entry = gtk_entry_new();
+  url_entry = gtk_entry_new();
 
   gtk_table_attach(GTK_TABLE(table), name_entry, 1, 2, 0, 1,
 		   GTK_FILL | GTK_SHRINK, GTK_FILL | GTK_SHRINK, 5, 5);
-  gtk_table_attach(GTK_TABLE(table), email_entry, 1, 2, 1, 2,
-		   GTK_FILL | GTK_SHRINK, GTK_FILL | GTK_SHRINK, 5, 5);
-  gtk_table_attach(GTK_TABLE(table), password_entry, 1, 2, 2, 3,
+  gtk_table_attach(GTK_TABLE(table), url_entry, 1, 2, 1, 2,
 		   GTK_FILL | GTK_SHRINK, GTK_FILL | GTK_SHRINK, 5, 5);
 
   gtk_widget_show_all(window);
