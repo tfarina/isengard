48e103ad76fe65161550dac4a8f6272114f81a2d
Thiago Farina <tfransosi@gmail.com>
2018-09-18 16:06:03 -0300

    add a simple program that shows the keywords of C language
    Here is a list of keywords recognized by ANSI C89.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/ckeywords.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("auto\n");
+  printf("break\n");
+  printf("case\n");
+  printf("char\n");
+  printf("const\n");
+  printf("continue\n");
+  printf("default\n");
+  printf("do\n");
+  printf("double\n");
+  printf("else\n");
+  printf("enum\n");
+  printf("extern\n");
+  printf("float\n");
+  printf("for\n");
+  printf("goto\n");
+  printf("if\n");
+  printf("int\n");
+  printf("long\n");
+  printf("register\n");
+  printf("return\n");
+  printf("short\n");
+  printf("signed\n");
+  printf("sizeof\n");
+  printf("static\n");
+  printf("struct\n");
+  printf("switch\n");
+  printf("typedef\n");
+  printf("union\n");
+  printf("unsigned\n");
+  printf("void\n");
+  printf("volatile\n");
+  printf("while\n");
+
+  return 0;
+}
