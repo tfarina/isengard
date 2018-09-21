f6ec585f7efed8bdb55dad2d6c00ab8d34bd9b64
Thiago Farina <tfransosi@gmail.com>
2018-09-21 05:22:40 -0300

    rename 'bin' function to 'dec2bin'
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/bin.c
+++ c/apps/bin.c
@@ -1,16 +1,15 @@
-/*
- * Code from: www.geeksforgeeks.org/binary-representation-of-a-given-number
- */
-
 #include <stdio.h>
 
 #include "math_util.h"
 
-/* To print binary representation of unsigned integer, start from 31th bit,
+/**
+ * To print binary representation of unsigned integer, start from 31th bit,
  * check wheter 31th bit is ON or OFF, if it is ON print "1" else print "0".
  * Do this for all bits from 31 to 0.
+ *
+ * Code from: www.geeksforgeeks.org/binary-representation-of-a-given-number
  */
-static void bin(unsigned n) {
+static void dec2bin(unsigned n) {
   unsigned i;
   for (i = 1 << 31; i > 0; i = i / 2) {
     (n & i) ? printf("1") : printf("0");
@@ -20,7 +19,7 @@ static void bin(unsigned n) {
 int main(int argc, char **argv) {
   long int five = 0b101;
 
-  bin(120);
+  dec2bin(120);
   printf("\n");
 
   printf("%ld\n", bin2dec(five));
