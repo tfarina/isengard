331f22edaa2f0684306ae00bcc6a6e514fb4f75f
Thiago Farina <tfransosi@gmail.com>
2018-09-21 07:54:37 -0300

    bit: show the powers of 2 in binary form
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/bit.c
+++ c/apps/bit.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 
 #define BITMASK(index) (1 << (index))
 
@@ -13,6 +14,27 @@ int main(void) {
   const uint8_t some_bits = 0b10110010;
   const uint8_t more_bits = 0b01011110;
   const uint8_t combined_bits = some_bits | more_bits; /* equals to 0b11111110 */ 
+
+  /* Powers of 2 in binary */
+  const uint8_t zero                  = 0b00000000;
+  const uint8_t one                   = 0b00000001;
+  const uint8_t two                   = 0b00000010;
+  const uint8_t four                  = 0b00000100;
+  const uint8_t eight                 = 0b00001000;
+  const uint8_t sixteen               = 0b00010000;
+  const uint8_t thirtytwo             = 0b00100000;
+  const uint8_t sixtyfour             = 0b01000000;
+  const uint8_t onehundredtwentyeight = 0b10000000;
+
+  printf("%d\n", zero);
+  printf("%d\n", one);
+  printf("%d\n", two);
+  printf("%d\n", four);
+  printf("%d\n", eight);
+  printf("%d\n", sixteen);
+  printf("%d\n", thirtytwo);
+  printf("%d\n", sixtyfour);
+  printf("%d\n", onehundredtwentyeight);
 
   return 0;
 }
