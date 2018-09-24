283bd743a40dafef660f7a2a77e1f7e509257e6f
Thiago Farina <tfransosi@gmail.com>
2018-09-24 15:53:33 -0300

    stats.h: document the 'a' parameter
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/stats.h
+++ c/apps/stats.h
@@ -6,32 +6,44 @@
 /**
  * Returns the minimum of the entries in the specified array, or NAN if the
  * array is empty.
+ *
+ * @param a The array.
  */
 double stats_min(double a[], size_t size);
 
 /**
  * Returns the maximum of the entries in the specified array, or NAN if the
  * array is empty.
+ *
+ * @param a The array.
  */
 double stats_max(double a[], size_t size);
 
 /**
  * Returns the sum of the values in the specified array.
+ *
+ * @param a The array.
  */
 double stats_sum(double a[], size_t size);
 
 /**
  * Returns the arithmetic mean of the entries in the specified array.
+ *
+ * @param a The array.
  */
 double stats_average(double a[], size_t size);
 
 /**
  * Returns the sample variance of the entries in the specified array.
+ *
+ * @param a The array.
  */
 double stats_var(double a[], size_t size);
 
 /**
  * Returns the sample standard deviation of the entries in the specified array.
+ *
+ * @param a The array.
  */
 double stats_stdev(double a[], size_t size);
 
