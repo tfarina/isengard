1ab993ac8c8cd7c054de0adffdffd8ae2568efa7
Thiago Farina <tfransosi@gmail.com>
2018-12-02 10:17:55 -0200

    md5: add cflags to build.sh script
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/md5/build.sh
+++ c/apps/md5/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-clang -c md5.c -o md5.o
-clang -c main.c -o main.o
+# Compile source files into object files.
+clang -g -std=c99 -pedantic -Wall -O0 -c md5.c -o md5.o
+clang -g -std=c99 -pedantic -Wall -O0 -c main.c -o main.o
+
+# Link the object files into an executable.
 clang -o md5 md5.o main.o
