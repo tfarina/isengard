0dc6feadfc5764c6215816a983395130063b3ad9
Thiago Farina <tfransosi@gmail.com>
2019-01-23 15:13:06 -0200

    umgr: add the basic widgets to the 'person editor' dialog
    Just the basic UI, no functionality provided yet.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/umgr/src/addrbook.c
+++ c/apps/umgr/src/addrbook.c
@@ -35,12 +35,66 @@ GtkToolItem *delete_item = NULL;
 static void new_item_cb(GtkWidget *widget, gpointer data)
 {
   GtkWidget *new_window;
+  GtkWidget *vbox;
+  GtkWidget *table;
+  GtkWidget *label;
+  GtkWidget *fname_entry;
+  GtkWidget *lname_entry;
+  GtkWidget *confirm_area;
+  GtkWidget *cancel_btn;
+  GtkWidget *ok_btn;
 
   new_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW(new_window), "Create New Person");
-  gtk_window_set_position(GTK_WINDOW(new_window), GTK_WIN_POS_CENTER);
+  gtk_window_set_transient_for(GTK_WINDOW(new_window), GTK_WINDOW(window));
   gtk_window_set_modal(GTK_WINDOW(new_window), TRUE);
-  gtk_widget_show(new_window);
+
+  vbox = gtk_vbox_new(FALSE, 6);
+  gtk_container_add(GTK_CONTAINER(new_window), vbox);
+
+  table = gtk_table_new(3, 2, FALSE);
+  gtk_container_set_border_width(GTK_CONTAINER(table), 4 );
+  gtk_table_set_row_spacings(GTK_TABLE(table), 4);
+  gtk_table_set_col_spacings(GTK_TABLE(table), 4);
+  gtk_box_pack_start(GTK_BOX(vbox), table, FALSE, FALSE, 0);
+
+  fname_entry = gtk_entry_new();
+  lname_entry = gtk_entry_new();
+
+  /* First row. */
+  label = gtk_label_new("First Name");
+  gtk_table_attach(GTK_TABLE(table), label, 0, 1, 0, 1,
+		   GTK_FILL, 0, 0, 0);
+  gtk_misc_set_alignment(GTK_MISC(label), 0, 0.5);
+
+  gtk_table_attach(GTK_TABLE(table), fname_entry, 1, 2, 0, 1,
+		   GTK_EXPAND | GTK_SHRINK | GTK_FILL, 0, 0, 0);
+
+  /* Second row. */
+  label = gtk_label_new("Last Name");
+  gtk_table_attach(GTK_TABLE(table), label, 0, 1, 1, 2,
+		   GTK_FILL, 0, 0, 0);
+  gtk_misc_set_alignment(GTK_MISC(label), 0, 0.5);
+
+  gtk_table_attach(GTK_TABLE(table), lname_entry, 1, 2, 1, 2,
+		   GTK_EXPAND | GTK_SHRINK | GTK_FILL, 0, 0, 0);
+
+  confirm_area = gtk_hbutton_box_new();
+  gtk_button_box_set_layout(GTK_BUTTON_BOX(confirm_area), GTK_BUTTONBOX_END);
+  gtk_box_set_spacing(GTK_BOX(confirm_area), 5);
+
+  cancel_btn = gtk_button_new_from_stock(GTK_STOCK_CANCEL);
+  gtk_box_pack_start(GTK_BOX(confirm_area), cancel_btn, TRUE, TRUE, 0);
+
+  ok_btn = gtk_button_new_from_stock(GTK_STOCK_OK);
+  gtk_box_pack_start(GTK_BOX(confirm_area), ok_btn, TRUE, TRUE, 0);
+
+  gtk_box_pack_end(GTK_BOX(vbox), confirm_area, FALSE, FALSE, 0);
+
+  g_signal_connect_swapped(cancel_btn, "clicked",
+			   G_CALLBACK(gtk_widget_destroy), new_window);
+
+  gtk_widget_show_all(new_window);
 }
 
 static void list_selection_changed_cb(GtkTreeSelection *selection, gpointer data)
