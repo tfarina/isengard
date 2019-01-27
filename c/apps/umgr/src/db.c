fa5bc94ad6c6af82dc26ddcc0b5262d87461107c
Thiago Farina <tfransosi@gmail.com>
2019-01-27 12:12:19 -0200

    umgr: use sqlite3_errmsg()
    It reports English-language text that describes the error rather than
    text that describes the 'result code' when using sqlite3_errstr().
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/umgr/src/db.c
+++ c/apps/umgr/src/db.c
@@ -9,9 +9,9 @@ int db_open(const char *filename, sqlite3 **db) {
 
   rc = sqlite3_open(filename, db);
   if (rc != SQLITE_OK) {
-    fprintf(stderr, "error opening SQLite database %s: %s\n", filename, sqlite3_errstr(rc));
+    fprintf(stderr, "error opening SQLite database %s: %s\n", filename, sqlite3_errmsg(*db));
     if ((rc = sqlite3_close(*db)) != SQLITE_OK) {
-      fprintf(stderr, "error closing SQLite database: %s\n", sqlite3_errstr(rc));
+      fprintf(stderr, "error closing SQLite database: %s\n", sqlite3_errmsg(*db));
     }
     return rc;
   }
@@ -23,6 +23,6 @@ void db_close(sqlite3 *db) {
   int rc;
 
   if ((rc = sqlite3_close(db)) != SQLITE_OK) {
-    fprintf(stderr, "error closing SQLite database: %s\n", sqlite3_errstr(rc));
+    fprintf(stderr, "error closing SQLite database: %s\n", sqlite3_errmsg(db));
   }
 }
