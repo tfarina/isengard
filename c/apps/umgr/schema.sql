1ea6167837bb6647b8024f6c1d460e417cb8ca3c
Thiago Farina <tfransosi@gmail.com>
2019-02-01 14:56:06 -0200

    umgr: update schema
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/umgr/schema.sql
+++ c/apps/umgr/schema.sql
@@ -1,17 +1,17 @@
 /**
  * SQLITE DATABASE SCHEMA FOR THE 'umgr' APP.
  *
- * To recreate the database: sqlite3 users.db < schema.sql
+ * To recreate the database: sqlite3 abdb.sqlite3 < schema.sql
  */
 
 PRAGMA foreign_keys=OFF;
 
 BEGIN TRANSACTION;
 
-/* User table --------------------------------------------------------------- */
+/* Contacts table ----------------------------------------------------------- */
 
-CREATE TABLE user (
-  uid INTEGER PRIMARY KEY,
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY,
   fname TEXT,
   lname TEXT,
   email TEXT
@@ -23,6 +23,6 @@ CREATE TABLE user (
  * Populating with some values.
  */
 
-INSERT INTO "user" VALUES (1, 'John', 'Smith', 'jsmith@yahoo.com');
+INSERT INTO "contacts" VALUES (1, 'John', 'Doe', 'jdoe@yahoo.com');
 
 COMMIT;
