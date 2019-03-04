1d86c8caae034ab5517bbc56a43af37f07f51d8b
Thiago Farina <tfransosi@gmail.com>
2019-03-04 16:29:53 -0300

    experiments: add a schema for a simple 'contacts' table
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/experiments/scripts/setup_mysql.sh
+++ c/apps/experiments/scripts/setup_mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cat <<MYSQLSCRIPT | mysql -u root -p
-CREATE DATABASE IF NOT EXISTS classicmodels;
-GRANT ALL ON classicmodels.* TO 'testuser'@'localhost' IDENTIFIED BY 'testpass';
+CREATE DATABASE IF NOT EXISTS sab;
+GRANT ALL ON sab.* TO 'testuser'@'localhost' IDENTIFIED BY 'testpass';
 FLUSH PRIVILEGES;
 MYSQLSCRIPT
