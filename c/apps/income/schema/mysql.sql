921d201b11300e04475b237a552fcc8da232f0a6
Thiago Farina <tfransosi@gmail.com>
2019-03-06 18:24:08 -0300

    income: move 'exchange' table schema into mysql.sql file
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/income/schema/mysql.sql
+++ c/apps/income/schema/mysql.sql
@@ -802,3 +802,19 @@ insert into symbol_code (symbol, code) values ('CRFB', 'CRFB3');
 insert into symbol_code (symbol, code) values ('ATOM', 'ATOM3');
 insert into symbol_code (symbol, code) values ('AZEV', 'AZEV3');
 insert into symbol_code (symbol, code) values ('AZEV', 'AZEV4');
+
+CREATE TABLE `exchange` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `abbrev` varchar(32) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `city` varchar(255) NULL,
+  `country` varchar(255) NULL,
+  `currency` varchar(64) NULL,
+  `timezone_offset` time NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+insert into exchange (abbrev, name, city, country, currency, created_date, last_updated_date)
+values ('B3', 'Brasil, Bolsa, Balcão', 'São Paulo', 'Brazil', 'BRL', NOW(), NOW());
