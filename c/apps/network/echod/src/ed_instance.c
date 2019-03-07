9d786bb42223f684790a806442b216de6c556cd4
Thiago Farina <tfransosi@gmail.com>
2019-03-07 14:51:19 -0300

    echod: set the default value for 'port'
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/ed_instance.c
+++ c/apps/network/echod/src/ed_instance.c
@@ -4,5 +4,6 @@ void ed_instance_init(ed_instance_t *instance) {
   instance->daemonize = 0;
   instance->log_filename = NULL;
   instance->pid = getpid();
+  instance->port = ED_TCP_PORT;
   instance->backlog = ED_BACKLOG;
 }
