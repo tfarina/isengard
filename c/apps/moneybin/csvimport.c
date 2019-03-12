06ad5f946b9b33b0d371765ebc4a0be988150d8a
Thiago Farina <tfransosi@gmail.com>
2019-03-12 16:20:37 -0300

    csvimport.c: if 'csv_parse' fails do some cleanup work
    * Free the csv parser
    * Close the file pointer
    * Return from the function
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/csvimport.c
+++ c/apps/moneybin/csvimport.c
@@ -193,6 +193,9 @@ static void csv_read_quotes(char const *filename, vector_t *ticks) {
   while ((bytes_read = fread(buf, sizeof(char), sizeof(buf), fp)) > 0) {
     if (csv_parse(&parser, buf, bytes_read, csv_new_field_cb, csv_new_row_cb, &state) != bytes_read) {
       fprintf(stderr, "Error while parsing %s: %s\n", filename, csv_strerror(csv_error(&parser)));
+      csv_free(&parser);
+      fclose(fp);
+      return;
     }
   }
 
