86af97298b85b43cc9f9e97dda02e5af2031a447
Thiago Farina <tfransosi@gmail.com>
2019-03-14 15:56:04 -0300

    csvdump.c: rework to use 'vector_t' data type
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/csvdump.c
+++ c/apps/moneybin/csvdump.c
@@ -1,21 +1,224 @@
-/*https://www.quora.com/How-do-I-read-a-csv-file-and-then-use-those-fields-to-create-an-array-structure-in-C*/
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "third_party/libcsv/csv.h"
-#include "csv_helper.h"
-#include "file.h"
-#include "stock.h"
 #include "strutils.h"
+#include "vector.h"
+#include "third_party/libcsv/csv.h"
+
+typedef enum result_code_e {
+  RC_OK,
+  RC_ERROR,
+} result_code_t;
+
+typedef struct quote_s {
+  char *date;
+  double open;
+  double high;
+  double low;
+  double close;
+  double adj_close;
+  int volume;
+} quote_t;
+
+typedef struct csv_state_s {
+  /**
+   * The next field (column number) to parse. */
+  int field;
+
+  /**
+   * Determines wheter there has been any error during parsing.
+   */
+  int any_error;
+
+  /**
+   * Data of the quote that we are currently parsing.
+   */
+  quote_t cur_quote;
+
+  /**
+   * List of quotes parsed so far.
+   */
+  vector_t *quotes;
+} csv_state_t;
+
+typedef enum csv_column_e {
+  CSV_COLUMN_DATE,
+  CSV_COLUMN_OPEN,
+  CSV_COLUMN_HIGH,
+  CSV_COLUMN_LOW,
+  CSV_COLUMN_CLOSE,
+  CSV_COLUMN_ADJ_CLOSE,
+  CSV_COLUMN_VOLUME
+} csv_column_t;
+
+static int ignore_first_line = 1;
+
+static char *parse_str(char const *field, size_t length, result_code_t *rc) {
+  if (length > 0) {
+    char *str = (char *)malloc((length + 1) * sizeof(char));
+    strncpy(str, field, length + 1);
+
+    *rc = RC_OK;
+    return str;
+  } else {
+    *rc = RC_ERROR;
+    return NULL;
+  }
+}
+
+static double parse_price(char const *field, size_t length, result_code_t *rc) {
+  char *endptr;
+  double price;
+
+  price = (double)strtod(field, &endptr);
+  if (length > 0 && (endptr == NULL || *endptr == '\0')) {
+    *rc = RC_OK;
+    return price;
+  }
+
+  *rc = RC_ERROR;
+  return -1.0f;
+}
+
+/**
+ * This functions is called each time a new csv field has been found.
+ * It is responsible for determining which field is being parsed, and
+ * updating the cur_quote object.
+ */
+static void csv_new_field_cb(void *field, size_t field_length, void *data) {
+  csv_state_t *state = (csv_state_t *)data;
+  char *buffer;
+  result_code_t rc;
+
+  if (state->field == -1) {
+    return;
+  }
+
+  if (ignore_first_line) {
+    return;
+  }
+
+  buffer = (char *)malloc((field_length + 1) * sizeof(char));
+  strncpy(buffer, field, field_length);
+  buffer[field_length] = '\0';
+
+  switch (state->field) {
+  case CSV_COLUMN_DATE:
+    state->cur_quote.date = parse_str(buffer, field_length, &rc);
+    break;
+  case CSV_COLUMN_OPEN:
+    state->cur_quote.open = parse_price(buffer, field_length, &rc);
+    break;
+  case CSV_COLUMN_HIGH:
+    state->cur_quote.high = parse_price(buffer, field_length, &rc);
+    break;
+  case CSV_COLUMN_LOW:
+    state->cur_quote.low = parse_price(buffer, field_length, &rc);
+    break;
+  case CSV_COLUMN_CLOSE:
+    state->cur_quote.close = parse_price(buffer, field_length, &rc);
+    break;
+  case CSV_COLUMN_ADJ_CLOSE:
+    state->cur_quote.adj_close = parse_price(buffer, field_length, &rc);
+    break;
+  case CSV_COLUMN_VOLUME:
+    state->cur_quote.volume = atoi(buffer);
+    break;
+  default:
+    rc = RC_ERROR;
+    break;
+  }
+
+  free(buffer);
+
+  if (rc == RC_OK) {
+    state->field++;
+  } else {
+    state->any_error = 1;
+    state->field = -1;
+  }
+}
+
+/**
+ * This functions is called each time a new row has been found.
+ */
+static void csv_new_row_cb(int c, void *data) {
+  csv_state_t *state = (csv_state_t *)data;
+
+  if (ignore_first_line) {
+    ignore_first_line = 0;
+    return;
+  }
+
+  if (state->field != -1) {
+    /**
+     * This makes a copy of cur_quote, and smells like a hack. But that
+     * is because 'vector_push_back' does not make a copy and the lifetime
+     * of cur_quote is short, so it does not outlive the call to
+     * csv_read_quotes and without a copy we end up with pointers pointing
+     * to garbage qutotes.
+     */
+    quote_t *copy = malloc(sizeof(quote_t));
+    copy->date = state->cur_quote.date;
+    copy->open = state->cur_quote.open;
+    copy->high = state->cur_quote.high;
+    copy->low = state->cur_quote.low;
+    copy->close = state->cur_quote.close;
+    copy->adj_close = state->cur_quote.adj_close;
+    copy->volume = state->cur_quote.volume;
+    vector_push_back(state->quotes, copy);
+  }
+
+  state->field = 0;
+}
+
+static void csv_read_quotes(char const *filename, vector_t *quotes) {
+  FILE* fp;
+  struct csv_parser parser;
+  char buf[1024];
+  size_t bytes_read;
+  csv_state_t state;
+
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Failed to open file %s: %s\n", filename, strerror(errno));
+    return;
+  }
+
+  memset(&state, 0, sizeof(csv_state_t));
+  state.quotes = quotes;
+
+  if (csv_init(&parser, CSV_STRICT | CSV_APPEND_NULL | CSV_STRICT_FINI) != 0) {
+    fprintf(stderr, "Failed to initialize csv parser\n");
+    return;
+  }
+
+  while ((bytes_read = fread(buf, sizeof(char), sizeof(buf), fp)) > 0) {
+    if (csv_parse(&parser, buf, bytes_read, csv_new_field_cb, csv_new_row_cb, &state) != bytes_read) {
+      fprintf(stderr, "Error while parsing %s: %s\n", filename, csv_strerror(csv_error(&parser)));
+      csv_free(&parser);
+      fclose(fp);
+      return;
+    }
+  }
+
+  csv_fini(&parser, csv_new_field_cb, csv_new_row_cb, &state);
+  csv_free(&parser);
+
+  if (ferror(fp)) {
+    fprintf(stderr, "error reading file %s\n", filename);
+    fclose(fp);
+    return;
+  }
+
+  fclose(fp);
+}
 
 int main(int argc, char **argv) {
-  char *csvdata;
-  size_t csvdata_len;
-  int rc;
-  struct csv_parser parser;
-  stock_info_t stock;
-  size_t bytes_processed;
+  char *filename;
+  vector_t *quotes;
   size_t i;
 
   if (argc != 2) {
@@ -23,53 +226,26 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  rc = read_file(argv[1], &csvdata, &csvdata_len);
-  if (rc < 0) {
-    return 1;
+  filename = f_strdup(argv[1]);
+
+  quotes = vector_alloc(256);
+
+  csv_read_quotes(filename, quotes);
+
+  for (i = 0; i < vector_size(quotes); i++) {
+    quote_t *quote = vector_get(quotes, i);
+
+    printf("%s,", quote->date);
+    printf("%.4lf,", quote->open);
+    printf("%.4lf,", quote->high);
+    printf("%.4lf,", quote->low);
+    printf("%.4lf,", quote->close);
+    printf("%.4lf,", quote->adj_close);
+    printf("%d\n", quote->volume);
   }
 
-  if (csv_init(&parser, CSV_APPEND_NULL) != 0) {
-    free(csvdata);
-    fprintf(stderr, "failed to initialize CSV parser\n");
-    return 1;
-  }
- 
-  memset((void *)&stock, 0, sizeof(stock_info_t));
-  stock_ticks_alloc(&stock, 2);
-  if (stock.ticks == NULL) {
-    fprintf(stderr, "failed to allocate %zu bytes for stock data\n",
-	    stock.ticks_capacity * sizeof(stock_tick_t));
-    free(csvdata);
-    return 1;
-  }
- 
-  bytes_processed = csv_parse(&parser, (void*)csvdata, csvdata_len,
-                              csv_column_cb, csv_row_cb, &stock);
-  rc = csv_fini(&parser, csv_column_cb, csv_row_cb, &stock);
-  free(csvdata);
- 
-  if (stock.error || rc != 0 || bytes_processed < csvdata_len) {
-    fprintf(stderr,
-            "read %zu bytes out of %zu: %s\n",
-	    bytes_processed, csvdata_len, csv_strerror(csv_error(&parser)));
-    return 1;
-  }
-
-  csv_free(&parser);
-
-  for (i = 0; i < stock.ticks_length; i++) {
-    stock_tick_t *tick = stock.ticks + i;
-
-    printf("%s,", tick->date);
-    printf("%.4lf,", tick->open);
-    printf("%.4lf,", tick->high);
-    printf("%.4lf,", tick->low);
-    printf("%.4lf,", tick->close);
-    printf("%.4lf,", tick->adj_close);
-    printf("%d\n", tick->volume);
-  }
-
-  free(stock.ticks);
+  vector_free(quotes);
+  free(filename);
  
   return 0;
 }
