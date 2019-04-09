c3543c1f989b39328eb14f5a000622871f99f826
Thiago Farina <tfransosi@gmail.com>
2019-04-09 17:06:01 -0300

    moneybin: add a pointer to the database driver to dba_conn_t
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/dba.h
+++ c/apps/moneybin/dba.h
@@ -1,24 +1,31 @@
 #ifndef MB_DBA_H_
 #define MB_DBA_H_
 
+struct dba_conn_s;
+
+typedef struct dba_driver_ops_s {
+  char *name;
+
+  int (*alloc)(struct dba_conn_s *conn);
+  int (*free)(struct dba_conn_s *conn);
+
+  int (*connect)(struct dba_conn_s *conn);
+  int (*disconnect)(struct dba_conn_s *conn);
+} dba_driver_ops_t;
+
+typedef struct dba_driver_s {
+  dba_driver_ops_t *ops;
+} dba_driver_t;
+
 typedef struct dba_conn_s {
   char const *host;
   char const *user;
   char const *password;
   char const *dbname;
   int unsigned port;
+  dba_driver_t *driver;
   void *data;
 } dba_conn_t;
-
-typedef struct dba_driver_ops_s {
-  char *name;
-
-  int (*alloc)(dba_conn_t *conn);
-  int (*free)(dba_conn_t *conn);
-
-  int (*connect)(dba_conn_t *conn);
-  int (*disconnect)(dba_conn_t *conn);
-} dba_driver_ops_t;
 
 int dba_conn_alloc(dba_conn_t *conn);
 int dba_conn_free(dba_conn_t *conn);
