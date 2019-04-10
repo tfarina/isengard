6484bc6735fa92a4696cd869229074486e775c2d
Thiago Farina <tfransosi@gmail.com>
2019-04-10 17:19:56 -0300

    moneybin: declare dba_s structure before dba_ops_s
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/dba.h
+++ c/apps/moneybin/dba.h
@@ -2,18 +2,19 @@
 #define MB_DBA_H_
 
 typedef struct dba_s dba_t;
+typedef struct dba_ops_s dba_ops_t;
 
-typedef struct dba_ops_s {
+struct dba_s {
+  dba_ops_t *ops;
+  void *data;
+};
+
+struct dba_ops_s {
   int (*init)(dba_t *handle);
   int (*connect)(dba_t *handle, char const *host, int unsigned port,
                  char const *username, char const *password, char const *dbname);
   int (*disconnect)(dba_t *handle);
   int (*query)(dba_t *handle, char const *query, long unsigned length);
-} dba_ops_t;
-
-struct dba_s {
-  dba_ops_t *ops;
-  void *data;
 };
 
 int dba_init(dba_t **handle, char const *backend);
