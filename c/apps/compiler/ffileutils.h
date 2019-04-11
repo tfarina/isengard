9a5f6663fb0d1be13f90598e86e91e7cf48c45c5
Thiago Farina <tfransosi@gmail.com>
2019-04-11 16:46:34 -0300

    compiler: move 'read_file' function into its own module
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/compiler/ffileutils.h
@@ -0,0 +1,9 @@
+#ifndef FFILEUTILS_H_
+#define FFILEUTILS_H_
+
+#include <stddef.h>
+
+char *read_file(const char *filename, size_t *rlen);
+
+#endif  /* FFILEUTILS_H_ */
+
--- c/apps/compiler/lexer.c
+++ c/apps/compiler/lexer.c
@@ -4,55 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static char *read_file(const char *filename, size_t *rlen)
-{
-  FILE *fp;
-  long len;
-  char *buf;
-  size_t bytes_read;
-
-  if ((fp = fopen(filename, "rb")) == NULL) {
-    fprintf(stderr, "error opening %s file\n", filename);
-    return NULL;
-  }
-
-  if (fseek(fp, 0, SEEK_END) == -1) {
-    fprintf(stderr, "unable to fseek file %s\n", filename);
-    fclose(fp);
-    return NULL;
-  }
-
-  if ((len = ftell(fp)) == -1) {
-    fprintf(stderr, "unable to ftell file %s\n", filename);
-    fclose(fp);
-    return NULL;
-  }
-
-  if (fseek(fp, 0, SEEK_SET) == -1) {
-    fprintf(stderr, "unable to fseek file %s\n", filename);
-    fclose(fp);
-    return NULL;
-  }
-
-  if ((buf = malloc(len)) == NULL) {
-    fprintf(stderr, "malloc failed (file too large?): %s\n", strerror(errno));
-    fclose(fp);
-    return NULL;
-  }
-
-  bytes_read = fread(buf, 1, len, fp);
-  if (ferror(fp) != 0 || bytes_read != (size_t)len) {
-    fprintf(stderr, "fread failed\n");
-    free(buf);
-    fclose(fp);
-    return NULL;
-  }
-
-  fclose(fp);
-
-  *rlen = len;
-  return buf;
-}
+#include "ffileutils.h"
 
 static void lex(const char *input)
 {
