52cc227064bf54668cfab9e37a2492ba77ca6928
Thiago Farina <tfransosi@gmail.com>
2019-04-13 09:52:40 -0300

    moneybin: implement dba_result_init for mysql backend
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/backend_mysql.c
+++ c/apps/moneybin/backend_mysql.c
@@ -6,6 +6,7 @@
 
 typedef struct mysql_dba_data_s {
   MYSQL *mysql;
+  int first_result; /* whether result_init was already called or not */
 } mysql_dba_data_t;
 
 typedef struct mysql_dba_res_data_s {
@@ -99,7 +100,57 @@ static int mysql_dba_query(dba_t *handle, char const *query, long unsigned lengt
     return -DBA_ERR_BACKEND;
   }
 
+  data->first_result = 1;
+
   return DBA_ERR_SUCCESS;
+}
+
+static int mysql_dba_result_init(dba_t *handle, dba_result_t **result)
+{
+  dba_result_t *res;
+  mysql_dba_data_t *data;
+  mysql_dba_res_data_t *res_data;
+
+  data = handle->data;
+  if (data->first_result == 0) {
+    return DBA_RES_DONE;
+  }
+
+  data->first_result = 0;
+
+  res = malloc(sizeof(dba_result_t));
+  if (res == NULL) {
+    return -DBA_ERR_NOMEM;
+  }
+
+  *result = res;
+
+  res_data = malloc(sizeof(mysql_dba_res_data_t));
+  if (data == NULL) {
+    free(res);
+    res = NULL;
+
+    return -DBA_ERR_NOMEM;
+  }
+
+  res->data = res_data;
+
+  res_data->columns = (long unsigned)mysql_field_count(data->mysql);
+  if (res_data->columns == 0) {
+    return DBA_RES_NOROWS;  /* It means empty or it was not a SELECT statement. */
+  }
+
+  res_data->result = mysql_store_result(data->mysql);
+  if (res_data->result == NULL) {
+    free(res_data);
+    *result = NULL;
+
+    return -DBA_ERR_BACKEND;
+  }
+
+  res_data->fields = mysql_fetch_fields(res_data->result);
+
+  return DBA_RES_ROWS;
 }
 
 dba_ops_t mysql_dba_ops = {
