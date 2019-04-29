a377ff7abf35094dfb2ffd4eca9c343d67a1fe8e
Thiago Farina <tfransosi@gmail.com>
2019-04-29 09:33:40 -0300

    bitops.h: add 'Bob Stout's bit operations url
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/bitops.h
+++ c/apps/bitops.h
@@ -28,5 +28,6 @@
 /* https://github.com/irrdnet/irrd/blob/03de032c2d1769a6ffb72513a03cec77d9759fc2/src/include/defs.h#L27 */
 /* https://github.com/moxuse/ofxSgp/blob/74ccfa7c164226d17e287beb63152a3d84ea4e7d/libs/sgp4sdp4/sgp4sdp4.c */
 /* https://github.com/acassen/keepalived/blob/7630e72ba38be27b060a9db4b01a6045ee794c66/lib/bitops.h#L37 */
+/* https://people.cs.umu.se/isak/snippets/bitops.c */
 
 #endif  /* BITOPS_H_ */
