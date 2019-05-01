2f110a0f4c3a9616efce8ac6d335a99a090cb2c4
Thiago Farina <tfransosi@gmail.com>
2019-05-01 13:18:54 -0300

    moneybin: add a function to convert a date string to time_t
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/lastdate.c
+++ c/apps/moneybin/lastdate.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "config.h"
 #include "dba.h"
@@ -7,6 +9,68 @@
 #include "futils.h"
 
 #define USERCONFFILE ".moneybinrc"
+
+/* https://github.com/BenjiWiebe/lctp/blob/master/datetime.c */
+#define FMT_ERR ((time_t)(-1))
+
+static time_t parse_date(char const *str, char sep)
+{
+  /* format (x is _sep_): yyyyxmmxdd */
+
+  if (strlen(str) != 10)
+    return FMT_ERR;
+
+  char syear[5];
+  char smonth[3];
+  char sday[3];
+  int year = 0, month = 0, day = 0;
+
+  strncpy(syear, str, 4);
+  syear[4] = 0;
+  year = strtol(syear, NULL, 10);
+
+  strncpy(smonth, str+5, 2);
+  smonth[2] = 0;
+  month = strtol(smonth, NULL, 10);
+
+  strncpy(sday, str+8, 2);
+  sday[2] = 0;
+  day = strtol(sday, NULL, 10);
+
+  if ((str[4] != sep) || (str[7] != sep))
+    return FMT_ERR;
+
+  /* format (x is _sep_): mmxddxyyyy */
+  /*
+  strncpy(smonth, str, 2);
+  smonth[2] = 0;
+  month = strtol(smonth, NULL, 10);
+
+  strncpy(sday, str+3, 2);
+  sday[2] = 0;
+  day = strtol(sday, NULL, 10);
+
+  strncpy(syear, str+6, 4);
+  syear[4] = 0;
+  year = strtol(syear, NULL, 10);
+
+  if((str[2] != sep) || (str[5] != sep))
+    return FMT_ERR;
+  */
+
+  struct tm *utm = calloc(1, sizeof(struct tm));
+  utm->tm_mday = day;
+  utm->tm_mon = month - 1;
+  utm->tm_year = year - 1900;
+
+  time_t t = mktime(utm);
+  free(utm);
+
+  if (t == -1)
+    return FMT_ERR;
+
+  return t;
+}
 
 int main(void)
 {
@@ -87,6 +151,15 @@ int main(void)
   }
 
   printf("%s\n", lastdate);
+
+  time_t ld;
+  struct tm *tim;
+  char last_date_str[12];
+
+  ld = parse_date(lastdate, '-');
+  tim = localtime(&ld);
+  strftime(last_date_str, sizeof(last_date_str), "%F", tim);
+  printf("%s\n", last_date_str);
 
   (void)dba_result_deinit(result);
   (void)dba_disconnect(handle);
