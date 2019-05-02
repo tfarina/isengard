e21daebc6bf958440c38feb88bbeca0d8d0621eb
Thiago Farina <tfransosi@gmail.com>
2019-05-02 11:49:58 -0300

    moneybin: add another program to calculate the end date
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/moneybin/enddate.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(void)
+{
+  time_t now;
+  struct tm* now_tm;
+  char end_date_str[11];
+
+  now = time(NULL); /* get time right now */
+  now_tm = localtime(&now);
+  strftime(end_date_str, sizeof(end_date_str), "%F", now_tm);
+
+  printf("%s\n", end_date_str);
+
+  return 0;
+}
