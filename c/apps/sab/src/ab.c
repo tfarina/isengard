5e1ef38eef4ca0ccf7598558eacd1474373c0c7c
Thiago Farina <tfransosi@gmail.com>
2019-05-09 07:11:19 -0300

    sab: use libfort to build a nice text table
    https://github.com/seleznevae/libfort/
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/sab/src/ab.c
+++ c/apps/sab/src/ab.c
@@ -4,6 +4,7 @@
 
 #include "third_party/sqlite/amalgamation/sqlite3.h"
 
+#include "fort.h"
 #include "strutils.h"
 
 static const char dbname[] = "abdb.sqlite3";
@@ -260,12 +261,28 @@ ab_contact_t *ab_get_contact_by_id(int id) {
 }
 
 int ab_print_contact_records(void) {
+  ft_table_t *table;
   alpm_list_t *i;
+
+  ft_set_default_border_style(FT_BASIC2_STYLE);
+  table = ft_create_table();
+  ft_set_cell_prop(table, FT_ANY_ROW, 0, FT_CPROP_TEXT_ALIGN, FT_ALIGNED_CENTER);
+  ft_set_cell_prop(table, FT_ANY_ROW, 1, FT_CPROP_TEXT_ALIGN, FT_ALIGNED_LEFT);
+
+  ft_set_cell_prop(table, 0, FT_ANY_COLUMN, FT_CPROP_ROW_TYPE, FT_ROW_HEADER);
+  ft_write_ln(table, "ID", "First Name", "Last Name", "Email");
 
   for (i = contact_list; i; i = alpm_list_next(i)) {
     ab_contact_t *contact = (ab_contact_t *)i->data;
-    printf("%d|%s|%s|%s\n", contact->id, contact->fname, contact->lname, contact->email);
+    int length = snprintf(NULL, 0, "%d", contact->id);
+    char* idstr = malloc(length + 1);
+    snprintf(idstr, length + 1, "%d", contact->id);
+    ft_write_ln(table, idstr, contact->fname, contact->lname, contact->email);
+    free(idstr);
   }
+
+  printf("%s\n", ft_to_string(table));
+  ft_destroy_table(table);
 
   return 0;
 }
