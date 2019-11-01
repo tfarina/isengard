e4ebe51c443e0d2cf6a23c131ad1d6290bc5ef2f
Thiago Farina <tfransosi@gmail.com>
2019-11-01 14:13:41 -0300

    c: document 'ARRAY_SIZE' macro
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/arraysize.h
+++ c/apps/arraysize.h
@@ -1,6 +1,14 @@
 #ifndef ARRAYSIZE_H_
 #define ARRAYSIZE_H_
 
+/**
+ * Helper macro for counting the number of elements in the array.
+ *
+ * \note Don't use this macro on pointers.
+ *
+ * \param[in] arr A C array.
+ * \return The number of elements in the array.
+ */
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
 #endif /* ARRAYSIZE_H_ */
