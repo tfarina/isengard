67cd9a8831ef89626a128df6d8d93f265af176db
Thiago Farina <tfransosi@gmail.com>
2019-12-12 12:46:08 -0200

    moneybin: check for 'realloc' error and return in case it failed
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/buffer.c
+++ c/apps/moneybin/buffer.c
@@ -12,9 +12,11 @@ void buffer_init(buffer_t *buf)
 void buffer_append(buffer_t *buf, char *data, size_t size)
 {
   buf->data = (char *)realloc(buf->data, buf->length + size + 1);
-  if (buf->data) {
-    memcpy(&(buf->data[buf->length]), data, size);
-    buf->length += size;
-    buf->data[buf->length] = 0;
+  if (buf->data == NULL) {
+    return;
   }
+
+  memcpy(&(buf->data[buf->length]), data, size);
+  buf->length += size;
+  buf->data[buf->length] = 0;
 }
