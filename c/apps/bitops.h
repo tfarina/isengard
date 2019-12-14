29586e6457a70996708d73f3bc40f7f967dafdad
Thiago Farina <tfransosi@gmail.com>
2019-12-14 11:35:05 -0200

    c: add a macro to toggle bits in the mask
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/bitops.h
+++ c/apps/bitops.h
@@ -24,10 +24,14 @@
  */
 #define BIT_ISSET(a, mask) ((a) & (mask))
 
+#define BIT_TOGGLE(a, mask) ((a) ^= (mask))
+
 /* https://www.pixelbeat.org/libs/bitops.h */
 /* https://github.com/irrdnet/irrd/blob/03de032c2d1769a6ffb72513a03cec77d9759fc2/src/include/defs.h#L27 */
 /* https://github.com/moxuse/ofxSgp/blob/74ccfa7c164226d17e287beb63152a3d84ea4e7d/libs/sgp4sdp4/sgp4sdp4.c */
 /* https://github.com/acassen/keepalived/blob/7630e72ba38be27b060a9db4b01a6045ee794c66/lib/bitops.h#L37 */
 /* https://people.cs.umu.se/isak/snippets/bitops.c */
+/* https://github.com/royratcliffe/ta-lib/blob/272b60afa70625b84ab647fd1e0a13909735280b/branches/ta-lib/ta-lib/c/src/ta_common/imatix/sfl/prelude.h#L429 */
+/* https://github.com/signalwire/libks/blob/master/src/include/libks/ks.h#L42 */
 
 #endif  /* BITOPS_H_ */
