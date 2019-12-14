335f7f1aae70147f1f4523e67173b14bd015d2ed
Thiago Farina <tfransosi@gmail.com>
2019-12-14 16:26:33 -0200

    echod: rename 'ED_DEFAULT_PIDFILE' to 'ED_PID_FILE_PATH'
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/ed_config.h
+++ c/apps/network/echod/src/ed_config.h
@@ -8,7 +8,7 @@
 #define ED_DEFAULT_LISTEN_ADDR "0.0.0.0"
 #define ED_DEFAULT_LISTEN_PORT 7
 #define ED_DEFAULT_LISTEN_BACKLOG 1024
-#define ED_DEFAULT_PIDFILE "/var/run/echod.pid"
+#define ED_PID_FILE_PATH "/var/run/echod.pid"
 
 typedef struct ed_config_s {
   pid_t pid;           /* process: server id */
