2bf5cd3b28e1d4aac20775ab1b31fe903ad13098
Thiago Farina <tfransosi@gmail.com>
2019-12-31 20:03:13 -0200

    echod: document ED_CONFIG_FILE_PATH and ED_PID_FILE_PATH macros
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/ed_default_paths.h
+++ c/apps/network/echod/src/ed_default_paths.h
@@ -1,8 +1,15 @@
 #ifndef ED_DEFAULT_PATHS_H_
 #define ED_DEFAULT_PATHS_H_
 
+/**
+ * The default location of the echod configuration file.
+ */
 #define ED_CONFIG_FILE_PATH ED_SYSCONFDIR"/echod.conf"
 
+/**
+ * The file in which to write the process id after the initial fork,
+ * when run in daemon (background) mode.
+ */
 #define ED_PID_FILE_PATH ED_LOCALSTATEDIR"/run/echod.pid"
 
 #endif  /* ED_DEFAULT_PATHS_H_ */
