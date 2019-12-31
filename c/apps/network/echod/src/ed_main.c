96869901539b6f89baa5e16168885a14ac813be5
Thiago Farina <tfransosi@gmail.com>
2019-12-31 13:58:11 -0200

    echod: print a banner before showing the configuration values
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/ed_main.c
+++ c/apps/network/echod/src/ed_main.c
@@ -209,6 +209,7 @@ int main(int argc, char **argv) {
   }
 
   if (show_config) {
+    fprintf(stdout, " *** %s configuration ***\n", config.progname);
     fprintf(stdout, "username  = %s\n", config.username);
     fprintf(stdout, "conffile  = %s\n", config.conffile);
     fprintf(stdout, "pidfile   = %s\n", config.pidfile);
