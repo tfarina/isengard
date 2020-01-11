cbc4d1289d60aedb35e390d61cb85dfbdd6a816d
Thiago Farina <tfransosi@gmail.com>
2020-01-11 18:35:24 -0200

    echod: restore the 'Try help' message
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/ed_cmdline.c
+++ c/apps/network/echod/src/ed_cmdline.c
@@ -126,23 +126,26 @@ int ed_cmdline_parse(int argc, char **argv, ed_config_t *config) {
 }
 
 void ed_cmdline_display_help(int status) {
-  printf("Usage: %s [OPTION]...\n\n", ed_g_progname);
+  if (status) {
+    fprintf(stderr, "Try '%s --help' for more information.\n", ed_g_progname);
+  } else {
+    printf("Usage: %s [OPTION]...\n\n", ed_g_progname);
 
-  fputs("Mandatory arguments to long options are mandatory for short options too.\n\n", stdout);
+    fputs("Mandatory arguments to long options are mandatory for short options too.\n\n", stdout);
 
-  fputs("Options:\n", stdout);
-  fputs("  -d, --daemonize            fork and detach from controlling terminal (background mode)\n", stdout);
-  fputs("  -c, --conffile=FILE        read configuration from the specified file\n", stdout);
-  fputs("  -L, --logfile=FILE         write log messages to the specified file\n", stdout);
-  fputs("  -P, --pidfile=FILE         write process id to the specified file\n", stdout);
-  fputs("  -u, --user=NAME            user identity to run as\n", stdout);
-  fputs("  -a, --address=ADDRESS      bind to the specified address\n", stdout);
-  fputs("  -p, --port=VALUE           set the tcp port to listen on\n", stdout);
-  fputs("  -b, --backlog=VALUE        the backlog argument of listen() applied to the\n", stdout);
-  fputs("                             listening socket\n", stdout);
-  fputs("  -h, --help                 display this help and exit\n", stdout);
-  fputs("  -V, --version              output version information and exit\n", stdout);
+    fputs("Options:\n", stdout);
+    fputs("  -d, --daemonize            fork and detach from controlling terminal (background mode)\n", stdout);
+    fputs("  -c, --conffile=FILE        read configuration from the specified file\n", stdout);
+    fputs("  -L, --logfile=FILE         write log messages to the specified file\n", stdout);
+    fputs("  -P, --pidfile=FILE         write process id to the specified file\n", stdout);
+    fputs("  -u, --user=NAME            user identity to run as\n", stdout);
+    fputs("  -a, --address=ADDRESS      bind to the specified address\n", stdout);
+    fputs("  -p, --port=VALUE           set the tcp port to listen on\n", stdout);
+    fputs("  -b, --backlog=VALUE        the backlog argument of listen() applied to the\n", stdout);
+    fputs("                             listening socket\n", stdout);
+    fputs("  -h, --help                 display this help and exit\n", stdout);
+    fputs("  -V, --version              output version information and exit\n", stdout);
+  }
 
   exit(status);
 }
-
