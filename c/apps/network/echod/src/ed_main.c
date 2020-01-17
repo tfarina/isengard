b6f444a0c29d1e55000a986bf605a324ddde269c
Thiago Farina <tfransosi@gmail.com>
2020-01-17 17:44:05 -0200

    echod: use getuid() to determine the real-user-id of the calling process
    From documentation:
    https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/getuid.2.html
    
    "The real user ID is that of the user who has invoked the program.  As the
    effective user ID gives the process additional permissions during execution
    of ``set-user-ID'' mode processes, getuid() is used to determine the
    real-user-id of the calling process."
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/ed_main.c
+++ c/apps/network/echod/src/ed_main.c
@@ -247,7 +247,7 @@ int main(int argc, char **argv) {
   /*
    * Check if it was run by the superuser.
    */
-  if (geteuid() != ED_ROOT_UID) {
+  if (getuid() != ED_ROOT_UID) {
     ed_log_fatal("You must be root (uid = 0) to run %s", ed_g_progname);
   }
 
