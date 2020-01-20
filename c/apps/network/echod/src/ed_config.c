8552986f2a96a10cc1fbc4fc254490b582b30233
Thiago Farina <tfransosi@gmail.com>
2020-01-20 19:33:53 -0200

    echod: replace 'ED_DEFAULT_' prefix by simpler 'DEF_'
    That way it can be easily reused in another project.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/ed_config.c
+++ c/apps/network/echod/src/ed_config.c
@@ -5,17 +5,17 @@
 /* User for running the Echo daemon. */
 #define DEF_DAEMON_USER "_echod"
 
-#define ED_DEFAULT_BIND_ADDR "0.0.0.0"
-#define ED_DEFAULT_ECHO_PORT 7
-#define ED_DEFAULT_BACKLOG 1024
+#define DEF_BIND_ADDR "0.0.0.0"
+#define DEF_ECHO_PORT 7
+#define DEF_BACKLOG   1024
 
 void ed_config_init(ed_config_t *config) {
   config->detach = 1;
   config->conffile = DEF_PATH_ECHODCONF;
   config->pidfile = DEF_PATH_ECHODPID;
   config->user = DEF_DAEMON_USER;
-  config->address = ED_DEFAULT_BIND_ADDR;
-  config->port = ED_DEFAULT_ECHO_PORT;
-  config->backlog = ED_DEFAULT_BACKLOG;
+  config->address = DEF_BIND_ADDR;
+  config->port = DEF_ECHO_PORT;
+  config->backlog = DEF_BACKLOG;
   config->logfile = (char *) 0;
 }
