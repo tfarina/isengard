948fe210e0f9abc4fa90c5dbca79ee9c1520c47c
Thiago Farina <tfransosi@gmail.com>
2020-01-25 16:45:35 -0200

    msg.h: remove variable names from parameters
    They are not needed, and if we decide to change it in the future,
    it would cause unnecessary compilation for the units that include
    msg.h.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echoc/msg.h
+++ c/apps/network/echoc/msg.h
@@ -3,9 +3,9 @@
 
 #include "hints.h"
 
-void fatal(char const *fmt, ...) ATTR_FORMAT_PRINTF(1, 2) ATTR_NORETURN;
-void error(char const *fmt, ...) ATTR_FORMAT_PRINTF(1, 2);
-void warn(char const *fmt, ...) ATTR_FORMAT_PRINTF(1, 2);
-void info(char const *fmt, ...) ATTR_FORMAT_PRINTF(1, 2);
+void fatal(char const *, ...) ATTR_FORMAT_PRINTF(1, 2) ATTR_NORETURN;
+void error(char const *, ...) ATTR_FORMAT_PRINTF(1, 2);
+void warn(char const *, ...) ATTR_FORMAT_PRINTF(1, 2);
+void info(char const *, ...) ATTR_FORMAT_PRINTF(1, 2);
 
 #endif  /* MSG_H_ */
