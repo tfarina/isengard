91f952d59a13472e1aee36541ae965ab0edf8316
Thiago Farina <tfransosi@gmail.com>
2020-01-27 12:12:49 -0200

    echod: fix documentation comment style
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/daemon.h
+++ c/apps/network/echod/src/daemon.h
@@ -2,11 +2,11 @@
 #define _DAEMON_H_INCLUDED_ 1
 
 /*
-http://man7.org/linux/man-pages/man3/daemon.3.html
-https://github.com/twitter/twemcache/blob/7c846a16e8eb2930b35c7484720b8ef59125c277/src/mc.c
-https://github.com/yandex/odyssey/blob/b8d65d7225774c90f519f7de77911704cedbd7f5/sources/daemon.h
-https://github.com/Netflix/dynomite/blob/032a6f3dc4523e55beebbfff04d993ad60a43dec/src/dynomite.c
-*/
+ * http://man7.org/linux/man-pages/man3/daemon.3.html
+ * https://github.com/twitter/twemcache/blob/7c846a16e8eb2930b35c7484720b8ef59125c277/src/mc.c
+ * https://github.com/yandex/odyssey/blob/b8d65d7225774c90f519f7de77911704cedbd7f5/sources/daemon.h
+ * https://github.com/Netflix/dynomite/blob/032a6f3dc4523e55beebbfff04d993ad60a43dec/src/dynomite.c
+ */
 
 /**
  * Daemonize and redirect stdin, stdout and stderr to /dev/null.
