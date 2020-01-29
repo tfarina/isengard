ad29368e1d03e7a4d2944d3b6108e8f9c593f3a7
Thiago Farina <tfransosi@gmail.com>
2020-01-29 10:06:28 -0200

    sizedt.c: use 'z' length modifier
    man 3 printf:
    
    To specify that the conversion corresponds to a size_t or
    ssize_t argument, or a following n conversion corresponds to a
    pointer to a size_t argument.
    
    http://man7.org/linux/man-pages/man3/printf.3.html
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/sizedt.c
+++ c/apps/sizedt.c
@@ -8,18 +8,18 @@ int main(void) {
            "represent 256 (0 to 255) (2^8) values.\n");
     printf("This is possible because one bit can only be either 0 or 1.\n\n");
 
-    printf("char: %lu byte\n", sizeof(char));
-    printf("short: %lu bytes\n", sizeof(short));
-    printf("int: %lu bytes\n", sizeof(int));
-    printf("long: %lu bytes\n", sizeof(long));
-    printf("float: %lu bytes\n", sizeof(float));
-    printf("double: %lu bytes\n", sizeof(double));
+    printf("char: %zu byte\n", sizeof(char));
+    printf("short: %zu bytes\n", sizeof(short));
+    printf("int: %zu bytes\n", sizeof(int));
+    printf("long: %zu bytes\n", sizeof(long));
+    printf("float: %zu bytes\n", sizeof(float));
+    printf("double: %zu bytes\n", sizeof(double));
 
     printf("\n");
 
-    printf("pointer to char: %lu bytes\n", sizeof(char *));
-    printf("pointer to int: %lu bytes\n", sizeof(int *));
-    printf("pointer to void: %lu bytes\n", sizeof(void *));
+    printf("pointer to char: %zu bytes\n", sizeof(char *));
+    printf("pointer to int: %zu bytes\n", sizeof(int *));
+    printf("pointer to void: %zu bytes\n", sizeof(void *));
 
     return 0;
 }
