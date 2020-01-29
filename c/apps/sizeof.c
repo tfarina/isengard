4ca590d8793a40ed5dc792aad37e0e8036a1b7ab
Thiago Farina <tfransosi@gmail.com>
2020-01-29 15:29:44 -0200

    sizeof.c: change the display to a table format
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/sizeof.c
+++ c/apps/sizeof.c
@@ -8,18 +8,18 @@ int main(void) {
            "represent 256 (0 to 255) (2^8) values.\n");
     printf("This is possible because one bit can only be either 0 or 1.\n\n");
 
-    printf("char: %zu byte\n", sizeof(char));
-    printf("short: %zu bytes\n", sizeof(short));
-    printf("int: %zu bytes\n", sizeof(int));
-    printf("long: %zu bytes\n", sizeof(long));
-    printf("float: %zu bytes\n", sizeof(float));
-    printf("double: %zu bytes\n", sizeof(double));
+    printf("%18s | %4zu byte\n", "char", sizeof(char));
+    printf("%18s | %4zu bytes\n", "short", sizeof(short));
+    printf("%18s | %4zu bytes\n", "int", sizeof(int));
+    printf("%18s | %4zu bytes\n", "long", sizeof(long));
+    printf("%18s | %4zu bytes\n", "float", sizeof(float));
+    printf("%18s | %4zu bytes\n", "double", sizeof(double));
 
     printf("\n");
 
-    printf("pointer to char: %zu bytes\n", sizeof(char *));
-    printf("pointer to int: %zu bytes\n", sizeof(int *));
-    printf("pointer to void: %zu bytes\n", sizeof(void *));
+    printf("%18s | %4zu bytes\n", "pointer to char", sizeof(char *));
+    printf("%18s | %4zu bytes\n", "pointer to int", sizeof(int *));
+    printf("%18s | %4zu bytes\n", "pointer to void", sizeof(void *));
 
     return 0;
 }
