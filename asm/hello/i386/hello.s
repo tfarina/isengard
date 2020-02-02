e45047d71b1c424a03f14e273458cecb92ce1955
Thiago Farina <tfransosi@gmail.com>
2020-02-02 19:32:16 -0200

    asm: port "hello, world" to i386 platform
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ asm/hello/i386/hello.s
@@ -0,0 +1,32 @@
+#
+# https://efxa.org/2011/03/02/assembly-gnulinux/
+#
+# This program is written with AT&T syntax.
+#
+
+.set WRITE_SYSCALL, 4
+.set EXIT_SYSCALL, 1
+.set STDOUT_FD, 1
+
+    .data
+
+hello_str:
+    .asciz "Hello, world!\n" # the string to print
+    len = . - hello_str      # length of the string
+
+    .text
+    .global main
+
+main:
+    # write(1, hello_str, 14)
+    movl $WRITE_SYSCALL,     %eax  # system call number (sys_write).
+    movl $STDOUT_FD,         %ebx  # first argument: file handle (stdout).
+    movl $hello_str,         %ecx  # second argument: pointer to message to write.
+    movl $len,               %edx  # third argument: message length.
+    int $0x80                      # call linux kernel.
+
+    # and exit.
+
+    movl $EXIT_SYSCALL,      %eax  # system call number (sys_exit).
+    movl $0,                 %ebx  # first argument: exit code.
+    int $0x80                      # call linux kernel.
