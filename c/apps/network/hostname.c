d3704b3f6bd739e6918f2870798938d9f2d25247
Thiago Farina <tfransosi@gmail.com>
2020-02-03 19:58:22 -0200

    check if MAXHOSTNAMELEN macro is defined
    Otherwise give it a reasonable value.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/hostname.c
+++ c/apps/network/hostname.c
@@ -5,17 +5,19 @@
 #include <string.h>
 #include <unistd.h>
 
-#define NET_MAXHOSTNAMELEN 256
+#ifndef MAXHOSTNAMELEN
+#define MAXHOSTNAMELEN 256
+#endif
 
 static char *get_hostname(void) {
-  static char hostname[NET_MAXHOSTNAMELEN + 1];
+  static char hostname[MAXHOSTNAMELEN + 1];
 
   if (gethostname(hostname, sizeof(hostname)) < 0) {
     fprintf(stderr, "gethostname() failed: %s\n", strerror(errno));
     return (char *) 0;
   }
 
-  hostname[NET_MAXHOSTNAMELEN] = '\0'; /* Null terminate, just to be safe. */
+  hostname[MAXHOSTNAMELEN] = '\0'; /* Null terminate, just to be safe. */
 
   return hostname;
 }
