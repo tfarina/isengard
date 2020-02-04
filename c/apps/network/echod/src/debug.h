b6a8d3d61557a896dd0231882a50236f7ed6d257
Thiago Farina <tfransosi@gmail.com>
2020-02-04 15:59:36 -0200

    echod: add macro to check if compiler has the given builtin
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/debug.h
+++ c/apps/network/echod/src/debug.h
@@ -1,6 +1,15 @@
 #ifndef _DEBUG_H_INCLUDED_
 #define _DEBUG_H_INCLUDED_ 1
 
+#if defined(M_HAS_BUILTIN)
+#undef M_HAS_BUILTIN
+#endif
+#if defined(__has_builtin)
+#define M_HAS_BUILTIN(builtin) __has_builtin(builtin)
+#else
+#define M_HAS_BUILTIN(builtin) (0)
+#endif
+
 /*
  * Branch prediction hints.
  */
