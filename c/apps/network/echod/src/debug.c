d38ac2d0b14b3eff58a7c8857719ab7ffc0167b0
Thiago Farina <tfransosi@gmail.com>
2020-02-05 21:20:36 -0200

    echod: simplify assertion message
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/debug.c
+++ c/apps/network/echod/src/debug.c
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 
 #ifndef NDEBUG
-void __assertion_failed( char const *assertion, char const *file, int unsigned line) {
-  printf("assertion failed: at \"%s\":%u\n  assertion: \"%s\"\n",
-         file, line, assertion);
+
+void __assertion_failed(char const *assertion, char const *file, int unsigned line) {
+  printf("%s:%u: Assertion failed: %s\n", file, line, assertion);
+
   abort();
 }
+
 #endif
