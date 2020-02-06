0386c31e1191ed3420f511bb5454ec203d0785be
Thiago Farina <tfransosi@gmail.com>
2020-02-06 16:38:26 -0200

    c: add a function to read a PID number from file
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/pidfile.c
@@ -0,0 +1,49 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+/* SEE https://github.com/freebsd/freebsd/blob/master/lib/libutil/pidfile.c#L77 */
+/* SEE http://git.savannah.gnu.org/cgit/tpop3d.git/tree/pidfile.c */
+
+static int pidfile_read(char const *path, pid_t *pid_out) {
+  char buf[16], *endptr;
+  int fd;
+  ssize_t nbytes;
+
+  fd = open(path, O_RDONLY);
+  if (fd < 0) {
+    return errno;
+  }
+
+  nbytes = read(fd, buf, sizeof(buf) - 1);
+
+  close(fd);
+
+  if (nbytes < 0) {
+    return errno;
+  }
+
+  buf[nbytes] = '\0';
+
+  *pid_out = (pid_t) strtol(buf, &endptr, 10);
+  if (*endptr != '\0') {
+    return EINVAL;
+  }
+
+  return 0;
+}
+
+int main(void) {
+  pid_t pid;
+
+  pidfile_read("/var/run/atd.pid", &pid);
+
+  printf("%ld\n", (long) pid);
+
+  return 0;
+}
