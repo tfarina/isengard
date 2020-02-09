a4e2f10ba6e2911e2181d35029d1fbd071d6eeb9
Thiago Farina <tfransosi@gmail.com>
2020-02-09 08:24:18 -0200

    cpuinfo.h: add a macro for x86 architecture
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/cpuinfo.h
+++ c/apps/cpuinfo.h
@@ -1,6 +1,10 @@
 #ifndef CPUINFO_H_
 #define CPUINFO_H_
 
+#if defined(__i686__) || defined(__i586__) || defined(__i486__) || defined(__i386__) || defined(__i386) || defined(_M_IX86) || defined(_X86_) || defined(__THW_INTEL__)
+#define CPU_ARCH_X86
+#endif
+
 /**
  * Returns the number of CPU cores available.
  */
