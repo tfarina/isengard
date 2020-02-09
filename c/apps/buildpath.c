d452b0553f030aed0515676f10f010c7b4485750
Thiago Farina <tfransosi@gmail.com>
2020-02-09 08:08:05 -0200

    c: add 'build_filename' function and a simple test case for it
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/buildpath.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static char *build_filename(char *dirname, char *filename) {
+  char *path;
+  size_t len;
+
+  len = strlen(dirname) + /* '/' */ 1 + strlen(filename) + /* '\0' */ 1;
+
+  path = (char *) malloc(len);
+
+  sprintf(path, "%s/%s", dirname, filename);
+
+  return path;
+}
+
+int main(void) {
+  char *path;
+
+  path = build_filename("/home/tfarina", ".config");
+
+  printf("%s\n", path);
+
+  return 0;
+}
