6ce1214177957b693411b0efc7f766ca0e230c65
Thiago Farina <tfransosi@gmail.com>
2020-02-18 00:53:18 -0300

    echod: remove 'NDEBUG' protection from debug.c
    The function is only/ever declared when NDEBUG is not defined, i.e.,
    in DEBUG build.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/debug.c
+++ c/apps/network/echod/src/debug.c
@@ -3,11 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifndef NDEBUG
-
 void __assertion_failed(char const *assertion, char const *file, int line) {
   fprintf(stderr, "%s:%d: Assertion failed: %s\n", file, line, assertion);
   abort();
 }
-
-#endif
