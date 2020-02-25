f72144590e885759037fd42a415992d6278efcd0
Thiago Farina <tfransosi@gmail.com>
2020-02-25 17:30:43 -0300

    echoc: import __GNUC_PREREQ macro from glib
    Copied from:
    [1] https://sourceware.org/git/?p=glibc.git;a=blob;f=include/features.h;hb=d423e17031f379d51c5ad6c33e8b57380c8eea35#l161
    [2] https://github.com/ghostbsd/ghostbsd/blob/stable/12/sys/sys/cdefs.h#L137
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echoc/hints.h
+++ c/apps/network/echoc/hints.h
@@ -1,6 +1,25 @@
 #ifndef HINTS_H_
 #define HINTS_H_
 
+/*
+ * Macro to test if we're using a specific version of gcc or later.
+ *
+ * Use like this:
+ *   #if __GNUC_PREREQ__(2,8)
+ *   ... code requiring gcc 2.8 or later ...
+ *   #endif
+ *
+ * Note: only works for GCC 2.0 and later, because __GNUC_MINOR__ was
+ * added in 2.0.
+ */
+#if defined __GNUC__ && defined __GNUC_MINOR__
+#define __GNUC_PREREQ__(major, minor) \
+  ((__GNUC__ << 16) + __GNUC_MINOR__ >= ((major) << 16) + (min))
+#else
+#define __GNUC_PREREQ__(major, minor) 0
+#endif
+
+
 #if defined(MK_NORETURN)
 #undef MK_NORETURN
 #endif
