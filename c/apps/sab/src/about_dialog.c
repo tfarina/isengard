92f4d5eb6a0f34e881ee4e563d4aa44e2401fbf3
Thiago Farina <tfransosi@gmail.com>
2020-03-09 20:17:17 +0000

    sab: move code for 'about' dialog into its own module
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/sab/src/about_dialog.c
@@ -0,0 +1,43 @@
+#include "about_dialog.h"
+
+#include <gtk/gtk.h>
+
+#define SAB_ABBR "SAB"
+#define SAB_NAME "Simple Address Book"
+#define SAB_COPYRIGHT "Copyright © 2019"
+#define SAB_LICENSE \
+    "SAB is free software: you can redistribute it and/or modify it "   \
+        "under the terms of the GNU General Public License as published by " \
+        "the Free Software Foundation; either version 3 of the License, or " \
+        "(at your option) any later version."                                \
+        "\n\n"                                                               \
+        "SAB is distributed in the hope that it will be useful, "           \
+        "but WITHOUT ANY WARRANTY; without even the implied warranty of "    \
+        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "     \
+        "GNU General Public License for more details."                       \
+        "\n\n"                                                               \
+        "You should have received a copy of the GNU General Public License " \
+    "along with SAB. If not, see: https://www.gnu.org/licenses/"
+
+void sab_about_dialog_show(void) {
+  GtkWindow *dialog;
+
+  dialog = g_object_new(GTK_TYPE_ABOUT_DIALOG,
+                        /* Hold the application while the window is shown */
+                        "role", "sab-about",
+			"window-position", GTK_WIN_POS_CENTER,
+			"title", "About SAB",
+			"program-name", SAB_ABBR,
+			"copyright", SAB_COPYRIGHT,
+			"comments", SAB_NAME,
+			"version", "0.0.1",
+			"license", SAB_LICENSE,
+			"wrap-license", TRUE,
+			"website", "https://sab.com",
+			"translator-credits", "CREDITS",
+			NULL);
+
+  g_signal_connect(dialog, "response",
+                   G_CALLBACK(gtk_widget_destroy), NULL);
+  gtk_window_present(dialog);
+}
