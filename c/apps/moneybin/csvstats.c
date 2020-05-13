f8eaf129a54ce5373808208a42024a5dccf1e0a5
Thiago Farina <tfarina@chromium.org>
2020-05-13 18:20:49 +0100

    csvstats.c: short 'min', 'max' variable names
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/moneybin/csvstats.c
+++ c/apps/moneybin/csvstats.c
@@ -6,8 +6,8 @@
 int main(int argc, char **argv) {
   double *close;
   int num_rows;
-  double min_close;
-  double max_close;
+  double min;
+  double max;
   double avg;
 
   if (argc != 2) {
@@ -20,12 +20,12 @@ int main(int argc, char **argv) {
   close = csv_close_prices();
   num_rows = csv_num_rows();
 
-  stats_min(close, num_rows, &min_close);
-  stats_max(close, num_rows, &max_close);
+  stats_min(close, num_rows, &min);
+  stats_max(close, num_rows, &max);
   stats_average(close, num_rows, &avg);
 
-  printf("Min: %f\n", min_close);
-  printf("Max: %f\n", max_close);
+  printf("Min: %f\n", min);
+  printf("Max: %f\n", max);
   printf("Avg: %f\n", avg);
 
   return 0;
