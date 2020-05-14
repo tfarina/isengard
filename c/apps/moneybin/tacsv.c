780a27c1ebbb03d519532cb18f2f89de7e084080
Thiago Farina <tfarina@chromium.org>
2020-05-14 17:41:13 +0100

    csvcat.c: move all csv parsing code into tacsv.c
    To be used by csvstats.c.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

---
+++ c/apps/moneybin/tacsv.c
@@ -0,0 +1,229 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ta.h"
+#include "third_party/libcsv/csv.h"
+
+typedef enum return_code_e {
+  RC_SUCCESS,    /* No error */
+  RC_ERROR,
+} return_code_t;
+
+typedef struct csv_counts_s {
+  long unsigned fields;
+  long unsigned rows;
+} csv_counts_t;
+
+typedef struct csv_state_s {
+  int ignore_first_line;
+
+  /**
+   * Which line (row) we are in the parsing process.
+   */
+  int row;
+
+  /**
+   * The next field (column number) to parse.
+   */
+  int column;
+
+  ta_bars_t *bars;
+} csv_state_t;
+
+typedef enum csv_column_e {
+  CSV_COLUMN_DATE,
+  CSV_COLUMN_OPEN,
+  CSV_COLUMN_HIGH,
+  CSV_COLUMN_LOW,
+  CSV_COLUMN_CLOSE,
+  CSV_COLUMN_ADJ_CLOSE,
+  CSV_COLUMN_VOLUME
+} csv_column_t;
+
+static char *parse_str(char const *field, size_t length, return_code_t *rc) {
+  if (length > 0) {
+    char *str = (char *)malloc((length + 1) * sizeof(char));
+    strncpy(str, field, length + 1);
+
+    *rc = RC_SUCCESS;
+    return str;
+  }
+
+  *rc = RC_ERROR;
+  return NULL;
+}
+
+static double parse_price(char const *field, size_t length, return_code_t *rc) {
+  char *endptr;
+  double price;
+
+  price = (double)strtod(field, &endptr);
+  if (length > 0 && (endptr == NULL || *endptr == '\0')) {
+    *rc = RC_SUCCESS;
+    return price;
+  }
+
+  *rc = RC_ERROR;
+  return -1.0f;
+}
+
+static void csv_field_count_cb(void *field, size_t field_length, void *data) {
+  csv_counts_t *cnt = (csv_counts_t *)data;
+
+  cnt->fields++;
+}
+
+static void csv_row_count_cb(int c, void *data) {
+  csv_counts_t *cnt = (csv_counts_t *)data;
+
+  cnt->rows++;
+}
+
+/**
+ * This functions is called each time a new field has been found.
+ */
+static void csv_read_field_cb(void *field, size_t field_length, void *data) {
+  csv_state_t *state = (csv_state_t *)data;
+  char *buffer;
+  return_code_t rc;
+
+  if (state->ignore_first_line) {
+    return;
+  }
+
+  buffer = (char *)malloc((field_length + 1) * sizeof(char));
+  strncpy(buffer, field, field_length);
+  buffer[field_length] = '\0';
+
+  switch (state->column) {
+  case CSV_COLUMN_DATE:
+    break;
+  case CSV_COLUMN_OPEN:
+    state->bars->open[state->row] = parse_price(buffer, field_length, &rc);
+    break;
+  case CSV_COLUMN_HIGH:
+    state->bars->high[state->row] = parse_price(buffer, field_length, &rc);
+    break;
+  case CSV_COLUMN_LOW:
+    state->bars->low[state->row] = parse_price(buffer, field_length, &rc);
+    break;
+  case CSV_COLUMN_CLOSE:
+    state->bars->close[state->row] = parse_price(buffer, field_length, &rc);
+    break;
+  case CSV_COLUMN_ADJ_CLOSE:
+    state->bars->adjclose[state->row] = parse_price(buffer, field_length, &rc);
+    break;
+  case CSV_COLUMN_VOLUME:
+    state->bars->volume[state->row] = atoi(buffer);
+    break;
+  default:
+    rc = RC_ERROR;
+    break;
+  }
+
+  free(buffer);
+
+  state->column++;
+}
+
+/**
+ * This functions is called each time a new row has been found.
+ */
+static void csv_read_row_cb(int c, void *data) {
+  csv_state_t *state = (csv_state_t *)data;
+
+  if (state->ignore_first_line) {
+    state->ignore_first_line = 0;
+    return;
+  }
+
+  state->row++;
+  state->column = 0;
+}
+
+int read_csv(char const *filename, ta_bars_t **outbars) {
+  FILE* fp;
+  struct csv_parser parser;
+  char buf[1024];
+  size_t bytes_read;
+  csv_counts_t c;
+  csv_state_t *state;
+  ta_bars_t *bars;
+  int i;
+
+  c.fields = 0;
+  c.rows = 0;
+
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Failed to open file %s: %s\n", filename, strerror(errno));
+    return -1;
+  }
+
+ if (csv_init(&parser, CSV_STRICT | CSV_APPEND_NULL | CSV_STRICT_FINI) != 0) {
+    fprintf(stderr, "Failed to initialize csv parser\n");
+    return -1;
+  }
+
+  /* First pass to count the total number of rows and fields. */
+  while ((bytes_read = fread(buf, sizeof(char), sizeof(buf), fp)) > 0) {
+    if (csv_parse(&parser, buf, bytes_read, csv_field_count_cb, csv_row_count_cb, &c) != bytes_read) {
+      fprintf(stderr, "Error while parsing %s: %s\n", filename, csv_strerror(csv_error(&parser)));
+      csv_free(&parser);
+      fclose(fp);
+      return -1;
+    }
+  }
+
+  csv_fini(&parser, csv_field_count_cb, csv_row_count_cb, &c);
+
+  printf("%s: %lu fields, %lu rows\n", filename, c.fields, c.rows);
+
+  state = malloc(sizeof(*state));
+  state->ignore_first_line = 1;
+  state->row = 0;
+  state->column = 0;
+
+  bars = malloc(sizeof(*bars));
+
+  bars->numrows = c.rows - (state->ignore_first_line ? 1 : 0);
+
+  bars->open = malloc(sizeof(double) * bars->numrows);
+  bars->high = malloc(sizeof(double) * bars->numrows);
+  bars->low = malloc(sizeof(double) * bars->numrows);
+  bars->close = malloc(sizeof(double) * bars->numrows);
+  bars->adjclose = malloc(sizeof(double) * bars->numrows);
+  bars->volume = malloc(sizeof(int) * bars->numrows);
+
+  state->bars = bars;
+
+  if (fp) {
+    fseek(fp, 0, SEEK_SET);
+  }
+
+  while ((bytes_read = fread(buf, sizeof(char), sizeof(buf), fp)) > 0) {
+    if (csv_parse(&parser, buf, bytes_read, csv_read_field_cb, csv_read_row_cb, state) != bytes_read) {
+      fprintf(stderr, "Error while parsing %s: %s\n", filename, csv_strerror(csv_error(&parser)));
+      csv_free(&parser);
+      fclose(fp);
+      return -1;
+    }
+  }
+
+  csv_fini(&parser, csv_read_field_cb, csv_read_row_cb, state);
+  csv_free(&parser);
+
+  if (ferror(fp)) {
+    fprintf(stderr, "error reading file %s\n", filename);
+    fclose(fp);
+    return -1;
+  }
+
+  fclose(fp);
+
+  *outbars = state->bars;
+
+  return 0;
+}
