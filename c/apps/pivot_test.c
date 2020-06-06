19f79aca3d60c8e6ff6127c0b7a749f5a496eddd
Thiago Farina <tfransosi@gmail.com>
2020-06-06 10:39:30 -0400

    report pivot points in table style
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/pivot_test.c
+++ c/apps/pivot_test.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
   s2 = support2(pp, r1, s1);
   s3 = support3(pp, high, low);
 
-  printf("%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\n", s3, s2, s1, pp, r1, r2, r3);
+  printf("S3: %.4f\nS2: %.4f\nS1: %.4f\nP:  %.4f\nR1: %.4f\nR2: %.4f\nR3: %.4f\n", s3, s2, s1, pp, r1, r2, r3);
 
   return 0;
 }
