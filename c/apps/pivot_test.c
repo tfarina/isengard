e6cfdb2e3fa814a462041f3bfd7e666b349d99df
Thiago Farina <tfransosi@gmail.com>
2020-06-06 16:18:52 -0400

    show the values of 'close', 'high' and 'low' in the pivot_test output
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/pivot_test.c
+++ c/apps/pivot_test.c
@@ -25,6 +25,9 @@ int main(int argc, char **argv)
   s2 = support2(pp, r1, s1);
   s3 = support3(pp, high, low);
 
+  printf("Close\tHigh\tLow\n");
+  printf("%.2f\t%.2f\t%.2f\n\n", close, high, low);
+
   printf("S3: %.4f\nS2: %.4f\nS1: %.4f\nP:  %.4f\nR1: %.4f\nR2: %.4f\nR3: %.4f\n", s3, s2, s1, pp, r1, r2, r3);
 
   return 0;
