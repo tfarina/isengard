603fce4f3f425c8ebf88af7ef63b0ca7aeb8ee4c
Thiago Farina <tfransosi@gmail.com>
2020-06-17 16:27:30 -0400

    moneybin: start doing something interesting in trade.c
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/trade.c
+++ c/apps/moneybin/trade.c
@@ -1,6 +1,17 @@
 #include "crossover.h"
+#include "ta.h"
 
 int main(void)
 {
+  int err;
+  char filename[13] = "PETR4.SA.csv";
+  ta_bars_t *bars;
+
+  /* Read bar data from CSV file. */
+  err = read_csv(filename, &bars);
+  if (err < 0) {
+    return err;
+  }
+
   return 0;
 }
