266ba46fc0edc4607e8c0023b31ccf9d3a2875f0
Thiago Farina <tfransosi@gmail.com>
2020-06-20 14:42:47 -0400

    homedir.c: test if a path exists and is a directory
    https://github.com/coreutils/coreutils/blob/aaba82431ceca97441d56152d8c7ec2d84fa1d12/src/test.c
    https://github.com/harai/gnu-global/blob/f86ba74d867385353815f8656c4a6cf4029c1f0b/libutil/test.c
    https://linux.die.net/man/2/stat
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/homedir.c
+++ c/apps/homedir.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 #include "futils.h"
+
+static int test_isdir(char const *path)
+{
+  struct stat sb;
+  int rc;
+
+  rc = stat(path, &sb);
+
+  return rc == 0 && S_ISDIR(sb.st_mode);
+}
 
 int main(int argc, char **argv) {
   const char *homedir;
@@ -9,6 +23,10 @@ int main(int argc, char **argv) {
   homedir = f_get_home_dir();
 
   printf("%s\n", homedir);
+
+  if (test_isdir(homedir)) {
+    printf("Looks look %s is a directory\n", homedir);
+  }
 
   return 0;
 }
