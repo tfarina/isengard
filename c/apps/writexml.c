3597e59546efd480a0bae3bceb579dbffdd7e082
Thiago Farina <tfransosi@gmail.com>
2020-06-25 11:51:25 -0400

    c: start trying to use mxml library
    The idea for the books catalog came from:
    https://docs.microsoft.com/en-us/previous-versions/windows/desktop/ms762271(v=vs.85)
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/writexml.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include "third_party/mxml/mxml.h"
+
+int main(int argc, char **argv)
+{
+  mxml_node_t *xml = NULL;
+  mxml_node_t *book = NULL;
+  mxml_node_t *node = NULL;
+  FILE *fp = NULL;
+
+  xml = mxmlNewXML("1.0");
+
+  book = mxmlNewElement(xml, "book");
+  node = mxmlNewElement(book, "author");
+  mxmlNewText(node, 0, "Gambardella, Matthew");
+
+  node = mxmlNewElement(book, "title");
+  mxmlNewText(node, 0, "XML Developer's Guide");
+
+  fp = fopen("books.xml", "w");
+  mxmlSaveFile(xml, fp, MXML_NO_CALLBACK);
+  fclose(fp);
+
+  return 0;
+}
