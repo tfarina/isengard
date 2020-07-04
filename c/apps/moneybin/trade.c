273702f3fa4fa2d90d65b4bf0a0c9f2751be305d
Thiago Farina <tfransosi@gmail.com>
2020-07-04 13:00:49 -0400

    moneybin: allocate memory for the moving average arrays
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/trade.c
+++ c/apps/moneybin/trade.c
@@ -1,16 +1,32 @@
+#include <stdlib.h>
+
 #include "crossover.h"
 #include "ta.h"
+#include "vector.h"
 
 int main(void)
 {
   int err;
   char filename[13] = "PETR4.SA.csv";
   ta_bars_t *bars;
+  vector_t /* of double */ *ma1, *ma2;
 
   /* Read bar data from CSV file. */
   err = read_csv(filename, &bars);
   if (err < 0) {
     return err;
+  }
+
+  /* Allocate memory for the moving average array. */
+  ma1 = vector_alloc(sizeof(double), bars->numrows);
+  if (ma1 == NULL) {
+    return -1;
+  }
+
+  /* Allocate memory for the moving average array. */
+  ma2 = vector_alloc(sizeof(double), bars->numrows);
+  if (ma2 == NULL) {
+    return -1;
   }
 
   return 0;
