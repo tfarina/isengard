efcc6673cedec32e087b172ba2f2a0e96f418095
Thiago Farina <tfransosi@gmail.com>
2020-07-05 15:09:00 -0400

    sma_test.c: rename some variables to make it easier to read/understand
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/sma_test.c
+++ c/apps/moneybin/sma_test.c
@@ -19,29 +19,28 @@ static void print_array(double const *arr, size_t const size)
 }
 
 int main(void) {
-  double data_in[] = { 0.61, 0.60, 0.63, 0.65, 0.69, 0.67, 0.70, 0.68, 0.62, 0.63, 0.61 };
-  int data_size = ARRAY_SIZE(data_in);
+  double indicator_data[] = { 0.61, 0.60, 0.63, 0.65, 0.69, 0.67, 0.70, 0.68, 0.62, 0.63, 0.61 };
   int period = 3;
   int start = 2;
-  const int output_length = data_size - start;
-  double *data_out;
+  const int output_length = ARRAY_SIZE(indicator_data) - start;
+  double *indicator_result;
   int err;
 
-  print_array(data_in, data_size);
+  print_array(indicator_data, ARRAY_SIZE(indicator_data));
 
   printf("The start amount is: %d\n", start);
 
-  data_out = malloc((unsigned int)output_length * sizeof(double));
+  indicator_result = malloc((unsigned int)output_length * sizeof(double));
 
   printf("The output length is: %d\n", output_length);
 
-  const double *all_inputs[] = {data_in};
-  double *all_outputs[] = {data_out};
+  const double *all_inputs[] = {indicator_data};
+  double *all_outputs[] = {indicator_result};
 
-  err = ta_sma(data_size, all_inputs, period, all_outputs);
+  err = ta_sma(ARRAY_SIZE(indicator_data), all_inputs, period, all_outputs);
 
   printf("The output data is: ");
-  print_array(data_out, output_length);
+  print_array(indicator_result, output_length);
 
   return 0;
 }
