77116e577353a86c562b668eed2f1c0a4f7737c2
Thiago Farina <tfransosi@gmail.com>
2020-07-07 09:28:16 -0400

    echod: move all macros out of debug.h
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/core.h
+++ c/apps/network/echod/src/core.h
@@ -10,4 +10,24 @@
 #define M_STRINGIFY_(x) #x
 #define M_STRINGIFY(x)  M_STRINGIFY_(x)
 
+#if defined(M_HAS_BUILTIN)
+#undef M_HAS_BUILTIN
+#endif
+#if defined(__has_builtin)
+#define M_HAS_BUILTIN(builtin) __has_builtin(builtin)
+#else
+#define M_HAS_BUILTIN(builtin) (0)
+#endif
+
+/*
+ * Branch prediction hints.
+ */
+#if __GNUC__ >= 3
+# define M_UNLIKELY(exp) __builtin_expect ((exp), 0)
+# define M_LIKELY(exp)   __builtin_expect ((exp), 1)
+#else
+# define M_UNLIKELY(exp) (exp)
+# define M_LIKELY(exp)   (exp)
+#endif
+
 #endif  /* !defined(_CORE_H_INCLUDED_) */
--- c/apps/network/echod/src/debug.h
+++ c/apps/network/echod/src/debug.h
@@ -1,24 +1,4 @@
 #ifndef _DEBUG_H_INCLUDED_
 #define _DEBUG_H_INCLUDED_ 1
 
-#if defined(M_HAS_BUILTIN)
-#undef M_HAS_BUILTIN
-#endif
-#if defined(__has_builtin)
-#define M_HAS_BUILTIN(builtin) __has_builtin(builtin)
-#else
-#define M_HAS_BUILTIN(builtin) (0)
-#endif
-
-/*
- * Branch prediction hints.
- */
-#if __GNUC__ >= 3
-# define M_UNLIKELY(exp) __builtin_expect ((exp), 0)
-# define M_LIKELY(exp)   __builtin_expect ((exp), 1)
-#else
-# define M_UNLIKELY(exp) (exp)
-# define M_LIKELY(exp)   (exp)
-#endif
-
 #endif  /* !defined(_DEBUG_H_INCLUDED_) */
