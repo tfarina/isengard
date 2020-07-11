8cf21f93fdfff2c18911b877a84b47e79896efbc
Thiago Farina <tfransosi@gmail.com>
2020-07-11 13:12:52 -0400

    stats_test.c: present 'min' and 'max' calculations first
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/stats_test.c
+++ c/apps/stats_test.c
@@ -22,31 +22,31 @@ static void print_array(double const *arr, size_t const size)
 int main(void)
 {
   double a[] = {1.6, 2.7, 3.8, 4.9};
+  double min = 0.0;
+  double max = 0.0;
   double sum = 0.0;
   double avg = 0.0;
   double variance = 0.0;
   double stdev = 0.0;
-  double min = 0.0;
-  double max = 0.0;
 
   printf("Array: ");
   print_array(a, ARRAY_SIZE(a));
 
+  stats_min(a, ARRAY_SIZE(a), &min);
+  stats_max(a, ARRAY_SIZE(a), &max);
   stats_sum(a, ARRAY_SIZE(a), &sum);
   stats_average(a, ARRAY_SIZE(a), &avg);
   stats_var_sam(a, ARRAY_SIZE(a), &variance);
   stats_stdev_sam(a, ARRAY_SIZE(a), &stdev);
-  stats_min(a, ARRAY_SIZE(a), &min);
-  stats_max(a, ARRAY_SIZE(a), &max);
 
   printf("\n\n");
   printf("Statistics summary:\n");
-  printf("Sum                 %f\n", sum);
-  printf("Average             %f\n", avg);
-  printf("Variance            %f\n", variance);
-  printf("Standard deviation  %f\n", stdev);
   printf("Min                 %f\n", min);
   printf("Max                 %f\n", max);
+  printf("Sum                 %f\n", sum);
+  printf("Mean                %f\n", avg);
+  printf("Variance            %f\n", variance);
+  printf("Standard deviation  %f\n", stdev);
 
   return 0;
 }
