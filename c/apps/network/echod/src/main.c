8f236e43d01327ee3fa8ddab1c6563c30809125d
Thiago Farina <tfransosi@gmail.com>
2020-07-26 12:46:09 -0400

    echod: tweak signal error messages
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/main.c
+++ c/apps/network/echod/src/main.c
@@ -114,16 +114,16 @@ static void init_signals(void) {
   action.sa_handler = sigterm;
 
   if (sigaction(SIGINT, &action, (struct sigaction *) 0) < 0) {
-    ulog_error("unable to set SIGINT: %s", strerror(errno));
+    ulog_error("could not set up signal handler for SIGINT: %s", strerror(errno));
   }
   if (sigaction(SIGTERM, &action, (struct sigaction *) 0) < 0) {
-    ulog_error("unable to set SIGTERM: %s", strerror(errno));
+    ulog_error("could not set up signal handler for SIGTERM: %s", strerror(errno));
   }
 
   action.sa_flags |= SA_NOCLDSTOP;
   action.sa_handler = sigchld;
   if (sigaction(SIGCHLD, &action, (struct sigaction *) 0) < 0) {
-    ulog_error("unable to set SIGCHLD: %s", strerror(errno));
+    ulog_error("could not set up signal handler for SIGCHLD: %s", strerror(errno));
   }
 }
 
