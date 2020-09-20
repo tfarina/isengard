ae4dffc0c8d2449ff50cc7efad6a3581a66fa25f
Thiago Farina <tfransosi@gmail.com>
2020-09-20 13:39:33 -0400

    sab: remove 'SAB' from header guards in about_dialog.h
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/sab/src/gui/about_dialog.h
+++ c/apps/sab/src/gui/about_dialog.h
@@ -1,6 +1,6 @@
-#ifndef _SAB_ABOUT_DIALOG_H_INCLUDED_
-#define _SAB_ABOUT_DIALOG_H_INCLUDED_ 1
+#ifndef _ABOUT_DIALOG_H_INCLUDED_
+#define _ABOUT_DIALOG_H_INCLUDED_ 1
 
 void sab_show_about_dialog(void);
 
-#endif  /* !defined(_SAB_ABOUT_DIALOG_H_INCLUDED_) */
+#endif  /* !defined(_ABOUT_DIALOG_H_INCLUDED_) */
