95383de60d75e79765518f1c9d05545f5b7b88b8
Thiago Farina <tfransosi@gmail.com>
2020-10-04 10:41:31 -0400

    echod: DEBUG_PRINT should do nothing in release build
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/debug.h
+++ c/apps/network/echod/src/debug.h
@@ -4,8 +4,16 @@
 #ifndef _DEBUG_H_INCLUDED_
 #define _DEBUG_H_INCLUDED_ 1
 
+#ifdef NDEBUG
+
+#define DEBUG_PRINT(msg) ((void) 0)
+
+#else
+
 #define DEBUG_PRINT(msg)  \
   do {                    \
   } while (0)
+
+#endif  /* NDEBUG */
 
 #endif  /* !defined(_DEBUG_H_INCLUDED_) */
