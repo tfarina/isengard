7850b9884936e77836876173b85fa7410f493f76
Thiago Farina <tfransosi@gmail.com>
2020-10-04 06:25:35 -0400

    vector: merge '_vector_insert' code into with 'vector_push_back' function
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/libs/vector/vector.c
+++ c/libs/vector/vector.c
@@ -3,42 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * Inserts the specified element at the given position at #self.
- *
- * @param self The container instance.
- * @param index The position where the element has to be inserted.
- * @param element The element to be inserted.
- *
- * @return On success, zero is returned. On error, -1 is returned.
- */
-static int _vector_insert(vector_t *self, int index, void *element)
-{
-        if (self == NULL) {
-	        return -1;
-	}
-
-        if (index < 0 || index > self->size) {
-	        return -1;
-	}
-
-        if (self->size + 1 > self->capacity) {
-                /* Increase the capacity by one to make room for the new element, then double it. */
-	        size_t const new_capacity = (self->capacity + 1) * 2;
-                self->data = realloc(self->data, self->datasize * new_capacity);
-                if (self->data == NULL) {
-                        return -1;
-                }
-
-                self->capacity = new_capacity;
-	}
-
-        memcpy((char *)self->data + (index * self->datasize), element, self->datasize);
-        self->size++;
-
-        return 0;
-}
-
 vector_t *vector_alloc(size_t datasize, size_t capacity)
 {
         vector_t *self;
@@ -64,10 +28,22 @@ vector_t *vector_alloc(size_t datasize, size_t capacity)
 void vector_push_back(vector_t *self, void *element)
 {
         if (self == NULL) {
-                return;
-        }
+	        return;
+	}
 
-        _vector_insert(self, self->size, element);
+        if (self->size + 1 > self->capacity) {
+                /* Increase the capacity by one to make room for the new element, then double it. */
+	        size_t const new_capacity = (self->capacity + 1) * 2;
+                self->data = realloc(self->data, self->datasize * new_capacity);
+                if (self->data == NULL) {
+                        return;
+                }
+
+                self->capacity = new_capacity;
+	}
+
+        memcpy((char *)self->data + (self->size * self->datasize), element, self->datasize);
+        self->size++;
 }
 
 void *vector_at(vector_t const * const self, int unsigned const index)
