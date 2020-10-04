80fe3f5fd74eeef9a92d1b71e3bd68d62409ccfc
Thiago Farina <tfransosi@gmail.com>
2020-10-04 10:53:28 -0400

    sab: initialize/deinitialize the address book library
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/sab/src/cli/cmd_add.c
+++ c/apps/sab/src/cli/cmd_add.c
@@ -2,11 +2,18 @@
 
 #include <stdio.h>
 
+#include "ab.h"
+
 int cmd_add(int argc, char **argv) {
   if (argc != 4) {
     printf("usage: add 'FIRST NAME' 'LAST NAME' 'E-MAIL'\n");
     return -1;
   }
+
+  ab_init();
+
+
+  ab_fini();
 
   return 0;
 }
