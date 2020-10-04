7301ba29dfaeff1f3b0417f1202ff5eb2c9e65b2
Thiago Farina <tfransosi@gmail.com>
2020-10-04 10:39:51 -0400

    Revert "echod: rename 'DEBUG_PRINT' macro to 'dprintf' which is shorter and function-like"
    This reverts commit c2127f6f84f01255590cc3110de896838b4fab1e.
    
    Looks like there is already a function called 'dprintf' in stdio.h, which can cause an error like this:
    
    /usr/include/stdio.h:415:64: error: macro "dprintf" passed 3 arguments, but takes just 1
     extern int dprintf (int __fd, const char *__restrict __fmt, ...)
                                                                     ^

--- c/apps/network/echod/src/debug.h
+++ c/apps/network/echod/src/debug.h
@@ -4,8 +4,8 @@
 #ifndef _DEBUG_H_INCLUDED_
 #define _DEBUG_H_INCLUDED_ 1
 
-#define dprintf(msg)  \
-  do {                \
+#define DEBUG_PRINT(msg)  \
+  do {                    \
   } while (0)
 
 #endif  /* !defined(_DEBUG_H_INCLUDED_) */
