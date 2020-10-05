7e325b7bf088d74ed0b12726d3dda5fc4273682e
Thiago Farina <tfransosi@gmail.com>
2020-10-05 09:08:54 -0400

    use echoc's attributes.h in network
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/network/attributes.h
@@ -0,0 +1,39 @@
+#ifndef _ATTRIBUTES_H_INCLUDED_
+#define _ATTRIBUTES_H_INCLUDED_ 1
+
+#include "compiler_tests.h"
+
+/**
+ * MK_NORETURN:
+ *
+ * Indicates to the compiler that a given function never returns.
+ */
+
+#if defined(MK_NORETURN)
+#undef MK_NORETURN
+#endif
+#if __GNUC_PREREQ__(2,8)
+#define MK_NORETURN __attribute__((__noreturn__))
+#elif defined(_MSC_VER)
+#define MK_NORETURN __declspec(noreturn)
+#else
+#define MK_NORETURN
+#endif
+
+/**
+ * MK_PRINTF:
+ *
+ * Tells the compiler to perform `printf` format string checking if the
+ * compiler supports it.
+ */
+
+#if defined(MK_PRINTF)
+#undef MK_PRINTF
+#endif
+#if defined(__GNUC__)
+#define MK_PRINTF(string_index, first_to_check) __attribute__ ((__format__ (__printf__, string_index, first_to_check)))
+#else
+#define MK_PRINTF(string_index, first_to_check)
+#endif
+
+#endif  /* !defined(_ATTRIBUTES_H_INCLUDED_) */
