c146c78a7c41654327d83c22cccc094d119fc098
Thiago Farina <tfransosi@gmail.com>
2020-10-06 12:34:46 -0400

    sab: implement 'add' command
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/sab/src/cli/cmd_add.c
+++ c/apps/sab/src/cli/cmd_add.c
@@ -5,6 +5,8 @@
 #include "ab.h"
 
 int cmd_add(int argc, char **argv) {
+  ab_contact_t *contact;
+
   if (argc != 4) {
     printf("usage: add 'FIRST NAME' 'LAST NAME' 'E-MAIL'\n");
     return -1;
@@ -12,6 +14,15 @@ int cmd_add(int argc, char **argv) {
 
   ab_init();
 
+  contact = ab_contact_alloc();
+
+  ab_contact_set_first_name(contact, argv[1]);
+  ab_contact_set_last_name(contact, argv[2]);
+  ab_contact_set_email(contact, argv[3]);
+
+  ab_add_contact(contact);
+
+  ab_contact_free(contact);
 
   ab_fini();
 
