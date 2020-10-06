5c6962380469e5e5415261b0a9fbac0e1afef403
Thiago Farina <tfransosi@gmail.com>
2020-10-06 11:59:25 -0400

    sab: replace lots of 'sab' occurrences
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/sab/src/config.h.cmake
+++ c/apps/sab/src/config.h.cmake
@@ -1,16 +1,16 @@
 /* config.h.  Generated from config.h.cmake by cmake.  */
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "sab"
+#define PACKAGE_NAME "${PROJECT_NAME}"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "sab"
+#define PACKAGE_TARNAME "${PROJECT_NAME}"
 
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "${SAB_VERSION}"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "sab ${SAB_VERSION}"
+#define PACKAGE_STRING "${PROJECT_NAME} ${SAB_VERSION}"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
@@ -19,7 +19,7 @@
 #define PACKAGE_URL "https://sab.com"
 
 /* Name of package */
-#define PACKAGE "sab"
+#define PACKAGE "${PROJECT_NAME}"
 
 /* Version number of package */
 #define VERSION "${SAB_VERSION}"
