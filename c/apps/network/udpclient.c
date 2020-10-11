c91aeb9dfbebfa7f77446bc9fc2f3c3b4c2e32df
Thiago Farina <tfransosi@gmail.com>
2020-10-11 08:40:27 -0400

    udpclient.c: hostname is a positional (and required) argument
    To make that clear, docopt.org states that words starting with "<", ending with ">"
    and upper-case words are interpreted as positional arguments.
    
    So we try to follow that here.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/udpclient.c
+++ c/apps/network/udpclient.c
@@ -24,7 +24,7 @@ int main(int argc, char **argv) {
   ssize_t recvlen;
 
   if (argc != 2) {
-    fprintf(stderr, "usage: %s [hostname]\n", argv[0]);
+    fprintf(stderr, "usage: %s <hostname>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
