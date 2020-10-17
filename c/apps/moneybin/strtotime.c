3be2737030c37993d0592b15d1015a943a920a3b
Thiago Farina <tfransosi@gmail.com>
2020-10-17 13:41:46 -0400

    moneybin: add a tool to convert a string to time_t
    https://github.com/korun/dcs-system/blob/5f98fa26466d2fbac242ca2510bdefd11a647efd/server/sync/sync.c#L53
    https://github.com/unwicky/antcitytool/blob/f24f76ab8b15f0635a075227606d4dda1b657602/src/util/at_util.c#L23
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/moneybin/strtotime.c
@@ -0,0 +1,53 @@
+#define _XOPEN_SOURCE       /* See feature_test_macros(7) */
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+
+static time_t str_to_time(char const *str) {
+  struct tm time_tm;
+  time_t unixtime;
+
+  if ((str == NULL) || (*str == '\0')) {
+    return (time_t) -1;
+  }
+
+  memset(&time_tm, 0, sizeof(struct tm));
+
+  strptime(str, "%Y-%m-%d", &time_tm);
+
+  unixtime = mktime(&time_tm);
+
+  return unixtime;
+}
+
+static void time_to_str(time_t t, char *dst, int len) {
+  struct tm *time_tm;
+
+  if (dst == NULL) {
+    return;
+  }
+
+  time_tm = localtime(&t);
+  strftime(dst, len, "%D", time_tm);
+}
+
+int main(int argc, char **argv) {
+  char *start;
+  time_t start_time;
+  char samestart[32];
+  
+  if (argc < 2) {
+    fputs("usage: strtotime YYYY-MM-DD\n", stderr);
+    return 1;
+  }
+
+  start = argv[1];
+
+  start_time = str_to_time(start);
+
+  time_to_str(start_time, samestart, sizeof(samestart));
+
+  printf("%s\n", samestart);
+
+  return 0;
+}
