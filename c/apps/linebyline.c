a6639f152a4c63fcac28151df1eb03ae314a9db7
Thiago Farina <tfransosi@gmail.com>
2020-10-19 05:07:48 -0400

    linebyline.c: avoid exit calls
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/linebyline.c
+++ c/apps/linebyline.c
@@ -13,23 +13,24 @@ int main(int argc, char **argv) {
 
   if (argc != 2) {
     fprintf(stderr, "usage: linebyline <filename>\n");
-    exit(EXIT_FAILURE);
+    return 1;
   }
 
   if (stat(argv[1], &st)) {
     fprintf(stderr, "%s: %s\n", argv[1], strerror(errno));
-    exit(EXIT_FAILURE);
+    return 1;
   }
 
   fp = fopen(argv[1], "r");
   if (fp == NULL) {
     fprintf(stderr, "%s: %s\n", argv[1], strerror(errno));
-    exit(EXIT_FAILURE);
+    return 1;
   }
 
   /* This loops through the lines of the file, printing it. */
   while (fgets(linebuf, sizeof(linebuf), fp)) {
     lineno++;
+
     printf("%d:%s", lineno, linebuf);
 
     /* This loops through *every* character present in this line. */
