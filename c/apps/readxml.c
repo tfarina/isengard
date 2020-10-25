26e293413e9a16850bcb135ff4391a57dd860ac6
Thiago Farina <tfransosi@gmail.com>
2020-10-25 15:35:01 -0400

    readxml.c: rename 'xml' variable to 'tree'
    So it is easier to grep and differentiate.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/readxml.c
+++ c/apps/readxml.c
@@ -2,10 +2,11 @@
 
 #include "third_party/mxml/mxml.h"
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
   FILE *fp = NULL;
-  mxml_node_t *xml = NULL;
+  mxml_node_t *tree = NULL;
   mxml_node_t *node = NULL;
 
   fp = fopen("books.xml", "r");
@@ -13,22 +14,22 @@ int main(int argc, char **argv)
     return -1;
   }
 
-  xml = mxmlLoadFile(NULL, fp, MXML_OPAQUE_CALLBACK);
+  tree = mxmlLoadFile(NULL, fp, MXML_OPAQUE_CALLBACK);
   fclose(fp);
 
-  if (xml == NULL) {
+  if (tree == NULL) {
     fprintf(stderr, "Unable to read xml file '%s'.\n", "books.xml");
     return -1;
   }
 
-  node = mxmlFindElement(xml, xml, "book", NULL, NULL, MXML_DESCEND);
+  node = mxmlFindElement(tree, tree, "book", NULL, NULL, MXML_DESCEND);
   if (node == NULL) {
-    fputs("Unable to find first <book> element in XML tree.\n", stderr);
-    mxmlDelete(xml);
+    fputs("Unable to find the <book> element in the XML.\n", stderr);
+    mxmlDelete(tree);
     return -1;
   }
 
-  mxmlDelete(xml);
+  mxmlDelete(tree);
 
   return 0;
 }
