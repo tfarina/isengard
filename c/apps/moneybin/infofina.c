becc4af89ad9d87d1a772c3252d90c54aa060ecb
Thiago Farina <tfransosi@gmail.com>
2020-11-06 13:59:35 -0500

    infofina.c: parse another important information - NumeroConta
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/infofina.c
+++ c/apps/moneybin/infofina.c
@@ -15,6 +15,8 @@ main(int argc, char **argv)
   mxml_node_t *tree;
   mxml_node_t *node;
   mxml_node_t *num_ident;
+  mxml_node_t *account_plan;
+  mxml_node_t *account_num;
 
   xmlfile = "InfoFinaDFin.xml";
   fp = fopen(xmlfile, "r");
@@ -45,7 +47,21 @@ main(int argc, char **argv)
       fputs("DescricaoConta1 not found!\n", stderr);
       break;
     } else {
-      printf("DescricaoConta1 is: %s\n", mxmlGetOpaque(num_ident));
+      printf("DescricaoConta1: %s\n", mxmlGetOpaque(num_ident));
+    }
+
+    account_plan = mxmlFindElement(node, tree, "PlanoConta", NULL, NULL, MXML_DESCEND);
+    if (account_plan == NULL) {
+      fputs("PlanoConta not found!\n", stderr);
+      break;
+    } else {
+      account_num = mxmlFindElement(account_plan, tree, "NumeroConta", NULL, NULL, MXML_DESCEND);
+      if (account_num == NULL) {
+        fputs("NumeroConta not found!\n", stderr);
+        break;
+      } else {
+        printf("NumeroConta: %s\n", mxmlGetOpaque(account_num));
+      }
     }
 
     node = mxmlFindElement(node, tree, "InfoFinaDFin", NULL, NULL, MXML_DESCEND);
