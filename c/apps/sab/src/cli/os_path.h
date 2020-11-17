e786a35ce034e1c075410f33b38be3cc5ab9a0a7
Thiago Farina <tfransosi@gmail.com>
2020-11-17 12:56:52 -0500

    os_path.h: add doc comment to 'os_path_basename' function
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/sab/src/cli/os_path.h
+++ c/apps/sab/src/cli/os_path.h
@@ -1,6 +1,9 @@
 #ifndef _OS_PATH_H_INCLUDED_
 #define _OS_PATH_H_INCLUDED_ 1
 
+/**
+ * Returns the last element of path.
+ */
 char const *os_path_basename(char const *path);
 
 #endif  /* !defined(_OS_PATH_H_INCLUDED_) */
