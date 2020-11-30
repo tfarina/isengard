6b0de6a6c23100d4ed77879c7ade9fb39cfb575e
Thiago Farina <tfransosi@gmail.com>
2020-11-30 18:50:06 -0500

    ffileutils.h: update f_read_file's parameter name
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/ffileutils.h
+++ c/apps/ffileutils.h
@@ -5,7 +5,7 @@
 
 char *f_build_filename(char *dir, char *file);
 
-char *f_read_file(const char *filename, size_t *rlen);
+char *f_read_file(const char *filename, size_t *out_file_size);
 
 int f_write_file(const char *filename, const char *data, size_t size);
 
