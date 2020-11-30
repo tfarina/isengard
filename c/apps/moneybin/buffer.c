afd9e0369ebcc64915cb96c5f372644c6f2c851a
Thiago Farina <tfransosi@gmail.com>
2020-11-30 13:29:30 -0500

    moneybin: check if memory has been allocated for buffer's data
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/buffer.c
+++ c/apps/moneybin/buffer.c
@@ -11,9 +11,13 @@ void buffer_init(buffer_t *buf)
 
 void buffer_append(buffer_t *buf, char *data, size_t size)
 {
-  buf->data = (char *)realloc(buf->data, buf->size + size + 1);
-  if (buf->data == NULL) {
-    return;
+  if (0 == buf->data) {
+    buf->data = malloc(size + 1);
+  } else {
+    buf->data = (char *)realloc(buf->data, buf->size + size + 1);
+    if (buf->data == NULL) {
+      return;
+    }
   }
 
   memcpy(&(buf->data[buf->size]), data, size);
