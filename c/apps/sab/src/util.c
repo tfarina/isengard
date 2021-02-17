5a4b6945880a3876266d65d5ceb7a4e81c5f2bb6
Thiago Farina <tfransosi@gmail.com>
2021-02-17 12:20:14 -0500

    sab: check the return value of 'f_mkdirp'
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/sab/src/util.c
+++ c/apps/sab/src/util.c
@@ -6,10 +6,14 @@
 
 int ensure_data_dir(void) {
   char const *data_dir;
+  int retval;
 
   data_dir = dirs_get_user_data_dir();
   if (!test_isdir(data_dir)) {
-    f_mkdirp(data_dir, 0700);
+    retval = f_mkdirp(data_dir, 0700);
+    if (retval < 0) {
+      return 0;
+    }
   }
 
   return 1;
