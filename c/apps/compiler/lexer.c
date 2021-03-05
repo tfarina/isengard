6cba04111cae2eadec56cd9ec1a9fdd202339a0f
Thiago Farina <tfransosi@gmail.com>
2021-03-05 10:21:22 -0500

    compiler: store the token kind in a variable for further use
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/compiler/lexer.c
+++ c/apps/compiler/lexer.c
@@ -2,6 +2,10 @@
 
 #include <stdio.h>
 
+#include "token.h"
+
+static token_kind_t kind;
+
 void lex(const char *input)
 {
   const char *c;
@@ -9,15 +13,19 @@ void lex(const char *input)
   for (c = input; *c; ++c) {
     switch (*c) {
       case '[':
+        kind = TOK_LBRACKET;
         printf("<'[', LEFT_BRACKET>\n");
         break;
       case ']':
+	kind = TOK_RBRACKET;
         printf("<']', RIGHT_BRACKET>\n");
         break;
       case '{':
+	kind = TOK_LBRACE;
         printf("<'{', LEFT_BRACE>\n");
         break;
       case '}':
+	kind = TOK_RBRACE;
         printf("<'}', RIGHT_BRACE>\n");
         break;
     }
