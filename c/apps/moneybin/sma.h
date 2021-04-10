56b1da3f9e5fb07682c6725928eed2666d0d8f2c
Thiago Farina <tfransosi@gmail.com>
2021-04-10 15:30:30 -0400

    moneybin: add doc comment to smovavg function
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/sma.h
+++ c/apps/moneybin/sma.h
@@ -3,6 +3,16 @@
 
 #include <stddef.h>
 
+/**
+ * Simple Moving Average
+ *
+ * @param[in] arr
+ * @param[in] size
+ * @param[in] window
+ * @param[out] outarr
+ *
+ * @return simple moving average
+ */
 void smovavg(double const *arr, size_t size, int window, double *outarr);
 
 #endif /* SMA_H_ */
