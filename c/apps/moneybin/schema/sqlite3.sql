0819701fdaf2fafa93733675835a4b3746eba18c
Thiago Farina <tfransosi@gmail.com>
2021-05-10 14:54:50 -0400

    moneybin: complete the set of tables to build the financial data database
    https://pythonforfinance.net/2020/10/24/build-a-financial-data-database-with-python/
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/schema/sqlite3.sql
+++ c/apps/moneybin/schema/sqlite3.sql
@@ -1,3 +1,32 @@
+CREATE TABLE exchange (
+  id INTEGER,
+  name TEXT NOT NULL,
+  currency,
+  code TEXT NOT NULL UNIQUE,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE company (
+  id INTEGER,
+  name TEXT NOT NULL,
+  industry TEXT,
+  sector TEXT,
+  hq_location TEXT,
+  securityID INTEGER,
+  PRIMARY KEY(id),
+  FOREIGN KEY(securityID) REFERENCES security (id)
+);
+
+CREATE TABLE security (
+  id INTEGER,
+  ticker TEXT NOT NULL UNIQUE,
+  name TEXT NOT NULL,
+  companyID INTEGER,
+  exchangeID INTEGER,
+  FOREIGN KEY(companyID) REFERENCES company (id),
+  FOREIGN KEY(exchangeID) REFERENCES exchange (id)
+);
+
 CREATE TABLE security_price (
   id INTEGER,
   date TEXT NOT NULL,
