59b85c81dfe9825204995ccaf4fe88fc5ed5e6f2
Thiago Farina <tfransosi@gmail.com>
2021-07-18 07:30:45 -0400

    ema.c: add formula and more comments
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/ema.c
+++ c/apps/moneybin/ema.c
@@ -8,12 +8,13 @@ void ema(double const *arr, size_t size, int lookback_period, double *outarr)
 {
   float val;
   int i;
-  float k = 2 / ((float)lookback_period + 1);
+  float k = 2 / ((float)lookback_period + 1); /* 2 is the smoothing factor */
 
   val = *arr;
   *outarr++ = val;
 
   for (i = 1; i < size; i++) {
+    /* EMA = [close - EMA(previous day)] * multiplier + EMA(previous day)*/
     val = (*(arr + i) - val) * k + val;
 
     *outarr++ = val;
