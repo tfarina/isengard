c5889d259b690b660732e519cded98d7090314e4
Thiago Farina <tfransosi@gmail.com>
2021-07-21 12:52:21 -0400

    ema.c: separate initialization of 'k' variable from declaration
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/ema.c
+++ c/apps/moneybin/ema.c
@@ -8,7 +8,9 @@ void ema(double const *arr, size_t size, int period, double *outarr)
 {
   float val;
   int i;
-  float k = 2 / ((float)period + 1); /* 2 is the smoothing factor */
+  float k;
+
+  k = 2 / ((float)period + 1); /* 2 is the smoothing factor */
 
   val = *arr;
   *outarr++ = val;
