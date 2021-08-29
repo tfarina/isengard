694e0e1e6a2b6253e592947d629e6929e7b931db
Thiago Farina <tfransosi@gmail.com>
2021-08-29 16:47:47 -0400

    echod: add help usage to verbosity tool
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/verbosity.c
+++ c/apps/network/echod/src/verbosity.c
@@ -6,15 +6,33 @@
 
 static int log_level = 2;
 
+static void
+usage(int status)
+{
+  printf("Usage: %s [OPTION]... FILE\n\n", "verbosity");
+  fputs(
+"  -v[v...], --verbosity  increase verbosity (can use multiple v)\n"
+       "  -vNUM, --verbosity=NUM set verbosity to NUM (higher level means more output):\n"
+       "                          -v1 - only errors and messages\n"
+       "                          -v2 - also warnings (default level)\n"
+       "                          -v3 - also information messages and timestamps\n"
+"                          -v4 - lots of debug\n", stdout);
+  fputs(
+"  -h, --help             display this help and exit\n", stdout);
+
+  exit(status);
+}
+
 int
 main(int argc, char **argv)
 {
   int optchr;
 
-  static char const short_options[] = "v::";
+  static char const short_options[] = "hv::";
   static struct option const long_options[] = {
-      { "verbosity", optional_argument, 0, 'v' },
-      { (char *) 0, no_argument, (int *) 0, 0 }
+    { "help",      no_argument,       0, 'h' },
+    { "verbosity", optional_argument, 0, 'v' },
+    { (char *) 0, no_argument, (int *) 0, 0 }
   };
 
   optind = 0;
@@ -29,6 +47,12 @@ main(int argc, char **argv)
     }
 
     switch (optchr) {
+    default:
+      usage(EXIT_FAILURE);
+
+    case 'h':
+      usage(EXIT_SUCCESS);
+
     case 'v':
       if (optarg) {
         if (optarg[0] == 'v') {
