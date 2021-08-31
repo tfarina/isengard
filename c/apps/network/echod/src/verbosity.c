2f44e1c14fcfb537d7a73f1a4d5b46ed75392449
Thiago Farina <tfransosi@gmail.com>
2021-08-31 19:03:03 -0400

    echod: fix indentation of usage string
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/verbosity.c
+++ c/apps/network/echod/src/verbosity.c
@@ -15,10 +15,10 @@ usage(int status)
   printf("Usage: %s [OPTION]... FILE\n\n", "verbosity");
   fputs(
 "  -v[v...], --verbosity  increase verbosity (can use multiple v)\n"
-       "  -vNUM, --verbosity=NUM set verbosity to NUM (higher level means more output):\n"
-       "                          -v1 - only errors and messages\n"
-       "                          -v2 - also warnings (default level)\n"
-       "                          -v3 - also information messages and timestamps\n"
+"  -vNUM, --verbosity=NUM set verbosity to NUM (higher level means more output):\n"
+"                          -v1 - only errors and messages\n"
+"                          -v2 - also warnings (default level)\n"
+"                          -v3 - also information messages and timestamps\n"
 "                          -v4 - lots of debug\n", stdout);
   fputs(
 "  -h, --help             display this help and exit\n", stdout);
