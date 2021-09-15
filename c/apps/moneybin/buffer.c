0287cb74807814794b13d7a4a29ae328e477cce7
Thiago Farina <tfransosi@gmail.com>
2021-09-15 19:19:27 -0400

    moneybin: buffer.c: always call sizeof for the data type being allocated
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/buffer.c
+++ c/apps/moneybin/buffer.c
@@ -12,7 +12,7 @@ void buffer_init(buffer_t *buf)
 void buffer_append(buffer_t *buf, char *data, size_t size)
 {
   if (0 == buf->data) {
-    buf->data = malloc(size + 1);
+    buf->data = malloc(sizeof(char) * size + 1);
   } else {
     buf->data = (char *)realloc(buf->data, buf->size + size + 1);
     if (buf->data == NULL) {
