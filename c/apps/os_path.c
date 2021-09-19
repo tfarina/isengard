90749cded38b34e460936e088762e808019f006e
Thiago Farina <tfransosi@gmail.com>
2021-09-19 05:26:45 -0400

    os_path: add 'join' function to 'os_path' module
    It is a copy of 'f_build_filename' function from 'ffileutils' module.
    
    This 'os_path' module is inspired on Python's os.path module.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/os_path.c
+++ c/apps/os_path.c
@@ -1,5 +1,6 @@
 #include "os_path.h"
 
+#include <stdlib.h>
 #include <string.h>
 
 #define DIR_SEP '/'
@@ -15,4 +16,27 @@ char const *os_path_basename(char const *path) {
   }
 
   return last_slash + 1; /* Move the pointer past the '/'. */
+}
+
+char *
+os_path_join(char *dir, char *file)
+{
+  char *path;
+  size_t len;
+
+  /* Add 2 bytes to the resulting length to have enough memory available
+   * for the directory separator ('/') and the null byte ('\0').
+   */
+  len = strlen(dir) + strlen(file) + 2;
+
+  path = malloc(sizeof(char) * len);
+  if (path == NULL) {
+    return NULL;
+  }
+
+  strcpy(path, dir);
+  strcat(path, "/");
+  strcat(path, file);
+
+  return path;
 }
--- c/apps/os_path.h
+++ c/apps/os_path.h
@@ -6,4 +6,6 @@
  */
 char const *os_path_basename(char const *path);
 
+char *os_path_join(char *dir, char *file);
+
 #endif  /* !defined(_OS_PATH_H_INCLUDED_) */
