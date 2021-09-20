c47bb0ba319471f6167ce3ecb232512c2d4b112c
Thiago Farina <tfransosi@gmail.com>
2021-09-20 11:52:26 -0400

    present_value.c: indicate that the number of periods is in years
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/present_value.c
+++ c/apps/present_value.c
@@ -7,9 +7,9 @@ int main(void)
   double principal, rate, periods;
   double pv;
 
-  principal = 100;
+  principal = 100; /* dollars */
   rate = 0.05; /* 5% */
-  periods = 5;
+  periods = 5; /* years */
 
   pv = present_value(principal, rate, periods);
 
