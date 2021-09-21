b81a0f6e909c0542bf99eaa117451bc0fcf039bb
Thiago Farina <tfransosi@gmail.com>
2021-09-21 16:53:33 -0400

    echod: add an example to DPRINT documentation
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/debug.h
+++ c/apps/network/echod/src/debug.h
@@ -4,16 +4,21 @@
 #ifndef _DEBUG_H_INCLUDED_
 #define _DEBUG_H_INCLUDED_ 1
 
+/**
+ * This must be called with an extra level of parentheses because it's not
+ * possible to pass a variable number of arguments to a macro
+ * (in portable C89).
+ *
+ * Example:
+ *   DPRINT(("this is a debug message"));
+ */
+
 #ifdef NDEBUG
 
 #define DPRINT(msg) ((void) 0)
 
 #else
 
-/**
- * This must be called with an extra level of parentheses because it's not
- * possible to pass a variable number of arguments to a macro (in portable C89).
- */
 #define DPRINT(msg)  \
   do {               \
   } while (0)
