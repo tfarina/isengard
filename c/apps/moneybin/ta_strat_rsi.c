f884a04ac274df3d4402b3cfe4763797724eff1f
Thiago Farina <tfransosi@gmail.com>
2021-09-22 13:23:35 -0400

    moneybin: add explanations for rsi's trading signals
    Explain what it means if it rises above overbought (70, for example) and
    falls below oversold (30, for example).
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/ta_strat_rsi.c
+++ c/apps/moneybin/ta_strat_rsi.c
@@ -2,9 +2,17 @@
 
 int ta_strat_rsi(double rsi, int oversold, int overbought)
 {
+  /* If calculated rsi value FALLS below the oversold threshold,
+   * signs point to an oversold stock; the price of the stock could
+   * very well bounce back (upwards) and start to increase.
+   */
   if ((int)rsi < oversold) {
     return TA_SIGNAL_BUY;
   }
+  /* If calculated rsi value RISES above the overbought threshold,
+   * signs point to an overbought stock; the price of the stock could
+   * very well fall back (downwards) in the near future and start to decrease.
+   */
   if ((int)rsi > overbought) {
     return TA_SIGNAL_SELL;
   }
