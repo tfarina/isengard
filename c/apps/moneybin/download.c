2f17a7200d9043ec76091e5a2cb24c84ec8af440
Thiago Farina <tfransosi@gmail.com>
2021-09-30 17:18:58 -0400

    moneybin: rework the code a little bit in 'download_file' function
    To keep most of curl setup code together and mixed with anything else.
    
    Close file pointer in more error locations.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/download.c
+++ c/apps/moneybin/download.c
@@ -18,29 +18,27 @@ download_file(char const *downloadurl, char const *destfile)
     return -1;
   }
 
+  fp = fopen(destfile, "wb");
+  if (fp == NULL) {
+    return -1;
+  }
+
   curl = curl_easy_init();
   if (curl == NULL) {
     fprintf(stderr, "curl_easy_init() failed\n");
+    fclose(fp);
     return -1;
   }
 
   curl_easy_setopt(curl, CURLOPT_URL, downloadurl);
   curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
   curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 20L);
-
   /*
    * It was necessary to set CURLOPT_SSL_VERIFYPEER to fix the following error:
    *
    * curl_easy_perform() failed: Peer certificate cannot be authenticated with given CA certificates
    */
   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-  
-  fp = fopen(destfile, "wb");
-  if (fp == NULL) {
-    curl_easy_cleanup(curl);
-    return -1;
-  }
-
   /*
    * By not setting CURLOPT_WRITEFUNCTION here, libcurl will use 'fwrite'
    * as a callback by default.
@@ -51,12 +49,13 @@ download_file(char const *downloadurl, char const *destfile)
   if (result != CURLE_OK) {
     fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(result));
     curl_easy_cleanup(curl);
+    fclose(fp);
     return -1;
   }
 
-  fclose(fp);
-
   curl_easy_cleanup(curl);
+
+  fclose(fp);
 
   curl_global_cleanup();
 
