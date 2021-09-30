fb585eb8e1fb94d11add079afed88ac537f5bb4f
Thiago Farina <tfransosi@gmail.com>
2021-09-30 12:58:22 -0400

    calcpivot.c: show the date to which the data refers to
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/calcpivot.c
+++ c/apps/moneybin/calcpivot.c
@@ -9,6 +9,7 @@ int main(int argc, char **argv)
   int err;
   char *filename;
   ta_bars_t *bars;
+  int year, month, day;
   double high, low, close;
   double pp, r1, r2, r3, s1, s2, s3;
 
@@ -37,6 +38,8 @@ int main(int argc, char **argv)
   high = bars->high[bars->numrows - 1];
   low = bars->low[bars->numrows - 1];
 
+  ta_getdate(&bars->timestamp[bars->numrows - 1], &year, &month, &day);
+
   pp = pivot_classic_pp(high, low, close);
   r1 = pivot_classic_r1(pp, low);
   r2 = pivot_classic_r2(pp, high, low);
@@ -45,8 +48,8 @@ int main(int argc, char **argv)
   s2 = pivot_classic_s2(pp, high, low);
   s3 = pivot_classic_s3(pp, high, low);
 
-  printf("Close\tHigh\tLow\n");
-  printf("%.2f\t%.2f\t%.2f\n\n", close, high, low);
+  printf("Close\tHigh\tLow\tDate\n");
+  printf("%.2f\t%.2f\t%.2f\t%.4d-%.2d-%.2d\n\n", close, high, low, year, month, day);
 
   printf("   %7s\n", "Classic");
   printf("S3: %4.2f\n", s3);
