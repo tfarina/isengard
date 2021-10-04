1ff7c3e9216fc4423047bf53d0621b7b5a05dfce
Thiago Farina <tfransosi@gmail.com>
2021-10-04 10:04:51 -0400

    moneybin: start documenting the iso8601 functions
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/iso8601.h
+++ c/apps/moneybin/iso8601.h
@@ -1,8 +1,14 @@
 #ifndef _ISO8601_H_INCLUDED_
 #define _ISO8601_H_INCLUDED_
 
+/**
+ * Converts a date/time in ISO 8601 format to a "struct tm".
+ */
 time_t str_to_time(char const *str);
 
+/**
+ * Converts a struct tm into an ISO 8601 standard date.
+ */
 void time_to_str(time_t t, char *dst, int len);
 
 #endif  /* !defined(_ISO8601_H_INCLUDED_) */
