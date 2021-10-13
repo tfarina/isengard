ca6c95c03f317dafc90d4222d4fa27ca1c3705da
Thiago Farina <tfransosi@gmail.com>
2021-10-13 09:39:17 -0400

    moneybin: move 'str_xxx' functions into fstrutils.[ch] module
    So it can be reused elsewhere, like in b3companies.c, etc.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/fstrutils.h
+++ c/apps/moneybin/fstrutils.h
@@ -3,4 +3,10 @@
 
 char *f_strdup(char const *src);
 
+int str_startswith(char const *str, char const *prefix);
+char *str_substring(char const *str, int start, int end);
+char *str_strip_leading(char *str);
+char *str_strip_trailing(char *str);
+char *str_strip(char *str);
+
 #endif  /* STRUTILS_H_ */
