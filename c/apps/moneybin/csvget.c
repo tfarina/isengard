4dac70c4e035bccf3c47d1a3d45a6a616c3ca1bd
Thiago Farina <tfransosi@gmail.com>
2021-10-15 12:57:55 -0400

    csvget.c: add a missing newline to error message
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/csvget.c
+++ c/apps/moneybin/csvget.c
@@ -74,7 +74,7 @@ static int http_get(char const *url, buffer_t *outbuf)
 
   curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
   if (response_code != 200) {
-    fprintf(stderr, "http_get: curl_easy_perform() returned code: %d", response_code);
+    fprintf(stderr, "http_get: curl_easy_perform() returned code: %d\n", response_code);
     curl_easy_cleanup(curl);
     return -1;
   }
