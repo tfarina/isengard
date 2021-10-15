6080ec37ec7f3b110912e1ca2c99ab848a107903
Thiago Farina <tfransosi@gmail.com>
2021-10-15 08:09:31 -0400

    moneybin: add new enum to store the values of bdi codes
    All the values came from SeriesHistoricas_Layout.pdf (TABELA CODBDI).
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/moneybin/b3types.h
+++ c/apps/moneybin/b3types.h
@@ -56,4 +56,46 @@ typedef enum market_type_e {
   MT_PUT = 80
 } market_type_t;
 
+/**
+ * TABELA DE CODBDI - RELAÇÃO DOS VALORES PARA CÓDIGOS DE BDI
+ */
+typedef enum security_bdi_e {
+  SB_INVALID = -1, /** Valor invalido */
+  SB_PADRAO = 2, /** Lote Padrao */
+  SB_CONCORDATARIA = 6, /** Concordataria */
+  SB_DIREITOS_E_RECIBOS = 10, /** Direitos e recibos */
+  SB_FUNDOS_IMOBILIARIOS = 12, /** Fundos imobiliarios */
+  SB_CERTIFIC_INVESTIMENTO = 14, /** Certific. investimento / debentures / titulo divida publica */
+  SB_OBRIGACOES = 18, /** Obrigacoes */
+  SB_BONUS_PRIVADOS = 22, /** Bonus (privados) */
+  SB_APOLICES = 26, /** Apolices / bonus / titulos publicos */
+  SB_EXERCICIO_COMPRA_INDICE = 32, /** Exercicio de opcoes de compra de indice */
+  SB_EXERCICIO_VENDA_INDICE = 33, /** Exercicio de opcoes de venda de indice */
+  SB_EXERCICIO_COMPRA = 38, /** Exercicio de opcoes de compra */
+  SB_EXERCICIO_VENDA = 42, /** Exercicio de opcoes de venda */
+  SB_LEILAO_TITULOS_N_COTADOS = 46, /** Leilao de titulos nao cotados */
+  SB_LEILAO_PRIVATIZACAO = 48, /** Leilao de privatizacao */
+  SB_LEILAO = 50, /** Leilao */
+  SB_LEILAO_FINOR = 51, /** Leilao FINOR */
+  SB_LEILAO_FINAM = 52, /** Leilao FINAM */
+  SB_LEILAO_FISET = 53, /** Leilao FISET */
+  SB_LEILAO_ACOES_MORA = 54, /** Leilao de acoes em mora */
+  SB_VENDAS_ALVARA_JUDICIAL = 56, /** Vendas por alvara judicial */
+  SB_OUTROS = 58, /** Outros */
+  SB_PERMUTA = 60, /** Permuta por acoes */
+  SB_META = 61, /** Meta */
+  SB_TERMO = 62, /** Termo */
+  SB_DEBENTUR_VENCIMENTO_ATE3 = 66, /** Debentures com data de vencimento ate 3 anos */
+  SB_DEBENTUR_VENCIMENTO_MAI3 = 68, /** Debentures com data de vencimento maior que 3 anos */
+  SB_FUTURO_MOV_CONTINUA = 70, /** Futuro com movimentacao continua */
+  SB_FUTURO_RETENCAO_GANHO = 71, /** Futuro com retencao de ganho */
+  SB_OPCOES_COMPRA_INDICE = 74, /** Opcoes de compra de indices */
+  SB_OPCOES_VENDA_INDICE = 75, /** Opcoes de venda de indices */
+  SB_OPCOES_COMPRA = 78, /** Opcoes de compra */
+  SB_OPCOES_VENDA = 82, /** Opcoes de venda */
+  SB_DEBENTUR_NOTA_PROMISSORIA = 83, /** Debentures e notas promissorias */
+  SB_FRACIONARIO = 96, /** Fracionario */
+  SB_TOTAL_GERAL = 99 /** Total geral */
+} security_bdi_t;
+
 #endif  /* !defined(_B3TYPES_H_INCLUDED_) */
