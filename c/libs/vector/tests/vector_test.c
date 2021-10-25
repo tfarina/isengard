8c361bbb50691f8d74b2a2d6845fa47c323940e6
Thiago Farina <tfransosi@gmail.com>
2021-10-25 07:54:13 -0400

    vector: add a small test case to make sure 'push_back' function is working
    And if it is, when we make changes, it doesn't stop working.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/libs/vector/tests/vector_test.c
+++ c/libs/vector/tests/vector_test.c
@@ -15,7 +15,25 @@ TEST vector_test_create(void)
   PASS();
 }
 
+TEST vector_test_push_back(void)
+{
+  vector_t *vp;
+  int i = 10;
+  int *ip;
+  vp = vector_create(sizeof(int), 2);
+
+  ASSERT_EQ(0, vector_size(vp));
+
+  vector_push_back(vp, &i);
+  ASSERT_EQ(1, vector_size(vp));
+
+  vector_destroy(vp);
+
+  PASS();
+}
+
 SUITE(vector_suite)
 {
   RUN_TEST(vector_test_create);
+  RUN_TEST(vector_test_push_back);
 }
