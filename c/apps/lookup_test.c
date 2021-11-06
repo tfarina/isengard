9f7fc7f6c49f3dd3431ce3d6cf13af037dbbfb9d
Thiago Farina <tfransosi@gmail.com>
2021-11-06 12:16:03 -0400

    lookup_test.c: naming clean up
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/lookup_test.c
+++ c/apps/lookup_test.c
@@ -2,15 +2,15 @@
 
 #include "lookup.h"
 
-enum log_severity {
+typedef enum {
         LOG_INFO = 0,
         LOG_WARNING = 1,
         LOG_ERROR = 2,
         LOG_FATAL = 3,
-        LOG_NUM_SEVERITIES = 4,
-};
+        LOG_COUNT = 4
+} log_level_t;
 
-static lookup_entry_t log_priority_info_table[] = {
+static lookup_entry_t log_level_table[] = {
         { LOG_INFO, "info" },
         { LOG_WARNING, "warning" },
         { LOG_ERROR, "error" },
@@ -23,7 +23,7 @@ main(void)
 {
         lookup_entry_t *item;
 
-	item = lookup_entry_by_id(log_priority_info_table, LOG_FATAL);
+	item = lookup_entry_by_id(log_level_table, LOG_FATAL);
 	if (item) {
 	        printf("%s\n", item->name);
 	}
