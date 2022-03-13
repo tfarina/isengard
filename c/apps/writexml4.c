253f98ed48e11cccd9a3a21167efa79d4cd35b0c
Thiago Farina <tfransosi@gmail.com>
2022-03-13 12:17:07 -0400

    writexml4.c: add a reference to the subfolder
    That way it will create a structure like a tree.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/writexml4.c
+++ c/apps/writexml4.c
@@ -4,14 +4,20 @@
 #include "third_party/libuuid/uuid.h"
 
 #define ELEM_FOLDER "folder"
+#define ELEM_ITEM_LIST "item-list"
+#define ELEM_ITEM "item"
 #define ATTR_UID "uid"
 #define ATTR_NAME "name"
+#define ATTR_TYPE "type"
 
 int main(int argc, char **argv)
 {
   mxml_node_t *xml = NULL;
   mxml_node_t *abook = NULL;
   mxml_node_t *folder_node = NULL;
+  mxml_node_t *child_node = NULL;
+  mxml_node_t *item_list_node = NULL;
+  mxml_node_t *item_node = NULL;
   uuid_t uuid;
   char uuid_str[37];
   FILE *fp = NULL;
@@ -30,9 +36,14 @@ int main(int argc, char **argv)
   uuid_generate(uuid);
   uuid_unparse(uuid, uuid_str);
 
-  folder_node = mxmlNewElement(abook, ELEM_FOLDER);
-  mxmlElementSetAttr(folder_node, ATTR_UID, uuid_str);
-  mxmlElementSetAttr(folder_node, ATTR_NAME, "subfolder");
+  child_node = mxmlNewElement(abook, ELEM_FOLDER);
+  mxmlElementSetAttr(child_node, ATTR_UID, uuid_str);
+  mxmlElementSetAttr(child_node, ATTR_NAME, "subfolder");
+
+  item_list_node = mxmlNewElement(folder_node, ELEM_ITEM_LIST);
+  item_node = mxmlNewElement(item_list_node, ELEM_ITEM);
+  mxmlElementSetAttr(item_node, ATTR_TYPE, "folder");
+  mxmlElementSetAttr(item_node, ATTR_UID, uuid_str);
 
   fp = fopen("addrbook-02.xml", "w");
   if (fp == NULL) {
