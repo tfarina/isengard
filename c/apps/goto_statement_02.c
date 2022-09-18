e83af0d0ba0203273e7ee83c802e37f4a18ce412
Thiago Farina <tfransosi@gmail.com>
2022-09-18 08:11:35 -0400

    add an error code and return it
    https://vilimpoc.org/research/raii-in-c/
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/goto_statement_02.c
+++ c/apps/goto_statement_02.c
@@ -3,14 +3,18 @@
 int
 main(void)
 {
+  int error = 0;
+
   int socket_fd = 0;
   int bind_error = 1;
 
   if (socket_fd < 0) {
+    error = -1;
     goto cleanup_generic;
   }
 
   if (bind_error) {
+    error = -1;
     goto cleanup_socket;
   }
 
@@ -18,5 +22,5 @@ cleanup_socket:
   printf("Socket closed!\n");
 cleanup_generic:
   printf("Generic cleanup!\n");
-  return 1;
+  return error;
 }
