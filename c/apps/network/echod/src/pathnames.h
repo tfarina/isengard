4820948c8ba6a6552a294e0c6c6bd5df3c712ff2
Thiago Farina <tfarina@chromium.org>
2022-10-01 23:15:28 +0100

    echod: rename LOCAL_STATE_DIR macro to DEF_LOCALSTATEDIR
    To follow the scheme used by DEF_SYSCONFDIR.
    
    Signed-off-by: Thiago Farina <tfarina@chromium.org>

--- c/apps/network/echod/src/pathnames.h
+++ c/apps/network/echod/src/pathnames.h
@@ -5,12 +5,11 @@
 #define _PATHNAMES_H_INCLUDED_ 1
 
 #define DEF_SYSCONFDIR "/etc"
-
-#define LOCAL_STATE_DIR "/var"
+#define DEF_LOCALSTATEDIR "/var"
 
 #define DEF_CFGFILE DEF_SYSCONFDIR "/echod.conf"
 
-#define DEF_PIDDIR LOCAL_STATE_DIR "/run/echod"
+#define DEF_PIDDIR DEF_LOCALSTATEDIR "/run/echod"
 #define DEF_PIDFILE DEF_PIDDIR "/echod.pid"
 
 #endif  /* !defined(_PATHNAMES_H_INCLUDED_) */
