1c9dbfd2ee5202b4844c821c863727563b5af302
Thiago Farina <tfransosi@gmail.com>
2022-10-02 16:47:24 -0400

    echod: make pathnames.h use SYSCONFDIR and LOCALSTATEDIR as defined by cmake
    Package mantainers would use cmake this way:
    
    $ cmake -GNinja -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONFDIR=/etc -DLOCALSTATEDIR=/var
    
    If you omit the -DSYSCONFDIR option, then the default location will be ${CMAKE_INSTALL_PREFIX}/etc, which may not be what you want.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/pathnames.h
+++ c/apps/network/echod/src/pathnames.h
@@ -4,12 +4,15 @@
 #ifndef _PATHNAMES_H_INCLUDED_
 #define _PATHNAMES_H_INCLUDED_ 1
 
-#define DEF_SYSCONFDIR "/etc"
-#define DEF_LOCALSTATEDIR "/var"
+/*
+ * Paths are controlled via cmake. One can pass the following options:
+ *
+ * cmake -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONFDIR=/etc -DLOCALSTATEDIR=/var
+ */
 
-#define DEF_CFGFILE DEF_SYSCONFDIR "/echod.conf"
+#define DEF_CFGFILE SYSCONFDIR "/echod.conf"
 
-#define DEF_PIDDIR DEF_LOCALSTATEDIR "/run/echod"
+#define DEF_PIDDIR LOCALSTATEDIR "/run/echod"
 #define DEF_PIDFILE DEF_PIDDIR "/echod.pid"
 
 #endif  /* !defined(_PATHNAMES_H_INCLUDED_) */
