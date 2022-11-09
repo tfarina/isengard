9c444255a06b375e8f35b341e4771143a03f5e07
Thiago Farina <tfransosi@gmail.com>
2022-11-09 12:23:52 -0500

    echod: add a macro to concatenate two tokens (M_CONCAT)
    ref: zyantific/zycore-c (Defines.h), pgbackrest (macro.h)
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/stringize.h
+++ c/apps/network/echod/src/stringize.h
@@ -13,4 +13,18 @@
 #define M_STRINGIZE_(x) #x
 #define M_STRINGIZE(x)  M_STRINGIZE_(x)
 
+/**
+ * M_CONCAT:
+ *
+ * Merges two tokens into one while expanding macros.
+ *
+ * Refer to https://gnu-c-language-manual.github.io/GNU-C-Language-Manual/Concatenation.html
+ * for more information.
+ *
+ * @param   A   The first token.
+ * @param   B   The second token.
+ */
+#define M_CONCAT_(A, B) A##B
+#define M_CONCAT(A, B)  M_CONCAT_(A, B)
+
 #endif  /* !defined(_STRINGIZE_H_INCLUDED_) */
