4383cb0e82d0846be2e3c50f4c24d1d7bc467fc6
Thiago Farina <tfransosi@gmail.com>
2022-11-19 08:18:43 -0500

    sab: remove 'Name' from text box labels
    There is no need to repeat 'Name' in every text box label on 'Name' tab page.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/sab/src/ui/gtk/contact_editor.c
+++ c/apps/sab/src/ui/gtk/contact_editor.c
@@ -96,7 +96,7 @@ void contact_editor_new(GtkWindow *parent, action_code_t ac, ab_contact_t *conta
   gtk_table_set_col_spacings(GTK_TABLE(table), 12);
 
   /* First row. */
-  label = gtk_label_new("First Name:");
+  label = gtk_label_new("First:");
   gtk_table_attach(GTK_TABLE(table), label, 0, 1, 0, 1,
 		   GTK_FILL, 0, 0, 0);
   gtk_misc_set_alignment(GTK_MISC(label), 1, 0.5);
@@ -107,7 +107,7 @@ void contact_editor_new(GtkWindow *parent, action_code_t ac, ab_contact_t *conta
 		   GTK_EXPAND | GTK_SHRINK | GTK_FILL, 0, 0, 0);
 
   /* Second row. */
-  label = gtk_label_new("Last Name:");
+  label = gtk_label_new("Last:");
   gtk_table_attach(GTK_TABLE(table), label, 0, 1, 1, 2,
 		   GTK_FILL, 0, 0, 0);
   gtk_misc_set_alignment(GTK_MISC(label), 1, 0.5);
