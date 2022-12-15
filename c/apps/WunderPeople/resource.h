c5663bbe0ac3cf093279b8f9582cf005dde83d98
Thiago Farina <tfransosi@gmail.com>
2022-12-15 08:35:26 -0500

    WunderPeople: renumber resource-identifier using ResOrg tool
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/WunderPeople/resource.h
+++ c/apps/WunderPeople/resource.h
@@ -1,28 +1,27 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by ui.rc
 //
+#define _APS_NO_MFC                     1
+#define IDI_APP_ICON                    2
+#define IDD_ABOUTBOX                    3
+#define IDR_MENUBAR                     4
+#define IDM_EXIT                        5
+#define IDM_STATUSBAR                   6
+#define IDM_ABOUT                       7
+#define MainAccel                       8
 #define IDS_APP_TITLE                   100
-#define IDI_APP_ICON                    101
-#define IDD_ABOUTBOX                    102
-#define IDR_MENUBAR                     103
-#define IDM_EXIT                        104
-#define IDM_STATUSBAR                   105
-#define IDM_ABOUT                       106
-#define MainAccel                       2007
+#define IDC_LISTVIEW                    2001
 #define IDC_STATIC                      -1
 
-#define IDC_LISTVIEW                    2001
-
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NO_MFC                     1
-#define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_3D_CONTROLS                1
+#define _APS_NEXT_RESOURCE_VALUE        101
 #define _APS_NEXT_COMMAND_VALUE         32772
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           110
+#define _APS_NEXT_CONTROL_VALUE         2003
+#define _APS_NEXT_SYMED_VALUE           2103
 #endif
 #endif
