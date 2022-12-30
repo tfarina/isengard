8cbed9d57b40d4b9952806fc52b036ece539eb5c
Thiago Farina <tfransosi@gmail.com>
2022-12-30 11:15:10 -0500

    sab: implement 'sab change' command
    Like it is already implemented in the standalone 'sab-change' tool.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/sab/src/ui/cli/cmd_change.c
+++ c/apps/sab/src/ui/cli/cmd_change.c
@@ -2,10 +2,51 @@
 
 #include <stdio.h>
 
+#include "ab.h"
+#include "dirs.h"
+#include "os_path.h"
+#include "util.h"
+
 int cmd_change(int argc, char **argv) {
-  printf("New argc count: %d\n", argc);
-  printf("New argv[0]: %s\n", argv[0]);
-  printf("function: cmd_change() called!\n");
+  char *dbdir;
+  int id;
+  ab_contact_t *contact = NULL;
+
+  if (argc != 5) {
+    fprintf(stderr, "usage: %s <id> <first-name> <last-name> <e-mail>\n", argv[0]);
+    return 1;
+  }
+
+  dirs_init();
+
+  if (!ensure_data_dir()) {
+    return 1;
+  }
+
+  dbdir = dirs_get_user_data_dir();
+
+  ab_init(dbdir);
+
+  ab_load_contacts();
+
+  id = atoi(argv[1]);
+
+  contact = ab_get_contact_by_id(id);
+  if (!contact) {
+    fprintf(stderr, "%s: contact (%s) does not exist in our database.\n",
+            argv[0], argv[1]);
+    return 1;
+  }
+
+  ab_contact_set_first_name(contact, argv[2]);
+  ab_contact_set_last_name(contact, argv[3]);
+  ab_contact_set_email(contact, argv[4]);
+
+  if (ab_update_contact(contact)) {
+    return 1;
+  }
+
+  ab_fini();
 
   return 0;
 }
