b769d75e330984546e5fc3bc459a140163bd3fab
Thiago Farina <tfransosi@gmail.com>
2023-01-07 14:59:30 -0500

    fstrdup: rename 'src' parameter to 'str'
    Since there is no 'dst' parameter, it makes more sense to have it just 'str'
    rather than 'src'.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/fstrdup.c
+++ c/apps/fstrdup.c
@@ -6,28 +6,28 @@
 /**
  * Duplicates a string.
  *
- * @param[in] src String to be duplicated
+ * @param[in] str String to be duplicated
  * @return A pointer to the duplicated string or `NULL` if no memory was
  * available.
  *
  * @remark
  *    The returned string should be freed when no longer needed.
  */
-char *f_strdup(char const *src) {
+char *f_strdup(char const *str) {
   size_t len;
   char *dst;
 
-  if (src == 0) {
+  if (str == 0) {
     return (char *) 0;
   }
 
-  len = strlen(src) + 1;
+  len = strlen(str) + 1;
   dst = malloc(sizeof(char) * len);
   if (dst == 0) {
     return (char *) 0;
   }
 
-  strcpy(dst, src);
+  strcpy(dst, str);
 
   return dst;
 }
