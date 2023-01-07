c2cd9bbefe4b9376d5817ff167af6e00c8defc79
Thiago Farina <tfransosi@gmail.com>
2023-01-07 15:27:02 -0500

    c: introduce 'xstring.[ch]' module to replace 'fstrdup'
    In other to add other 'xstrfoo' functions, let's introduce
    a new module to serve the home for these functions.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/strdup.c
+++ c/apps/strdup.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "fstrdup.h"
+#include "xstring.h"
 
 int main(int argc, char **argv) {
   const char *src = "hello, world";
   char *dst;
 
-  dst = f_strdup(src);
+  dst = xstrdup(src);
 
   printf("%s\n", dst);
 
