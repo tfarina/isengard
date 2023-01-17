b5795d93ea2587a68c22fc7950fbe3bb175e6de1
Thiago Farina <tfransosi@gmail.com>
2023-01-17 10:59:11 -0500

    c: sync other copies of 'os_path.[ch]' with the master copy
    That will allow the other users to use 'os_path_join' function.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/os_path.h
+++ c/apps/network/echod/src/os_path.h
@@ -2,11 +2,20 @@
 #define _OS_PATH_H_INCLUDED_ 1
 
 /**
- * Returns the last element of path.
+ * Returns a pointer to the last element of the path.
  *
- * @param path The path to get the last element of
- * @return The last element of the path
+ * @param[in] path A pointer to a string that contains the path to search.
+ * @return Returns a pointer to the last element of the path if successful.
  */
 char const *os_path_basename(char const *path);
+
+/**
+ * Joins directory with file name into a single path, separating them with an
+ * Linux specific separator.
+ *
+ * @param[in] dir A pointer to a string that contains the directory path.
+ * @param[in] file A pointer to a string that contains the file name.
+ */
+char *os_path_join(char *dir, char *file);
 
 #endif  /* !defined(_OS_PATH_H_INCLUDED_) */
--- c/apps/sab/src/os_path.c
+++ c/apps/sab/src/os_path.c
@@ -1,5 +1,6 @@
 #include "os_path.h"
 
+#include <stdlib.h>
 #include <string.h>
 
 #define DIR_SEP '/'
@@ -9,9 +10,34 @@ char const *os_path_basename(char const *path) {
 
   /* Search for the last slash in path. */
   last_slash = strrchr(path, DIR_SEP);
+  /* If there is no DIR_SEP in path, the basename is path. */
   if (last_slash == 0) {
     return path;
   }
 
-  return last_slash + 1; /* move pointer past the '/' character */
+  return last_slash + 1; /* Move the pointer past the '/'. */
+}
+
+char *
+os_path_join(char *dir, char *file)
+{
+  char *path;
+  size_t len;
+
+  /*
+   * Add 2 bytes to the resulting length to have enough memory available
+   * for the directory separator ('/') and the null byte ('\0').
+   */
+  len = strlen(dir) + strlen(file) + 2;
+
+  path = malloc(sizeof(char) * len);
+  if (path == NULL) {
+    return NULL;
+  }
+
+  strcpy(path, dir);
+  strcat(path, "/");
+  strcat(path, file);
+
+  return path;
 }
