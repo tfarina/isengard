6f2ecbe77dc2204fb5d719a32b2ac1213df2d32c
Thiago Farina <tfransosi@gmail.com>
2023-01-19 12:14:27 -0500

    sab: migrate 'dirs' to 'user' module
    Use 'user' module functions, which replace the old, 'futils' module.
    
    Later, that change will allow us to replace 'ffileutils' module as well,
    since we can now use 'os_path_join' function from 'os_path' module.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/sab/src/user.c
@@ -0,0 +1,64 @@
+#include "user.h"
+
+#include <pwd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "os_path.h"
+
+/**
+ * Returns the current user's home directory.
+ */
+char *
+user_home_dir(void)
+{
+  char *home;
+  struct passwd *pw;
+
+  home = getenv("HOME");
+
+  if ((home == NULL) || (*home == '\0')) {
+    /*
+     * Try to get user's home directory from the password file (/etc/passwd).
+     */
+    pw = getpwuid(getuid());
+    if (pw != NULL && pw->pw_dir != NULL && *pw->pw_dir != '\0') {
+      home = pw->pw_dir;
+    } else {
+      home = "/";
+    }
+  }
+
+  return home;
+}
+
+/**
+ * Returns the base directory for user-specific data files.
+ */
+char *
+user_data_dir(void)
+{
+  char *homedir;
+  char *datadir;
+
+  homedir = user_home_dir();
+  datadir = os_path_join(homedir, ".local/share");
+
+  return datadir;
+}
+
+/**
+ * Returns the base directory for user-specific configuration files.
+ */
+char *
+user_config_dir(void)
+{
+  char *homedir;
+  char *configdir;
+
+  homedir = user_home_dir();
+  configdir = os_path_join(homedir, ".config");
+
+  return configdir;
+}
