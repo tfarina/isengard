e26c8d8b143a58c2af43b4c16ea5528525ece6d9
Thiago Farina <tfransosi@gmail.com>
2023-03-21 15:35:57 -0400

    clock.c: add a timeout
    That way every one 1 second or 1000 ms, the time is updated.
    
    https://zetcode.com/gui/gtk2/gtkevents
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/gui/gtk/clock.c
+++ c/apps/gui/gtk/clock.c
@@ -3,11 +3,8 @@
 
 #define TIMEFORMAT "%I:%M:%S %p"
 
-int main(int argc, char **argv)
+gboolean time_handler(GtkWidget *widget)
 {
-  GtkWidget *window;
-  GtkWidget *label;
-
   time_t now;
   struct tm *curtime;
   char timebuf[32];
@@ -17,6 +14,16 @@ int main(int argc, char **argv)
 
   strftime(timebuf, sizeof(timebuf), TIMEFORMAT, curtime);
 
+  gtk_label_set_text(GTK_LABEL(widget), timebuf);
+
+  return TRUE;
+}
+
+int main(int argc, char **argv)
+{
+  GtkWidget *window;
+  GtkWidget *label;
+
   gtk_init(&argc, &argv);
 
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -24,14 +31,18 @@ int main(int argc, char **argv)
 
   gtk_container_set_border_width(GTK_CONTAINER(window), 15);
 
-  label = gtk_label_new(timebuf);
+  label = gtk_label_new("");
 
   gtk_container_add(GTK_CONTAINER(window), label);
 
   g_signal_connect(window, "destroy",
 		   G_CALLBACK(gtk_main_quit), NULL);
 
+  g_timeout_add(1000, (GSourceFunc) time_handler, (gpointer) label);
+
   gtk_widget_show_all(window);
+
+  time_handler(label);
 
   gtk_main();
 
