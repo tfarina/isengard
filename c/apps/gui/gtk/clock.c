0d55e0b9e89b7b3c43899d1b1ed1ca8e938fdf68
Thiago Farina <tfransosi@gmail.com>
2023-03-24 13:31:22 -0400

    clock.c: add a frame around the clock
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/gui/gtk/clock.c
+++ c/apps/gui/gtk/clock.c
@@ -22,6 +22,8 @@ gboolean time_handler(GtkWidget *widget)
 int main(int argc, char **argv)
 {
   GtkWidget *window;
+  GtkWidget *table;
+  GtkWidget *timeframe;
   GtkWidget *label;
 
   gtk_init(&argc, &argv);
@@ -33,9 +35,19 @@ int main(int argc, char **argv)
 
   gtk_container_set_border_width(GTK_CONTAINER(window), 15);
 
+  table = gtk_table_new(1, 1, TRUE);
+  gtk_table_set_row_spacings(GTK_TABLE(table), 10);
+  gtk_table_set_col_spacings(GTK_TABLE(table), 10);
+  gtk_container_add(GTK_CONTAINER(window), table);
+
+  timeframe = gtk_frame_new("Time");
+  gtk_frame_set_shadow_type(GTK_FRAME(timeframe), GTK_SHADOW_ETCHED_IN);
+
+  gtk_table_attach_defaults(GTK_TABLE(table), timeframe, 0, 1, 0, 1);
+
   label = gtk_label_new("");
 
-  gtk_container_add(GTK_CONTAINER(window), label);
+  gtk_container_add(GTK_CONTAINER(timeframe), label);
 
   g_signal_connect(window, "destroy",
 		   G_CALLBACK(gtk_main_quit), NULL);
