835562d8730d94904d102989c8f0bef2e86672d5
Thiago Farina <tfransosi@gmail.com>
2023-05-10 10:54:56 -0400

    bit.c: write 'initial_bits' in hexadecimal
    Avoids a gcc warning that says "binary constants are a GCC extension".
    
    https://www.pngkey.com/png/detail/103-1037729_ascii-table-black-ascii-decimal-hex-table.png
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/bit.c
+++ c/apps/bit.c
@@ -4,7 +4,7 @@
 #define BITMASK(index) (1 << (index))
 
 int main(void) {
-  const uint8_t initial_bits = 0b00001111;
+  const uint8_t initial_bits = 0x0F; /* 0000 1111 */
   const uint8_t inverted_bits = ~initial_bits; /* equals to 0b11110000 */
 
   const uint8_t first_six_bits = 0b11111100;
