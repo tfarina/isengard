1e42b401df875394c1e8176c6cac5a5293f5fd8d
Thiago Farina <tfransosi@gmail.com>
2023-05-10 14:10:56 -0400

    bit.c: write decimal values in comment
    And use 'flagxxx' as variable names.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/bit.c
+++ c/apps/bit.c
@@ -4,24 +4,24 @@
 #define BITMASK(index) (1 << (index))
 
 int main(void) {
-  /* Power of two values */
-  const uint8_t one                   = 0x01; /* 0b0000 0001 */
-  const uint8_t two                   = 0x02; /* 0b0000 0010 */
-  const uint8_t four                  = 0x04; /* 0b0000 0100 */
-  const uint8_t eight                 = 0x08; /* 0b0000 1000 */
-  const uint8_t sixteen               = 0x10; /* 0b0001 0000 */
-  const uint8_t thirtytwo             = 0x20; /* 0b0010 0000 */
-  const uint8_t sixtyfour             = 0x40; /* 0b0100 0000 */
-  const uint8_t onehundredtwentyeight = 0x80; /* 0b1000 0000 */
+  /* Power of two values                 Binary    | Decimal */
+  const uint8_t flag1       = 0x01; /* 0b0000 0001 | 1       */
+  const uint8_t flag2       = 0x02; /* 0b0000 0010 | 2       */
+  const uint8_t flag3       = 0x04; /* 0b0000 0100 | 4       */
+  const uint8_t flag4       = 0x08; /* 0b0000 1000 | 8       */
+  const uint8_t flag5       = 0x10; /* 0b0001 0000 | 16      */
+  const uint8_t flag6       = 0x20; /* 0b0010 0000 | 32      */
+  const uint8_t flag7       = 0x40; /* 0b0100 0000 | 64      */
+  const uint8_t flag8       = 0x80; /* 0b1000 0000 | 128     */
 
-  printf("%d\n", one);
-  printf("%d\n", two);
-  printf("%d\n", four);
-  printf("%d\n", eight);
-  printf("%d\n", sixteen);
-  printf("%d\n", thirtytwo);
-  printf("%d\n", sixtyfour);
-  printf("%d\n", onehundredtwentyeight);
+  printf("%d\n", flag1);
+  printf("%d\n", flag2);
+  printf("%d\n", flag3);
+  printf("%d\n", flag4);
+  printf("%d\n", flag5);
+  printf("%d\n", flag6);
+  printf("%d\n", flag7);
+  printf("%d\n", flag8);
 
   /*
    * left-shift operator (<<)
