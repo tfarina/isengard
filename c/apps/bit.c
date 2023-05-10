9fa5d47cf6390f6cbd84db755a35230c25fbe3af
Thiago Farina <tfransosi@gmail.com>
2023-05-10 12:55:38 -0400

    bit.c: silence the rest of gcc warnings
    I will come up with better ways to show the usage of
    bitwise inclusive OR operator (|), bitwise AND operator (&),
    bitwise exclusive OR operator (^) and one's complement operator (~).
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/bit.c
+++ c/apps/bit.c
@@ -4,17 +4,6 @@
 #define BITMASK(index) (1 << (index))
 
 int main(void) {
-  const uint8_t initial_bits = 0x0F; /* 0000 1111 */
-  const uint8_t inverted_bits = ~initial_bits; /* equals to 0b11110000 */
-
-  const uint8_t first_six_bits = 0b11111100;
-  const uint8_t last_six_bits  = 0b00111111;
-  const uint8_t middle_four_bits = first_six_bits & last_six_bits; /* equals to 0b00111100 */
-
-  const uint8_t some_bits = 0b10110010;
-  const uint8_t more_bits = 0b01011110;
-  const uint8_t combined_bits = some_bits | more_bits; /* equals to 0b11111110 */
-
   /* Powers of 2 in binary */
   const uint8_t zero                  = 0x00; /* 0b0000 0000 */
   const uint8_t one                   = 0x01; /* 0b0000 0001 */
