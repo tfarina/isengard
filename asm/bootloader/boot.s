fe82ce1be99fa463e6288230549a8439b78d1ff1
Thiago Farina <tfransosi@gmail.com>
2023-08-31 11:32:08 -0400

    asm/bootloader: indent assembly code with tab in emacs
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- asm/bootloader/boot.s
+++ asm/bootloader/boot.s
@@ -10,6 +10,7 @@
 
 _start: 			# code entry point
 	jmp _start
+
 	.fill 510-(.-_start), 1, 0
-        .byte 0x55		# append boot signature
-        .byte 0xaa 		# append boot signature
+	.byte 0x55		# append boot signature
+	.byte 0xaa 		# append boot signature
