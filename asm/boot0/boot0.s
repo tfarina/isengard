a245447f96efb647f4d6fc71818d6004923999b3
Thiago Farina <tfransosi@gmail.com>
2023-08-31 00:39:48 -0400

    asm/boo0: move comments in boot0.s to column 32
    Starting comments at column 32 make the code more readable.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- asm/boot0/boot0.s
+++ asm/boot0/boot0.s
@@ -1,10 +1,15 @@
+#
 # Code from:
 # www.codeproject.com/Articles/664165/Writing-a-boot-loader-in-Assembly-and-C-Part
-.code16 # generate 16-bit code
-.text   # executable code location
+#
+
+.code16 			# generate 16-bit code
+
+.text   			# executable code location
         .globl _start;
-_start: # code entry point
+
+_start: 			# code entry point
 	jmp _start
 	.fill 510-(.-_start), 1, 0
-        .byte 0x55 # append boot signature
-        .byte 0xaa # append boot signature
+        .byte 0x55		# append boot signature
+        .byte 0xaa 		# append boot signature
