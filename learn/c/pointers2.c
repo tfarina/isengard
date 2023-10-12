b2c4d658e11babcfd18ff167ee403b13c694da0c
Thiago Farina <tfransosi@gmail.com>
2023-10-12 05:52:38 -0400

    pointers2.c: clean up the messages displayed to be more readable
    Also, remove the unnecessary casts (void *).
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- learn/c/pointers2.c
+++ learn/c/pointers2.c
@@ -6,10 +6,10 @@ int main(int argc, char **argv) {
   char ch = 'a';
   char *cptr = &ch;
 
-  printf("The memory address of 'ch' is -> %p\n", (void *)&ch);
-  printf("The value of the pointer 'cptr' is -> %p\n", (void *)cptr);
-  printf("The memory address of the pointer 'cptr' is -> %p\n", (void *)&cptr);
-  printf("The value of 'ch' variable is -> %c\n", *cptr);
+  printf("The memory address of ch is: %p\n", &ch);
+  printf("The value of the pointer cptr is: %p\n", cptr);
+  printf("The memory address of the pointer cptr is: %p\n", &cptr);
+  printf("The value of ch is: %c\n", *cptr);
 
   return 0;
 }
