d5ae28b1a15a764237c710f456ce17c42f2210f3
Thiago Farina <tfransosi@gmail.com>
2023-10-17 12:05:48 -0400

    learn/c: show another facet of indirection operator (*)
    When used on a pointer to pointer, it returns the address stored on that
    pointer to a pod variable.
    
    Demonstrated at:
    https://learn.microsoft.com/en-us/cpp/cpp/indirection-operator-star?view=msvc-170
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- learn/c/indirectionoperator.c
+++ learn/c/indirectionoperator.c
@@ -13,6 +13,7 @@ int main(void)
 
   int x = 10;
   int *p = &x;
+  int **pp = &p;
 
   printf("The direct value of x: %d\n", x);
 
@@ -25,4 +26,16 @@ int main(void)
    * type that the operand addresses.
    */
   printf("The indirect value of x: %d\n", *p);
+
+  /*
+   * If we use the unary indirection operator (*) on a pointer to pointer
+   * variable, that is it, if we dereference a pointer to pointer or a double
+   * pointer, what do we get? We get the VALUE of the pointer we are pointing
+   * to, and that is the address of x variable!
+   * In both cases it returns the value, when used on a pointer it returns the
+   * value of POD (Plain Old Data), but when used on a pointer to pointer it
+   * returns the ADDRESS!
+   */
+  printf("The address of x: %p\n", p);
+  printf("The address of x via indirection: %p\n", *pp);
 }
