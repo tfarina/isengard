884a01576a58ca5907dcfc59c384e3a5cfb35200
Thiago Farina <tfransosi@gmail.com>
2023-10-21 05:04:17 -0400

    learn/c: add an example of the postfix subscript operator []
    Which shows how array subscripting or indexing works.
    
    Refs:
    https://learn.microsoft.com/en-us/cpp/cpp/subscript-operator?view=msvc-170
    https://www.open-std.org/jtc1/sc22/wg14/www/docs/n2310.pdf
    http://port70.net/~nsz/c/c89/c89-draft.html
    https://web.archive.org/web/20200909074736if_/https://www.pdf-archive.com/2014/10/02/ansi-iso-9899-1990-1/ansi-iso-9899-1990-1.pdf
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ learn/c/subscriptoperator.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(void)
+{
+  /*
+   * Ref:
+   *
+   * Subscript Operator []
+   * https://learn.microsoft.com/en-us/cpp/cpp/subscript-operator?view=msvc-170
+
+   * 6.5.2.1 Array subscripting
+   *
+   * Constraints
+   * One of the expressions shall have type "pointer to complete object type",
+   * the other expression shall have integer type, and the result has type
+   * "type".
+   *
+   * Semantics
+   * A postfix expression followed by an expression in square brackets[]is a
+   * subscripted designation ofan element of an array object. The definition
+   * of the subscript operator[] is that E1[E2] is identical to(*((E1)+(E2))).
+   * Because of the conversion rules that apply to the binary+ operator, if E1
+   * is an array object (equivalently, a pointer to the initial element of an
+   * array object) and E2 is an integer, E1[E2] designates the E2-th element
+   * of E1 (counting from zero).
+   *
+   * https://www.open-std.org/jtc1/sc22/wg14/www/docs/n2310.pdf
+   */
+
+  int arr[5] = {1, 2, 3, 4, 5};
+
+  /*
+   * The next two examples shows that the expression arr[2] is identical
+   * to 2[arr]. The reason for that comes from the subscript operator []
+   * definition.
+   */
+  printf("%d\n", arr[2]);
+
+  printf("%d\n", 2[arr]);
+
+  /*
+   * The definition of the subscript operator [] is that E1[E2] (and by E1 we
+   * mean Expression 1 and E2 Expression 2) is identical to (*((E1) + (E2))).
+   *
+   * https://www.open-std.org/jtc1/sc22/wg14/www/docs/n2310.pdf
+   *
+   * The following demonstrates that.
+   */
+  printf("%d\n", *(arr + 2));
+
+  printf("%d\n", *(2 + arr));
+
+  return 0;
+}
