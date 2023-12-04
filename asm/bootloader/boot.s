e57abde3640e54ecaa686ac7027cdd8716babb73
Thiago Farina <tfransosi@gmail.com>
2023-12-04 12:22:00 +0000

    bootloader: use BIOS to print a single character
    We can make use of BIOS functions to print a character on screen.
    These functions are available to us via the so-called interrupts.
    
    From Ralf Brown's interrupt list we can find that the video interrupt is 0x10.
    
    The function Teletype sounds like a good match, it prints a character given in AL
    and automatically advances the cursor.
    
    Then, we can select that function by setting AH to 0x0e, put the ASCII code we want
    to print into AL and the call the interrupt 0x10 to actually print it.
    
    Cool.
    
    Ref:
    https://medium.com/@g33konaut/writing-an-x86-hello-world-boot-loader-with-assembly-3e4c5bdd96cf
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- asm/bootloader/boot.s
+++ asm/bootloader/boot.s
@@ -1,6 +1,7 @@
 #
 # Code from:
 # www.codeproject.com/Articles/664165/Writing-a-boot-loader-in-Assembly-and-C-Part
+# https://medium.com/@g33konaut/writing-an-x86-hello-world-boot-loader-with-assembly-3e4c5bdd96cf
 #
 
 .code16 			# generate 16-bit code
@@ -9,7 +10,9 @@
         .globl _start;
 
 _start: 			# code entry point
-	jmp _start
+	mov $0x0e41, %ax	# set AH to 0x0e (function Teletype) and AL to 0x41 (ASCII for "A")
+	int $0x10		# call the function in AH from interrupt 0x10
+	hlt			# stop executing
 
 	.fill 510-(.-_start), 1, 0
 	.word 0xaa55		# append bootloader signature
