e4135900bcf9e657726c1f3c776de8793bcf5c5a
Thiago Farina <tfransosi@gmail.com>
2023-12-04 15:03:59 +0000

    bootloader: document the reason we write 0xaa55 instead of 0x55aa in x86
    It is because the machine is little endian and thus bytes get swapped in memory.
    
    Ref:
    https://medium.com/@g33konaut/writing-an-x86-hello-world-boot-loader-with-assembly-3e4c5bdd96cf
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- asm/bootloader/boot.s
+++ asm/bootloader/boot.s
@@ -3,6 +3,13 @@
 # www.codeproject.com/Articles/664165/Writing-a-boot-loader-in-Assembly-and-C-Part
 # https://medium.com/@g33konaut/writing-an-x86-hello-world-boot-loader-with-assembly-3e4c5bdd96cf
 #
+# If the bootloader signature bytes are 0x55aa, why are we swapping them down
+# below? That is because x86 (the machine I'm coding this on) is little endian,
+# so bytes get swapped in memory, and thus we have to invert and write them as
+# 0xaa55. Otherwise, qemu gives the error "Boot failed: not a bootable disk".
+# https://ftp.gnu.org/old-gnu/Manuals/gas-2.9.1/html_chapter/as_7.html
+# https://github.com/rofl0r/endianness.h/blob/master/endianness.h
+#
 
 .code16 			# generate 16-bit code
 
