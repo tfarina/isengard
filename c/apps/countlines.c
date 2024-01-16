4b47f0b4daf9dd493bbebc87dc525840b8761c0d
Thiago Farina <tfransosi@gmail.com>
2024-01-16 10:32:18 +0000

    c: add a small program to count the number of lines in a file
    I built this into a small separate program, to be able to test the
    following code more easily and freely:
    
    <code>
    
     ch = fgetc(fp);
     while (ch != EOF)
     {
         if (ch == '\n')
             numLines++;
    
         ch = fgetc(fp);
     }
    
    </code>
    
    That part of the code is the one I'm interested, because I'm going to
    use it in the numbers program to build an array of that size, that is it,
    the size of the number of lines in the file, so the size is not hardcoded.
    
    To test it, is easy, just create a text file, put some numbers or text one per line,
    then run `countlines numbers.txt` for example.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/countlines.c
@@ -0,0 +1,40 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "xstring.h"
+
+int main(int argc, char **argv)
+{
+  char *filename;
+  FILE *fp;
+  int ch;
+  int numLines = 0;
+
+  if (argc != 2) {
+    fputs("usage: countlines FILE\n", stderr);
+    return 1;
+  }
+
+  filename = xstrdup(argv[1]);
+
+  fp = fopen(filename, "r");
+  if (fp == NULL)
+  {
+    fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+    return -1;
+  }
+
+  ch = fgetc(fp);
+  while (ch != EOF)
+  {
+    if (ch == '\n')
+      numLines++;
+
+    ch = fgetc(fp);
+  }
+
+  printf("There are %d lines in %s file.\n", numLines, filename);
+
+  return 0;
+}
