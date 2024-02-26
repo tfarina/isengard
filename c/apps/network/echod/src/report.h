4433b73f93bee00fc8637c29276e1fe55856965f
Thiago Farina <tfransosi@gmail.com>
2024-02-26 17:31:31 +0000

    echod: add doxygen '@file' and '@brief' commands to report.h
    According to the doxygen documentation to document global objects (functions,
    typedefs, enums, macros, etc) you must document the file in which they are
    defined.
    
    Refs:
    https://www.doxygen.nl/manual/docblocks.html#specialblock
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/report.h
+++ c/apps/network/echod/src/report.h
@@ -1,6 +1,11 @@
 #ifndef _REPORT_H_INCLUDED_
 #define _REPORT_H_INCLUDED_ 1
 
+/**
+ * @file report.h
+ * @brief Output diagnose messages
+ */
+
 void fatal_exit(int const code, char const *format, ...);
 
 #endif  /* !defined(_REPORT_H_INCLUDED_) */
