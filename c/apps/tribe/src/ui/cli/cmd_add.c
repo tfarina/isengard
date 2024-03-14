6f2d5ad817c4fa3fecb75fb5155ae7b9cb78a995
Thiago Farina <tfransosi@gmail.com>
2024-03-14 20:56:32 +0000

    tribe: add duplicate comment to cmd_add.c
    I added this comment to the standalone version of 'add' cmd
    in commit 1acff254 ("umgr: change 'user_add' function to take
    a pointer to user_t structure").
    
    Let's put it in the built-in command to keep the two versions in sync.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/tribe/src/ui/cli/cmd_add.c
+++ c/apps/tribe/src/ui/cli/cmd_add.c
@@ -38,6 +38,7 @@ int cmd_add(int argc, char **argv) {
   ab_contact_set_last_name(contact, argv[2]);
   ab_contact_set_email(contact, argv[3]);
 
+  /* It's okay to add duplicate records. */
   rc = ab_add_contact(contact);
   if (rc < 0) {
     fputs("Failed to create a new contact.\n", stderr);
