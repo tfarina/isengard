tfarina/isengard

commit c473dcf51a95a345c8ef790455d24d44eae77f26

tribe: check the length of entry string
Before even thinking of duplicating the string, check the length to see if it
is not empty.

The benefit of doing this is that it will avoid duplicating empty strings
unnecessarily. Which is good and save memory space.

Signed-off-by: Thiago Farina <tfransosi@gmail.com>

c/apps/tribe/src/ui/gtk/contact_editor.c
@@ -3,6 +3,8 @@
 #include <gdk/gdk.h>
 #include <gdk/gdkkeysyms.h>
 
+#include <string.h>
+
 static action_code_t action_code;
 static ab_contact_t *current_contact;
 typedef void (*editor_post_cb_t)(ab_contact_t *contact);
@@ -18,6 +20,7 @@ static GtkWidget *email_entry;
 
 static void _contact_editor_ok_cb(GtkWidget *widget, gboolean *cancelled)
 {
+  char const *entry_text;
   char const *first_name;
   char const *last_name;
   char const *email;
@@ -26,14 +29,23 @@ static void _contact_editor_ok_cb(GtkWidget *widget, gboolean *cancelled)
     ab_contact_create(&current_contact);
   }
 
-  first_name = g_strdup(gtk_entry_get_text(GTK_ENTRY(fname_entry)));
-  ab_contact_set_first_name(current_contact, first_name);
+  entry_text = gtk_entry_get_text(GTK_ENTRY(fname_entry));
+  if (strlen(entry_text)) {
+    first_name = g_strdup(entry_text);
+    ab_contact_set_first_name(current_contact, first_name);
+  }
 
-  last_name = g_strdup(gtk_entry_get_text(GTK_ENTRY(lname_entry)));
-  ab_contact_set_last_name(current_contact, last_name);
+  entry_text = gtk_entry_get_text(GTK_ENTRY(lname_entry));
+  if (strlen(entry_text)) {
+    last_name = g_strdup(entry_text);
+    ab_contact_set_last_name(current_contact, last_name);
+  }
 
-  email = g_strdup(gtk_entry_get_text(GTK_ENTRY(email_entry)));
-  ab_contact_set_email(current_contact, email);
+  entry_text = gtk_entry_get_text(GTK_ENTRY(email_entry));
+  if (strlen(entry_text)) {
+    email = g_strdup(entry_text);
+    ab_contact_set_email(current_contact, email);
+  }
 
   if (action_code == AC_ADD) {
     ab_add_contact(current_contact);

commit 82758a7296e64f18d1fc9899ff7455bc78c7aa0b

tribe: allow the user to empty some fields
This fixes this bug introduced by commit c473dcf ("tribe: check the length
of entry string").

At that commit I didn't test updating the fields and having them empty. I have
tested this case now and it works as before.

Signed-off-by: Thiago Farina <tfransosi@gmail.com>

c/apps/tribe/src/ui/gtk/contact_editor.c
@@ -30,19 +30,28 @@ static void _contact_editor_ok_cb(GtkWidget *widget, gboolean *cancelled)
   }
 
   entry_text = gtk_entry_get_text(GTK_ENTRY(fname_entry));
-  if (strlen(entry_text)) {
+  if (0 == strlen(entry_text)) {
+    first_name = "";
+    ab_contact_set_first_name(current_contact, first_name);
+  } else {
     first_name = g_strdup(entry_text);
     ab_contact_set_first_name(current_contact, first_name);
   }
 
   entry_text = gtk_entry_get_text(GTK_ENTRY(lname_entry));
-  if (strlen(entry_text)) {
+  if (0 == strlen(entry_text)) {
+    last_name = "";
+    ab_contact_set_last_name(current_contact, last_name);
+  } else {
     last_name = g_strdup(entry_text);
     ab_contact_set_last_name(current_contact, last_name);
   }
 
   entry_text = gtk_entry_get_text(GTK_ENTRY(email_entry));
-  if (strlen(entry_text)) {
+  if (0 == strlen(entry_text)) {
+    email = "";
+    ab_contact_set_email(current_contact, email);
+  } else {
     email = g_strdup(entry_text);
     ab_contact_set_email(current_contact, email);
   }