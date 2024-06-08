2f4e46bd80959595aeb4843634cab66b7354a1c5
Thiago Farina <tfransosi@gmail.com>
2024-06-08 16:43:08 +0100

    tribe: move sqlite3_finalize call into the cleanup area
    This is to unify the code path for deleting the prepared statement.
    By having it under the out label, we make sure it is called from
    any goto statement.
    
    I also confirmed that calling sqlite3_finalize() on a NULL pointer is a harmless no-op.
    It does not crash the application, so checking the statement before calling it is not
    necessary and thus I removed this check as well.
    
    Refs:
    https://www.sqlite.org/c3ref/finalize.html
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/tribe/src/ab.c
+++ c/apps/tribe/src/ab.c
@@ -342,16 +342,13 @@ int _db_insert_contact(ab_contact_t *contact) {
     errcode = -1;
   }
 
+out:
   rc = sqlite3_finalize(insert_stmt);
+  insert_stmt = NULL;
   if (rc != SQLITE_OK) {
     fprintf(stderr, "sqlite3_finalize failed: %s\n",
             sqlite3_errmsg(hdb));
-  }
-  insert_stmt = NULL;
-
-out:
-  if (insert_stmt) {
-    sqlite3_finalize(insert_stmt);
+    errcode = -1;
   }
 
   return errcode;
