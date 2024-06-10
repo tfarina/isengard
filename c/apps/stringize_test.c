4bca9af94871b0f74e9f526e212ca82c291fbab8
Thiago Farina <tfransosi@gmail.com>
2024-06-10 14:39:27 +0100

    c: demonstrate why you need a macro indirection to stringize a macro's value
    This will show that if you apply the preprocessing operator '#' directly to a
    macro, it will just put the macro's name in quotes. To put the macro's value
    in quotes, you to expand it first, then apply the operator '#' on it.
    
    See the example stringize_test.c file to see the results.
    
    I'm adding this example to show how it works, because I will need this macro
    to build a version string out of the version numbers in the tribe project.
    
    Refs:
    https://c-faq.com/ansi/stringize.html
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/stringize_test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "stringize.h"
+
+#define OP plus
+
+#define MAJOR 6
+#define MINOR 9
+
+#define STR_VERSION M_STRINGIZE(MAJOR) "." M_STRINGIZE(MINOR)
+
+int
+main(void)
+{
+  /* This shows why you need a macro indirection to put a macro value
+   * in quotes. Applying the # to the macro, will just put the macro name
+   * in quotes. So, in order to put the value of the macro in quotes,
+   * the macro has to be "expanded" first, then the # has to be applied to
+   * the result to stringize it.
+   * See https://c-faq.com/ansi/stringize.html for more information.#
+   */
+  char *macroname = M_STRINGIZE_(OP);
+  char *opname = M_STRINGIZE(OP);
+
+  printf("%s\n", macroname); /* This will print OP */
+  printf("%s\n", opname);    /* This will print plus */
+
+  printf("%s\n", STR_VERSION);
+
+  return 0;
+}
