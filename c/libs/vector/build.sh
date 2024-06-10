154b18b2278d7707e0c7dc29b40e0fbd60afb7b2
Thiago Farina <tfransosi@gmail.com>
2024-06-10 17:05:23 +0100

    vector: switch back to gcc
    I prefer to use gcc whenever possible. Since I reinstalled my system, I did not
    reinstall clang.
    
    This fixes the following error:
    
    ./build.sh: 5: ./build.sh: clang: not found
    ar: out/vector.o: No such file or directory
    ./build.sh: 9: ./build.sh: clang: not found
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/libs/vector/build.sh
+++ c/libs/vector/build.sh
@@ -2,8 +2,8 @@
 
 mkdir -p out/
 
-clang -c vector.c -o out/vector.o
+gcc -c vector.c -o out/vector.o
 
 rm -rf out/libvector.a && ar rcs out/libvector.a out/vector.o
 
-clang -o out/vector_test vector_test.c -Lout/ -lvector
+gcc -o out/vector_test vector_test.c -Lout/ -lvector
