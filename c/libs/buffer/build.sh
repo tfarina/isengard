a56254ddaa3bf755a7df455ecf489d77e2852b04
Thiago Farina <tfransosi@gmail.com>
2024-06-11 15:21:56 +0100

    buffer: migrate shell scripts to make
    Use make to build the object file, the library archive and the elf executable.
    
    Refs:
    https://www.gnu.org/software/make/
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/libs/buffer/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p out/
-
-gcc -c buffer.c -o out/buffer.o
-
-rm -rf out/libbuffer.a && ar rcs out/libbuffer.a out/buffer.o
-
-gcc -o out/buffer_test buffer_test.c -Lout/ -lbuffer
