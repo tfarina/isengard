2e0d056eb2734eb9b4a4ff247184b1a46a8e67b8
Thiago Farina <tfransosi@gmail.com>
2024-06-20 12:46:02 +0100

    vector: add doc comments to some greatest's macros
    This way it is easier to understand what the code is doing,
    or it is supposed to do when reading years from now.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/libs/vector/tests/run_all_unittests.c
+++ c/libs/vector/tests/run_all_unittests.c
@@ -2,13 +2,17 @@
 
 #include "vector.h"
 
+/* Define a suite, compiled separately.
+ */
 SUITE_EXTERN(vector_suite);
 
+/* Add all the definitions that need to be in the test runner's main file.
+ */
 GREATEST_MAIN_DEFS();
 
 int main(int argc, char **argv)
 {
-  GREATEST_MAIN_BEGIN();      /* init & parse command-line args */
+  GREATEST_MAIN_BEGIN();      /* init and parse command-line arguments */
 
   RUN_SUITE(vector_suite);
 
