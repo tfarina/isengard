e76fada399f41ebd45b147cdfcbc913c5143054a
Thiago Farina <tfransosi@gmail.com>
2024-06-21 14:40:57 +0100

    vector: convert the build and cleanup from shell scripts to Makefile
    This converts the process for building and cleaning up from shell scripts to GNU make tool.
    
    Tested with the following commands:
    
    $ make clean
    $ make
    
    The second make command will build everything and the output is:
    
    gcc -g -c -o vector.o vector.c
    rm -f libvector.a
    ar cru libvector.a vector.o
    ar: `u' modifier ignored since `D' is the default (see `U')
    ranlib libvector.a
    gcc -g -c -o tests/run_all_unittests.o tests/run_all_unittests.c
    gcc -g -c -o tests/vector_test.o tests/vector_test.c
    gcc -g -L. -o vector_unittests tests/run_all_unittests.o tests/vector_test.o -lvector
    
    Refs:
    https://www.gnu.org/software/make/
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/libs/vector/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p out/
-
-gcc -c vector.c -o out/vector.o
-
-rm -rf out/libvector.a && ar rcs out/libvector.a out/vector.o
-
-gcc -o out/vector_test vector_test.c -Lout/ -lvector
