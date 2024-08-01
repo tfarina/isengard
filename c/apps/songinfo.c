0d2b67491c30152aec5646a3cab5fe98c6276b75
Thiago Farina <tfransosi@gmail.com>
2024-08-01 17:24:18 +0100

    c: add a small program to show song info from mp3 files
    I picked id3tag library because it was written in C and
    had a simple API.
    
    id3tag is a ID3 tag reading library from the MAD project.
    It has full support for reading ID3v1, ID3v1.1,  ID3v2.2,
    ID3v2.3, and ID3v2.4 tags, as well as support for writing ID3v1,
    ID3v1.1, and ID3v2.4 tags.
    
    I installed it on my system with the following command:
    
    $ sudo apt install libid3tag0-dev
    
    I gathered its version with:
    
    $ pkg-config --modversion id3tag
    0.15.0b
    
    Refs:
    https://id3.org/Implementations
    https://www.underbit.com/products/mad
    https://sourceforge.net/projects/mad/files/libid3tag/
    https://github.com/audacity/libid3tag/blob/master/id3tag.h
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/songinfo.c
@@ -0,0 +1,65 @@
+#include <id3tag.h>
+
+#include <stdlib.h>
+
+int
+main(int argc, char **argv)
+{
+  struct id3_file *file;
+  struct id3_tag *tag;
+  struct id3_frame *frame;
+  union id3_field *field;
+  id3_ucs4_t const *ucs4;
+  id3_latin1_t *latin1;
+  id3_utf8_t *utf8;
+
+  file = id3_file_open(argv[1], ID3_FILE_MODE_READONLY);
+  if (!file)
+  {
+    return 1;
+  }
+
+  tag = id3_file_tag(file);
+  if (!tag)
+  {
+    return 1;
+  }
+
+  frame = id3_tag_findframe(tag, ID3_FRAME_TITLE, 0);
+  if (!frame)
+  {
+    return 1;
+  }
+
+  field = id3_frame_field(frame, 1);
+  if (!field)
+  {
+    return 1;
+  }
+
+  ucs4 = id3_field_getstrings(field, 0);
+  if (!ucs4)
+  {
+    return 1;
+  }
+
+  latin1 = id3_ucs4_latin1duplicate(ucs4);
+  if (!latin1)
+  {
+    return 1;
+  }
+
+  free(latin1);
+
+  utf8 = id3_ucs4_utf8duplicate(ucs4);
+  if (!utf8)
+  {
+    return 1;
+  }
+
+  free(latin1);
+
+  id3_file_close(file);
+
+  return 0;
+}
