67febbb858316a6d09ae6c1a6aa6e69c39ea75d1
Thiago Farina <tfransosi@gmail.com>
2024-08-03 11:42:56 +0100

    c: add a small program to play a song from a mp3 file
    It shows how to use miniaudio library to decode a mp3 file (using
    the builtin mp3 decoder) and play the sound of the song.
    
    Tested with the following command line:
    
    $ ./songplay ~/Music/sample.mp3
    
    Refs:
    https://miniaud.io/docs/manual/index.html
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/songplay.c
@@ -0,0 +1,71 @@
+#define MINIAUDIO_IMPLEMENTATION
+#include "third_party/miniaudio/miniaudio.h"
+
+#include <stdio.h>
+
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
+{
+  ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
+  if (NULL == pDecoder)
+  {
+    return;
+  }
+
+  ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount, NULL);
+
+  (void)pInput;
+}
+
+int
+main(int argc, char **argv)
+{
+  ma_result result;
+  ma_decoder decoder;
+  ma_device_config deviceConfig;
+  ma_device device;
+
+  if (argc < 2)
+  {
+    fputs("No input file.\n", stderr);
+    return -1;
+  }
+
+  result = ma_decoder_init_file(argv[1], NULL, &decoder);
+  if (MA_SUCCESS != result)
+  {
+    fprintf(stderr, "Could not load file: %s\n", argv[1]);
+    return -2;
+  }
+
+  deviceConfig = ma_device_config_init(ma_device_type_playback);
+  deviceConfig.playback.format = decoder.outputFormat;
+  deviceConfig.playback.channels = decoder.outputChannels;
+  deviceConfig.sampleRate = decoder.outputSampleRate;
+  deviceConfig.dataCallback = data_callback;
+  deviceConfig.pUserData = &decoder;
+
+  result = ma_device_init(NULL, &deviceConfig, &device);
+  if (MA_SUCCESS != result)
+  {
+    fputs("Failed to open playback device.\n", stderr);
+    ma_decoder_uninit(&decoder);
+    return -3;
+  }
+
+  result = ma_device_start(&device);
+  if (MA_SUCCESS != result)
+  {
+    fputs("Failed to start playback device.\n", stderr);
+    ma_device_uninit(&device);
+    ma_decoder_uninit(&decoder);
+    return -4;
+  }
+
+  fputs("Press Enter to quit...", stdout);
+  getchar();
+
+  ma_device_uninit(&device);
+  ma_decoder_uninit(&decoder);
+
+  return 0;
+}
