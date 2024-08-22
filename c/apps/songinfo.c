48da2a4fcb590a95625d735b00464519fc121965
Thiago Farina <tfransosi@gmail.com>
2024-08-22 10:57:23 +0100

    songinfo.c: iterate through frame's fields
    A frame might have more than one field. In our
    test, with the mp3 file we had and tested, the
    title frame had to fields. The first one, at
    index 0 was ID3_FIELD_TYPE_TEXTENCODING, and the
    second at index 1 was ID3_FIELD_TYPE_STRINGLIST.
    
    We are not handling the field_type properly yet,
    so the following warnings are being issued:
    
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_TEXTENCODING’ not handled in switch [-Wswitch]
         switch (field_type)
              ^
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_LATIN1’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_LATIN1FULL’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_LATIN1LIST’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_STRING’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_STRINGFULL’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_STRINGLIST’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_LANGUAGE’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_FRAMEID’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_DATE’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_INT8’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_INT16’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_INT24’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_INT32’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_INT32PLUS’ not handled in switch [-Wswitch]
    ../songinfo.c:50:5: warning: enumeration value ‘ID3_FIELD_TYPE_BINARYDATA’ not handled in switch [-Wswitch]
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/songinfo.c
+++ c/apps/songinfo.c
@@ -11,7 +11,7 @@ main(int argc, char **argv)
   struct id3_frame *frame;
   union id3_field *field;
   enum id3_field_type field_type;
-  unsigned int nstrings, i;
+  unsigned int nstrings, i, j;
   id3_ucs4_t const *ucs4;
   id3_latin1_t *latin1;
   id3_utf8_t *utf8;
@@ -37,50 +37,57 @@ main(int argc, char **argv)
     goto out;
   }
 
-  field = id3_frame_field(frame, 1);
-  if (!field)
+  for (i = 0; i < frame->nfields; i++)
   {
-    errcode = 1;
-    goto out;
-  }
-
-  field_type = id3_field_type(field);
-
-  if (ID3_FIELD_TYPE_STRINGLIST == field_type)
-  {
-    nstrings = id3_field_getnstrings(field);
-    for (i = 0; i < nstrings; i++)
+    field = id3_frame_field(frame, i);
+    if (!field)
     {
-      ucs4 = id3_field_getstrings(field, i);
-      if (ucs4)
+      errcode = 1;
+      goto out;
+    }
+
+    field_type = id3_field_type(field);
+    switch (field_type)
+    {
+
+    }
+
+    if (ID3_FIELD_TYPE_STRINGLIST == field_type)
+    {
+      nstrings = id3_field_getnstrings(field);
+      for (j = 0; j < nstrings; j++)
       {
-        break;
+        ucs4 = id3_field_getstrings(field, j);
+        if (ucs4)
+        {
+          break;
+        }
       }
+
+      if (!ucs4)
+      {
+        errcode = 1;
+        goto out;
+      }
+
+      latin1 = id3_ucs4_latin1duplicate(ucs4);
+      if (!latin1)
+      {
+        errcode = 1;
+        goto out;
+      }
+
+      free(latin1);
+
+      utf8 = id3_ucs4_utf8duplicate(ucs4);
+      if (!utf8)
+      {
+        errcode = 1;
+        goto out;
+      }
+
+      free(utf8);
     }
-
-    if (!ucs4)
-    {
-      errcode = 1;
-      goto out;
-    }
-
-    latin1 = id3_ucs4_latin1duplicate(ucs4);
-    if (!latin1)
-    {
-      errcode = 1;
-      goto out;
-    }
-
-    free(latin1);
-
-    utf8 = id3_ucs4_utf8duplicate(ucs4);
-    if (!utf8)
-    {
-      errcode = 1;
-      goto out;
-    }
-
-    free(utf8);
   }
 
 out:
