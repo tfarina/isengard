77b76dca7c8bc0e22ff15c4e6c6209b7342f89f2
Thiago Farina <tfransosi@gmail.com>
2024-08-26 13:54:39 +0100

    c: add a small program to read a png image
    This code was mostly copied from https://github.com/ShadowsocksR-Live/zbar/blob/master/examples/scan_image.c.
    
    I want to read bar code and qr code images with it. That is the main goal.
    
    It does not do much at the moment. For now, it just knows how to
    get the width and height of the image.
    
    I got the version of the png library I have installed on my system with
    the following command:
    
    $ pkg-config --modversion libpng
    1.2.54
    
    $ pkg-config --cflags --libs libpng
    -I/usr/include/libpng12 -lpng12
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/apps/scanimg.c
@@ -0,0 +1,67 @@
+#include <png.h>
+#include <stdlib.h>
+
+/* https://github.com/ShadowsocksR-Live/zbar/blob/master/examples/scan_image.c
+ */
+
+int
+main(
+  int argc,
+  char **argv)
+{
+  FILE *fp = NULL;
+  png_structp png = NULL;
+  png_infop info = NULL;
+  png_bytep *rows = NULL;
+  int width = 0;
+  int height = 0;
+  int y = 0;
+
+  if (argc < 2)
+  {
+    fprintf(stderr, "Usage: scanimg <filename.png>\n");
+    return 1;
+  }
+
+  fp = fopen(argv[1], "rb");
+  if (!fp)
+    goto out;
+
+  png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  if (!png)
+    goto out;
+
+  if (setjmp(png_jmpbuf(png)))
+    goto out;
+
+  info = png_create_info_struct(png);
+  if (!info)
+    goto out;
+
+  png_init_io(png, fp);
+  png_read_info(png, info);
+
+  width = png_get_image_width(png, info);
+  height = png_get_image_height(png, info);
+
+  rows = malloc(height * sizeof(png_bytep));
+  if (!rows)
+  {
+    /* Out of memory */
+    goto out;
+  }
+
+  for (y = 0; y < height; y++)
+  {
+  }
+
+  return 0;
+
+out:
+  png_destroy_read_struct(&png, &info, NULL);
+
+  if (fp)
+    fclose(fp);
+
+  return 1;
+}
