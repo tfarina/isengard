2ae42f32c333f9582f0df1dbc1d1cc750b566f6b
Thiago Farina <tfransosi@gmail.com>
2024-09-23 15:06:58 +0100

    tribe: delete contact using ab_delete_contact_v2 function
    ab_delete_contact function is just taking the pointer to contact and passing
    the id to the other function. Now that we don't delete and don't need
    to delete the contact from the linked list, we can just use
    ab_delete_contact_v2 function.
    
    Tested running tribe and deleting a few entries. Everything worked fine.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/tribe/src/ui/gtk/window.c
+++ c/apps/tribe/src/ui/gtk/window.c
@@ -390,7 +390,7 @@ _remove_selection(void)
       if (gtk_tree_model_get_iter(model, &iter, path))
       {
         gtk_tree_model_get(model, &iter, COL_PTR, (ab_contact_t *)&contact, -1);
-        ab_delete_contact(contact);
+        ab_delete_contact_v2(contact->id);
         has_row = gtk_list_store_remove(GTK_LIST_STORE(model), &iter);
       }
 
