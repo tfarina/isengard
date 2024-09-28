0a00d5c3797c81a1ae23638878eea2feef37d7fb
Thiago Farina <tfransosi@gmail.com>
2024-09-28 10:25:28 +0100

    tribe: fix memory leak in ls.c
    We should free the pointer to contacts returned by ab_enum_contacts_v2 (if it
    succeeded) after done using it. Why? Because ab_enum_contacts_v2 calls
    _db_enum_contacts and this function allocates memory for the array using malloc
    with the following code:
    
    contacts = malloc(row_count * sizeof(ab_contact_t));
    
    So if we don't free or release this memory after using it, we are basically
    leaking it, which is no good.
    
    I forgot to do this when I did the same at cmd_list.c at commit ec9ed06 (
    "tribe: free the contacts list array after displaying it").
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/tribe/src/ui/cli/ls.c
+++ c/apps/tribe/src/ui/cli/ls.c
@@ -29,6 +29,11 @@ int main(int argc, char **argv) {
 
   rc = print_contact_list_v2(num_contacts, contacts);
 
+  if (contacts) {
+    free(contacts);
+    contacts = NULL;
+  }
+
   ab_fini();
 
   return rc;
