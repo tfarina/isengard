8c65d012fa34435a109996dd34ca39dd52818a2c
Thiago Farina <tfransosi@gmail.com>
2025-01-03 15:20:08 +0000

    tribe: remove obvious comment in sql file
    There is no need to state the obvious.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/tribe/database/sqlite3/schema_data.sql
+++ c/apps/tribe/database/sqlite3/schema_data.sql
@@ -1,7 +1,3 @@
-/* Initial Data ------------------------------------------------------------- */
-
-/**
- * Populating with some values.
- */
+/* Initial Data ------------------------------------------------------------ */
 
 INSERT INTO "contacts" VALUES (1, 'John', 'Doe', 'jdoe@yahoo.com');
