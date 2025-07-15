85673c240fa76d26e5c9c3c73d1b51f907aecad4
Thiago Farina <tfransosi@gmail.com>
2025-07-15 14:16:51 +0100

    feat: add experiments/ directory with initial linebyline program
    Introduced a new experiments/ directory to organize small, self-contained
    programs written for learning and exploration.
    
    Added the first experiment: linebyline/, which reads and prints lines
    from a file using standard C I/O. This structure makes it easier to grow
    a collection of focused C programs without cluttering the main project
    layout.
    
    Included CMake support for building the linebyline executable.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

---
+++ c/experiments/linebyline/main.c
@@ -0,0 +1,49 @@
+#include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "os_path.h"
+
+int main(int argc, char **argv) {
+  char *filename;
+  FILE *fp;
+  char linebuf[BUFSIZ];
+  int lineno = 0;
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: linebyline <filename>\n");
+    return 1;
+  }
+
+  filename = argv[1];
+
+  if (!os_path_exists(filename)) {
+    fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+    return 1;
+  }
+
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+    return 1;
+  }
+
+  /* This loops through the lines of the file, printing it. */
+  while (fgets(linebuf, sizeof(linebuf), fp)) {
+    lineno++;
+
+    printf("%d:%s", lineno, linebuf);
+
+    /* This loops through *every* character present in this line. */
+    char *c;
+    for (c = linebuf; *c; c++) {
+      printf("%c", *c);
+    }
+  }
+
+  fclose(fp);
+
+  return 0;
+}
