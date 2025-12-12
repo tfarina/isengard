9e64e869dfe6f012bc53f184259e59262ccfa8f2
Thiago Farina <tfransosi@gmail.com>
2025-12-12 16:36:19 +0000

    echod: add robust feature detection for __builtin_expect in optimization.h
    Introduced M_HAS_BUILTIN_EXPECT which is set when the compiler provides
    __builtin_expect. Detection tries Clang/modern compilers' __has_builtin()
    first and falls back to checking __GNUC__ >= 3 (when __has_builtin is not
    available). The rest of the header will use M_HAS_BUILTIN_EXPECT to choose
    between the fast-path builtin and a portable fallback.
    
    No changes to M_LIKELY/M_UNLIKELY semantics are made in this commit.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/apps/network/echod/src/optimization.h
+++ c/apps/network/echod/src/optimization.h
@@ -6,6 +6,26 @@
  * @brief Branch prediction hints.
  */
 
+/* Feature-detect __builtin_expect once (Clang/GCC or compilers with
+ * __has_builtin)
+ */
+#if !defined(M_HAS_BUILTIN_EXPECT)
+# if defined(__has_builtin)
+#  if __has_builtin(__builtin_expect)
+#   define M_HAS_BUILTIN_EXPECT 1
+#  endif /* __has_builtin(__builtin_expect) */
+# endif /* defined(__has_builtin) */
+
+/* Fallback to checking GCC version (__builtin_expect was introduced in
+ * GCC version 3.0)
+ */
+# if !defined(M_HAS_BUILTIN_EXPECT)
+#  if defined(__GNUC__) && (__GNUC__ >= 3)
+#   define M_HAS_BUILTIN_EXPECT 1
+#  endif /* defined(__GNUC__) && (__GNUC__ >= 3) */
+# endif /* !defined(M_HAS_BUILTIN_EXPECT) */
+#endif /* !defined(M_HAS_BUILTIN_EXPECT) */
+
 /**
  * M_LIKELY:
  *
@@ -14,7 +34,7 @@
  * @exp: C expression.
  */
 #if !defined(M_LIKELY)
-#if defined(__GNUC__) && (__GNUC__ >= 3)
+#if defined(M_HAS_BUILTIN_EXPECT)
 #define M_LIKELY(exp) __builtin_expect ((exp), 1)
 #else
 #define M_LIKELY(exp) (exp)
@@ -29,7 +49,7 @@
  * @exp: C expression.
  */
 #if !defined(M_UNLIKELY)
-#if defined(__GNUC__) && (__GNUC__ >= 3)
+#if defined(M_HAS_BUILTIN_EXPECT)
 #define M_UNLIKELY(exp) __builtin_expect ((exp), 0)
 #else
 #define M_UNLIKELY(exp) (exp)
