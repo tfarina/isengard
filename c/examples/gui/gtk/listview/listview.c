b3fb3cff5ec8936a05c44becd5579ce371420190
Thiago Farina <tfransosi@gmail.com>
2026-04-18 19:17:02 +0100

    listview: group gtk_window_set_* calls together
    Reorder window setup code to keep all `gtk_window_set_*()` calls
    grouped together, and move `gtk_container_set_border_width()`
    after them.
    
    This improves readability by organizing related calls and making
    the window configuration section more consistent and easier to scan.
    No functional changes.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- c/examples/gui/gtk/listview/listview.c
+++ c/examples/gui/gtk/listview/listview.c
@@ -33,8 +33,8 @@ int main(int argc, char *argv[])
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW(window), "List view");
   gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-  gtk_container_set_border_width(GTK_CONTAINER(window), 10);
   gtk_window_set_default_size(GTK_WINDOW(window), 270, 250);
+  gtk_container_set_border_width(GTK_CONTAINER(window), 10);
 
   g_signal_connect(G_OBJECT (window), "destroy",
 		   G_CALLBACK(gtk_main_quit), NULL);
